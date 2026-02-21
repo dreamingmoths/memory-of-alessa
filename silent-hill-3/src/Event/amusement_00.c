174a8d952da2599b8db63eb7a9b129f610ae2c0c
Antonio Piccinno <105627859+Piccio23@users.noreply.github.com>
2026-02-22 00:15:18 +0100

    Updated amusement_00.c : added func_01F6E6A0_amusement_00

--- silent-hill-3/src/Event/amusement_00.c
+++ silent-hill-3/src/Event/amusement_00.c
@@ -18,7 +18,15 @@ INCLUDE_ASM("asm/nonmatchings/Event/amusement_00", func_01F6E420_amusement_00);
 
 INCLUDE_ASM("asm/nonmatchings/Event/amusement_00", func_01F6E590_amusement_00);
 
-INCLUDE_ASM("asm/nonmatchings/Event/amusement_00", func_01F6E6A0_amusement_00);
+u32 func_01F6E6A0_amusement_00(s32 arg0) {
+    int x;
+    switch (arg0) {
+        case 1:
+        x = (D_1D3169C >> 9) & 1 ? 0 : 1;
+        break;
+    }
+    return x;
+}
 
 INCLUDE_ASM("asm/nonmatchings/Event/amusement_00", func_01F6E6D0_amusement_00);
 
