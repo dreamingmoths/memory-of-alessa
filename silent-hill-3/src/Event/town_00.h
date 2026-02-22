8ea82085d4ae5a28721377a94e0f0c979427e255
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-02-22 23:17:58 +0100

    Update town_00.h

--- silent-hill-3/src/Event/town_00.h
+++ silent-hill-3/src/Event/town_00.h
@@ -15,19 +15,19 @@ extern s32 D_01F6DF74_town_00;
 extern f32 D_01F6DF78_town_00;
 
 extern s32 D_01F6E000_town_00;
+extern s32 D_01F6E008_town_00;
                                
 extern f32 D_01F6DF80_town_00;
 extern s32 D_01F6DF84_town_00;
 extern s32 D_01F6DF88_town_00;
+extern s32 D_01F6DF90_town_00;
 
 extern s32 func_0012FD80(s32, s64);                         
 extern s32 func_00199770(s32);                             
 extern s32 func_001E2110(s32);                             
 extern void shCharacter_Manage_Delete(s32, u16);                
-extern s32 D_01F6DF90_town_00;
-extern u32 D_1D31684;
 
-extern s32 D_01F6E008_town_00;
+extern u32 D_1D31684;
 
 extern void func_01F6D8A0_town_00();                          
 
