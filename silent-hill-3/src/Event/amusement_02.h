e908d5676da76b4291fecb205bc64cfb70b98187
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-02-22 12:20:24 +0100

    Added amusement_02.h

---
+++ silent-hill-3/src/Event/amusement_02.h
@@ -0,0 +1,17 @@
+#ifndef AMUSEMENT_02_H
+#define AMUSEMENT_02_H
+
+#include "common.h"
+
+extern void SeCall(float arg0, float arg1, long arg2);
+extern short func_00170810();                               
+extern int D_01F7AB58_amusement_02;
+
+extern s32 D_01F7AB78_amusement_02;
+extern s32 D_01F7AB50_amusement_02;
+extern f32 D_01F76698_amusement_02;
+extern s32 D_01F76690_amusement_02;
+
+extern s32 func_01F6F560_amusement_02(); 
+
+#endif
