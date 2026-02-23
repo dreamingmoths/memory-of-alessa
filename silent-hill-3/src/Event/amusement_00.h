766778869625515694807699a9d698ed117b0840
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-02-23 22:40:57 +0100

    Update amusement_00.h

--- silent-hill-3/src/Event/amusement_00.h
+++ silent-hill-3/src/Event/amusement_00.h
@@ -3,6 +3,42 @@
 
 #include "common.h"
 
-extern u32 D_1D3169C;
+typedef struct {
+    float unk0;
+    int   unk4;
+} D_01F72DD8_struct;
+
+void func_0013D250(int, int, float);
+void func_0013D280(int);
+void func_0015DCD0(int, int (*)[4], int, int, float, float);
+void func_001602D0(int, int, int, float);
+int  func_001643C0();
+int  func_001646C0();
+int  func_001646D0();
+int  func_0016C1C0(int);
+int  func_0016C540(int*, int*);
+int  func_0016CB70();
+void func_0016D0E0(int, int);
+int  func_0016D240(int, int (*)[4], int, int, float, float);
+long func_00170810();
+void func_00190C40();
+float func_00190A20(int);
+void func_001C2290(int, float);
+int  func_001C2580(int);
+void SeCall(float arg0, float arg1, long arg2);
+
+extern int D_01F72830_amusement_00;
+extern int D_01F72890_amusement_00;
+extern int D_01F728B0_amusement_00;
+extern int D_01F728E0_amusement_00;
+extern int D_01F72940_amusement_00;
+extern int D_01F72C10_amusement_00[4];
+extern int D_01F72C20_amusement_00[4];
+extern int D_01F72C30_amusement_00[4];
+extern int D_01F72D80_amusement_00;
+extern int D_01F72D90_amusement_00;
+extern int D_01F72DA0_amusement_00;
+extern int D_1D3169C;
+extern D_01F72DD8_struct* D_01F72DD8_amusement_00;
 
 #endif
