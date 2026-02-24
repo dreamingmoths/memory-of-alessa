64224d9f50c4761db2ba0a9ea5d19cd8b6e9faac
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-02-24 19:47:08 +0100

    add motel_00.h

---
+++ silent-hill-3/src/Event/motel_00.h
@@ -0,0 +1,37 @@
+#ifndef MOTEL_00_H
+#define MOTEL_00_H
+
+#include "common.h"
+
+int   func_0013D080(int, int, int, int);
+void  func_0015E780(int);
+void  func_00164210();
+int   func_001643D0();
+int   func_001646F0();
+void  func_00164750(int);
+int   func_0016C540(int*, int*);
+void  func_0016CA40(int);
+void  func_0016CB30(int);
+void  func_0018A260(int);
+void  func_0018A310(int, int, int);
+void  func_00190A20(int);
+void  func_0019A940();
+int   func_0019A9B0(float);
+void  func_001C2290(int, float);
+void  func_001C2A60(int, float);
+void  func_00316C50(int);
+float shGetDT();
+
+extern int D_01F6DF60_motel_00;
+extern int D_01F6DFC0_motel_00;
+extern int D_01F6DFE0_motel_00;
+extern int D_01F6E040_motel_00;
+extern int D_01F6E060_motel_00;
+extern int D_01F6E0C0_motel_00;
+extern int D_01F6E100_motel_00;
+extern float D_01F6E108_motel_00;
+extern int D_1D316AC;
+extern int D_1D31684;
+extern int D_1D316FC;
+
+#endif
