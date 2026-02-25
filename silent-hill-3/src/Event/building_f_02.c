88f192c563a88a5a7a2b6ea4e1be23c304205c3b
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-02-25 04:55:30 +0100

    sh3: match building_f_02 (#7)
    * match building_f_02.c
    
    * add building_f_02.h

--- silent-hill-3/src/Event/building_f_02.c
+++ silent-hill-3/src/Event/building_f_02.c
@@ -1,3 +1,30 @@
-#include "common.h"
+#include "building_f_02.h"
 
-INCLUDE_ASM("asm/nonmatchings/Event/building_f_02", func_01F6D680_building_f_02);
+void func_01F6D680_building_f_02(void) {
+    switch (func_00170810()) {                              
+        case 0x7A:
+            if (!(((u32)D_1D3166C >> 1) & 1)) {
+                func_0016CA40(7);
+                D_1D31710 |= 0x200;
+            } else {
+                D_1D31710 &= ~0x200;
+            }
+        break;
+        case 0x7C:
+            if ((func_00199820() & 0xFF) && !(((u32)D_1D31668 >> 0x1E) & 1)) {
+                func_0016CA40(4);
+                D_1D31710 |= 0x1000;
+            } else {
+                D_1D31710 &= ~0x1000;
+            }
+        break;
+        case 0x7D:
+            if (!(D_1D3166C & 1)) {
+                func_0016CA40(3);
+                D_1D31710 |= 0x2000;
+            } else {
+                D_1D31710 &= ~0x2000;
+            }
+        break;
+    }
+}
---
+++ silent-hill-3/src/Event/building_f_02.h
@@ -0,0 +1,13 @@
+#ifndef BUILDING_F_02_H
+#define BUILDING_F_02_H
+
+#include "common.h"
+
+void func_0016CA40(int);                                 
+int func_00170810();                                
+int func_00199820();                                
+extern u_int D_1D31668;
+extern u_int D_1D3166C;
+extern int D_1D31710;
+
+#endif
