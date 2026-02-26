f1a0cc0a14fb74da8545e2aa4f2b82d3106c7d8a
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-02-26 20:59:51 +0100

    add church_03.h

---
+++ silent-hill-3/src/Event/church_03.h
@@ -0,0 +1,34 @@
+#ifndef CHURCH_03_H
+#define CHURCH_03_H
+
+#include "common.h"
+
+typedef struct {
+    float unk0;
+    int   unk4;
+} D_01F6DF88_struct;
+
+int   func_0012FD80(int, long);
+void  func_0013D250(int, int, float);
+void  func_0013D280(int);
+void  func_001602D0(int, int, int, float);
+void  func_001603E0(int, int);
+float func_001643C0();
+int   func_0016C540(int*, int*);
+void  func_0016CAF0(int);
+int   func_00199C70(int);
+void  func_001C2290(int, float);
+void  func_001DC9E0(int, int);
+int   func_0022F150(int);
+void  func_00316C50(int);
+
+extern int D_01F6DDB0_church_03;
+extern int D_01F6DE10_church_03;
+extern int D_01F6DE30_church_03;
+extern int D_01F6DE90_church_03;
+extern int D_01F6DEF0_church_03;
+extern int D_01F6DF80_church_03;
+extern D_01F6DF88_struct* D_01F6DF88_church_03;
+extern int D_1D316AC;
+
+#endif
