6f415063fb46e7cda74cdb7d3140dcf2110c61b2
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-02-27 12:03:38 +0100

    add building_b_01.h

---
+++ silent-hill-3/src/Event/building_b_01.h
@@ -0,0 +1,23 @@
+#ifndef BUILDING_B_01_H
+#define BUILDING_B_01_H
+
+#include "common.h"
+
+#define BUILDING_OTHERWORLD_BATHTUB_ROOM                0x83
+#define BUILDING_OTHERWORLD_HANGING_MOTHER_ROOM         0x84
+#define BUILDING_OTHERWORLD_VINCENT_ROOM                0x85
+#define BUILDING_OTHERWORLD_VINCENT_CORRIDOR            0x86
+#define BUILDING_OTHERWORLD_OXYDOL_ROOM                 0x87
+
+void func_0016CA40(int);                                 
+int func_00170810();                                
+void func_00189F00(u_short);                                 
+void func_00189FA0(u_short, u_short);                              
+int func_00199820();                                
+void func_01F6DFF0_building_b_01();
+
+extern u32 D_1D3166C;
+extern u32 D_1D31670;
+extern u32 D_1D31714;
+
+#endif
