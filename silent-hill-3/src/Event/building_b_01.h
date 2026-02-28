7585743b5fa90258afa4e86d46f0ae2c1dd57892
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-02-28 11:55:01 +0100

    update building_b_01.h

--- silent-hill-3/src/Event/building_b_01.h
+++ silent-hill-3/src/Event/building_b_01.h
@@ -10,20 +10,24 @@
 #define BUILDING_OTHERWORLD_OXYDOL_ROOM         0x87
 
 void func_0016CA40(int);
-void func_00170810(); //RoomName()
+int  func_0016C540(int*, int*);
+int  func_00170810(); //RoomName()
 void func_00189F00(u_short);
 void func_00189FA0(u_short, u_short);
 int  func_00199820();
 void func_001C2290(int, float);
 void func_01F6DFF0_building_b_01();
 
-extern u32 D_01D31640;
-extern s32 D_01F6F118_building_b_01;
-extern s32 D_01F6F130_building_b_01;
-extern s32 D_01F6F138_building_b_01;
-extern u32 D_1D3166C;
-extern u32 D_1D31670;
-extern u32 D_1D31714;
-extern u32 D_1D317AC;
+extern u_int D_01D31640;
+extern int D_01F6EFF0_building_b_01;
+extern int D_01F6F050_building_b_01;
+extern int D_01F6F118_building_b_01;
+extern int D_01F6F130_building_b_01;
+extern int D_01F6F138_building_b_01;
+extern u_int D_1D3166C;
+extern u_int D_1D31670;
+extern u_int D_1D31714;
+extern u_int D_1D317AC;
+extern u_int D_1D316AC;
 
 #endif
