02eb5c615b548899e60206ae6abf8efd0b0032fc
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-02-28 10:45:46 +0100

    update building_b_01.h

--- silent-hill-3/src/Event/building_b_01.h
+++ silent-hill-3/src/Event/building_b_01.h
@@ -3,21 +3,27 @@
 
 #include "common.h"
 
-#define BUILDING_OTHERWORLD_BATHTUB_ROOM                0x83
-#define BUILDING_OTHERWORLD_HANGING_MOTHER_ROOM         0x84
-#define BUILDING_OTHERWORLD_VINCENT_ROOM                0x85
-#define BUILDING_OTHERWORLD_VINCENT_CORRIDOR            0x86
-#define BUILDING_OTHERWORLD_OXYDOL_ROOM                 0x87
+#define BUILDING_OTHERWORLD_BATHTUB_ROOM        0x83
+#define BUILDING_OTHERWORLD_HANGING_MOTHER_ROOM 0x84
+#define BUILDING_OTHERWORLD_VINCENT_ROOM        0x85
+#define BUILDING_OTHERWORLD_VINCENT_CORRIDOR    0x86
+#define BUILDING_OTHERWORLD_OXYDOL_ROOM         0x87
 
-void func_0016CA40(int);                                 
-int func_00170810();                                
-void func_00189F00(u_short);                                 
-void func_00189FA0(u_short, u_short);                              
-int func_00199820();                                
+void func_0016CA40(int);
+void func_00170810(); //RoomName()
+void func_00189F00(u_short);
+void func_00189FA0(u_short, u_short);
+int  func_00199820();
+void func_001C2290(int, float);
 void func_01F6DFF0_building_b_01();
 
+extern u32 D_01D31640;
+extern s32 D_01F6F118_building_b_01;
+extern s32 D_01F6F130_building_b_01;
+extern s32 D_01F6F138_building_b_01;
 extern u32 D_1D3166C;
 extern u32 D_1D31670;
 extern u32 D_1D31714;
+extern u32 D_1D317AC;
 
 #endif
