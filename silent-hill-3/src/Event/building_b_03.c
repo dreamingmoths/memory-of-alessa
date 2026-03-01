09a6eaf5c2c19409c04b12ac7a6cee8ccb74c499
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-03-01 13:05:56 +0100

    match func_01F6E8C0_building_b_03

--- silent-hill-3/src/Event/building_b_03.c
+++ silent-hill-3/src/Event/building_b_03.c
@@ -1,4 +1,4 @@
-#include "common.h"
+#include "building_b_03.h"
 
 INCLUDE_ASM("asm/nonmatchings/Event/building_b_03", func_01F6D680_building_b_03);
 
@@ -20,9 +20,77 @@ INCLUDE_ASM("asm/nonmatchings/Event/building_b_03", func_01F6DE40_building_b_03)
 
 INCLUDE_ASM("asm/nonmatchings/Event/building_b_03", func_01F6DF60_building_b_03);
 
-INCLUDE_ASM("asm/nonmatchings/Event/building_b_03", func_01F6E700_building_b_03);
+#ifdef NON_MATCHING
+int func_01F6E700_building_b_03(void) {
+    int var_s0;
 
-INCLUDE_ASM("asm/nonmatchings/Event/building_b_03", func_01F6E8C0_building_b_03);
+    var_s0 = 0;
+    if (!(( D_1D31670 >> 0x16) & 1)) {
+        D_01F6FC00_building_b_03 = 0;
+        D_01F6FC08_building_b_03 = 0.0f;
+        D_1D31670 |= 0x400000;
+        func_001C2290(3, 1.5f);
+        func_0016C1A0();
+        SeCall(1.0f, 0.0f, 0x4A59);
+    }
+    D_01F6FC08_building_b_03 += shGetDT();
+    switch (D_01F6FC00_building_b_03) {
+
+        case 0:
+            if (D_01F6FC08_building_b_03 < 0.5f) {
+                return 0;
+            }
+            SeCall(1.0f, 0.0f, 0x4A58);
+            D_01F6FC00_building_b_03 += 1;
+
+        case 1:
+            if (D_01F6FC08_building_b_03 < 1.5f) {
+                return 0;
+            }
+            func_0016C1B0();
+            D_01F6FC00_building_b_03 += 1;
+            break;
+
+        default:
+            var_s0 = 1;
+            D_1D31670 &= 0xFFBFFFFF;
+            D_1D31714 |= 0x80000000;            
+            D_1D31670 |= 0x200;
+            D_1D317B0 |= 0x2000;
+    }
+
+    return var_s0;
+}
+#else
+INCLUDE_ASM("asm/nonmatchings/Event/building_b_03", func_01F6E700_building_b_03);
+#endif
+
+void func_01F6E8C0_building_b_03(void) {
+    int room;    
+    room = RoomName();
+    D_01F6FC28_building_b_03 = 0;
+    switch (room) { //I would like to put RoomName() here directly
+
+        case BUILDING_OTHERWORLD_MATCHBOOK_ROOM: //Matchbook room
+            break;
+
+        case BUILDING_OTHERWORLD_GALLERY_ROOM: //otherworld gallery
+            D_01F6FC40_building_b_03 = 0;
+            D_1D31718 |= 0x10; //first time entering gallery 
+            break;
+
+        case BUILDING_OTHERWORLD_ELEVATOR: //elevator
+            D_01F6FC10_building_b_03 = 0;
+            D_01F6FC18_building_b_03 = 1.0f;
+            D_1D31718 |= 0x20; //this get set the FIRST TIME you enter the elevator
+            D_01F6FC20_building_b_03 = 1500.0f;
+
+            if (!((D_1D31670 >> 6) & 1) && !((D_1D31670 >> 7) & 1) && !((D_1D31670 >> 8) & 1) && !((D_1D31670 >> 9) & 1)) {
+                D_1D31670 |= 0x80; //this get set ONLY WHEN YOU ENTER THE ELEVATOR FROM THE SECOND FLOOR, then it get reset everytime you exit the elevator
+            }            
+
+    }
+}
 
 INCLUDE_ASM("asm/nonmatchings/Event/building_b_03", func_01F6E9B0_building_b_03);
 
