85d69fdf38b96dd4828b5da8e8e28b2a27738bac
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-03-04 17:11:02 +0100

    add building_b_00.h

---
+++ silent-hill-3/src/Event/building_b_00.h
@@ -0,0 +1,34 @@
+#ifndef BUILDING_B_00_H
+#define BUILDING_B_00_H
+
+#include "common.h"
+
+#define BUILDING_OTHERWORLD_1F_HALL                         0x7E
+#define BUILDING_OTHERWORLD_LIVER_ROOM                      0x7F
+#define BUILDING_OTHERWORLD_1F_SLURPERS_CORRIDOR            0x80
+#define BUILDING_OTHERWORLD_1F_LAST_FAIRY_TAIL_PIECE_ROOM   0x81
+
+int   RoomName();
+void  SeCall(float, float, int);
+int   func_00168440();
+void  func_0016C1A0();
+void  func_0016C1B0();
+int   func_0016C1C0(u_int);
+void  func_0016C3C0();
+void  func_0016D6B0(u_int);
+void  func_00190A20(u_int);
+void  func_001C2290(int, float);
+void  func_00317420(u_int);
+void  func_00317490(int, float);
+void  func_003174B0(float);
+float shGetDT();
+
+extern int   D_01F6FB00_building_b_00;
+extern int   D_01F6FB08_building_b_00;
+extern float D_01F6FB10_building_b_00;
+extern u_int D_1D31670;
+extern u_int D_1D31714;
+extern int   D_1D31710;
+extern int   D_1D317A8;
+
+#endif
