e88187652dc70c44687258a7c7b6e7b4e763d2ea
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-03-12 21:49:21 +0100

    add church_02.h

---
+++ silent-hill-3/src/Event/church_02.h
@@ -0,0 +1,26 @@
+#ifndef CHURCH_02_H
+#define CHURCH_02_H
+
+#include "common.h"
+
+#define CHURCH_LIBRARY 0xFD
+//I will add all the others later
+#define CHURCH_ALESSAS_BEDROOM 0x106
+
+u_char GetRiddleLevel();
+short RoomName();
+int func_0016D5B0();
+int GetExtraNewGame();
+void func_001C2290(int, float);
+
+extern int D_01F77580_church_02;
+extern int D_01F77590_church_02;
+extern int D_01F77598_church_02;
+extern int D_01F775C8_church_02;
+extern int D_01F775C0_church_02;
+extern int D_01F77600_church_02;
+extern int D_01F77608_church_02;
+extern int D_01F77610_church_02;
+extern u_int D_1D316A8;
+
+#endif
