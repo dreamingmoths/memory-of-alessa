88a5413773f8deeafe209dde5b84c35b6fd099e7
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-03-13 19:13:10 +0100

    update church_00.h

--- silent-hill-3/src/Event/church_00.h
+++ silent-hill-3/src/Event/church_00.h
@@ -1,19 +1,55 @@
+#ifndef CHURCH_00_H
+#define CHURCH_00_H
+
 #include "common.h"
 
+struct ChurchStruct00 { float t; int id; };
+
+int func_0013D080(int, int, int, int);
+void func_0013D250(int, int, float);
+void func_0013D280(int);
+int func_001646C0(void);
+float func_001643C0(void);
+int func_0016BED0(int, int);
+int func_0016C1C0(int);
+int func_0016C540(void*, void*);
+void func_0016CA40(int);
+void func_0016DCE0(int);
+void func_0016E400(int, int);
+void func_0016ECE0(int);
+void func_00190A20(int);
+void func_0019A940(void);
+int func_0019A9B0(float);
+void func_001C0EB0(void*, int, int);
+void func_001C0EE0(int);
+void func_001C2290(int, float);
+int func_001C2580(int);
+void func_00300390(void);
+void func_00316C50(int);
+void func_01F6DCF0_church_00(Q*, int*, Q*, int*, int*);
+void SeCall(float, float, int);
+float shGetDT(void);
+
+extern short D_01F6E550_church_00[];
+extern short D_01F6E560_church_00[];
+
 extern int D_01F6E600_church_00;
+extern int D_01F6E608_church_00;
+
+extern float D_01F6E610_church_00;
+
+extern struct ChurchStruct00* D_01F6E618_church_00;
+extern struct ChurchStruct00* D_01F6E620_church_00;
+extern struct ChurchStruct00 D_01F6E3B0_church_00[];
+extern struct ChurchStruct00 D_01F6E480_church_00[];
+
+extern void* D_01F6E4D0_church_00;
+extern void* D_01F6E530_church_00;
+
+extern Q D_01F6E570_church_00;
+extern Q D_01F6E580_church_00;
+
+extern u_int D_1D31644;
 extern u_int D_1D316A4;
 
-extern void func_00190A20(int a0);
-extern void func_0019A940(void);
-extern void func_001C0EE0(int a0);
-extern void func_001C0EB0(void*, int, int);
-
-extern int func_0019A9B0(float a0);
-extern int func_0016C1C0(int a0);
-extern int func_0013D080(int a0, int a1, int a2, int a3);
-
-void func_01F6D9C0_church_00(int);
-void func_01F6DCD0_church_00(void);
-void func_01F6DCF0_church_00(void);
-
-
+#endif
