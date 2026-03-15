ae9ec6f09755ac6570ded9ecfa95b70b7d911bd2
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-03-15 11:17:57 +0100

    add hospital_f_03

---
+++ silent-hill-3/src/Event/hospital_f_03.h
@@ -0,0 +1,37 @@
+#ifndef HOSPITAL_F_03_H
+#define HOSPITAL_F_03_H
+
+#include "common.h"
+
+int RoomName();
+int func_0016BED0(u_int, u_int);
+int func_0016C1C0(int);
+void func_0016C3C0();
+void func_0016C540(float *, float *);
+void func_0016CA40(int);
+void func_00190A20(int);
+int func_00190C00();
+int func_00199C70(int);
+void func_00317420(u_int);
+void func_00317490(int, float);
+void func_003174B0(float);
+void SeCall(float, float, int);
+
+extern float D_01F70580_hospital_f_03;
+extern float D_01F705E0_hospital_f_03;
+
+extern int D_01F70890_hospital_f_03;
+extern int D_01F708D0_hospital_f_03;
+extern int D_01F708D8_hospital_f_03;
+extern int D_01F708E0_hospital_f_03;
+extern int D_01F70908_hospital_f_03;
+extern int D_01F70910_hospital_f_03;
+
+extern u_int D_1D31644;
+extern u_int D_1D31680;
+extern u_int D_1D31684;
+extern u_int D_1D31688;
+extern u_int D_1D316F4;
+extern u_int D_1D31724;
+
+#endif
