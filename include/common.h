235e3ff2f5b2cf518b34dbc29b02b6c93ad209a0
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-03-19 22:13:27 +0100

    add SET_FLAG

--- include/common.h
+++ include/common.h
@@ -33,7 +33,8 @@ typedef unsigned int u_long128;
 #define READ_UNCACHED(addr) ((((u_int)(addr)) & 0x0fffffff) | 0x20000000)
 
 #define GET_FLAG(x, i) (((x[i >> 5]) >> (i & 0x1F)) & 1)
-
+#define SET_FLAG(x, i) ((x[i >> 5]) |= (1 << (i & 0x1F)))
+    
 #define ABORT() asm("breakc 0")
 
 #ifdef DEBUG
