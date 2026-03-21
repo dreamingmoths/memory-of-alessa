8328eedfeb382973a1aa7c0aefebfa127831b275
moth dreams <mothdreams666@gmail.com>
2026-03-20 23:03:47 -0700

    update `model3_sub_n.h`

--- silent-hill-3/src/Chacter_Draw/model3_sub_n.h
+++ silent-hill-3/src/Chacter_Draw/model3_sub_n.h
@@ -16,24 +16,6 @@ typedef struct ModelWork
     void *draw_hook_data;                     // offset 0x24, size 0x4
 } ModelWork;
 
-typedef struct
-{
-    int texture_id;
-    int text_pos_param;
-} ModelTextPose;
-
-typedef struct
-{
-    ModelWork works[16];
-    void *current;
-} ModelWorkGroup;
-
-// By wrapping, we encourage more quadword ops
-typedef struct
-{
-    sceVu0FMATRIX m;
-} ModelMatrix;
-
 extern Model3Junk model3_junk;
 void shMulMatrix(sceVu0FMATRIX m0, sceVu0FMATRIX m1, sceVu0FMATRIX m2);
 
@@ -44,13 +26,7 @@ extern int initialized;
  */
 float* func_001D3780(void *work_);
 
-float func_001B4200(void);
-float func_001B4210(void);
-s64 func_001CC710(int flag, int text_poses_offset, int texture_blocks_offset);
-int func_001D0D80(int tex);
-int func_001D0DB0(int tex);
-int func_001D0DF0(int tex);
-void sh3gfw_Thr_d2TextureSend(int tex0, int arg1, int tex1, int tex2);
-__int128 *func_001B5B00(int tex0, int text_pos_param, int arg2);
-void func_001D37C0(void *work, __int128 mask, __int128 value);
+extern float func_001B4200(void);
+extern float func_001B4210(void);
+
 #endif
