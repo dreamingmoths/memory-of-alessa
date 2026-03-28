2e19cfe4446e110cbcaf94e641e41ec196b85009
Daniel Freitas <101065507+Dandiels@users.noreply.github.com>
2026-03-28 13:37:26 -0300

    update subway_01.h

--- silent-hill-3/src/Event/subway_01.h
+++ silent-hill-3/src/Event/subway_01.h
@@ -2,11 +2,14 @@
 #define SUBWAY_01_H
 
 #include "common.h"
+#include "Chacter/m3_sc.h"
 
-s32 func_0016CED0(s32, s32, s32 *);
+s32 func_0016CED0(s32, s32, s32*);
 s32 func_00190A20(s32);
 
 extern s32 D_01F70370_subway_01;
 extern s32 D_01F70700_subway_01;
+extern s32 D_01F70650_subway_01;
+extern s32 D_1D3165C;
 
 #endif
