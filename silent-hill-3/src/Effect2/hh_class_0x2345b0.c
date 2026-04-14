7a80a54522cb5dd83dfec555cebbf2caaa5fdeb9
moth dreams <mothdreams666@gmail.com>
2026-04-13 21:03:00 -0700

    sh3: split `hh_class_0x2345b0.c`

---
+++ silent-hill-3/src/Effect2/hh_class_0x2345b0.c
@@ -0,0 +1,15 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_0x2345b0", func_00334030);
+
+INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_0x2345b0", HH_Class_pPrefix_0x10);
+
+INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_0x2345b0", HH_Class_pSuffix_0x10);
+
+INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_0x2345b0", HH_Class_pPrefix_0x11);
+
+INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_0x2345b0", HH_Class_pSuffix_0x11);
+
+INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_0x2345b0", HH_Class_pMain_0x10);
+
+INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_0x2345b0", HH_Class_pMain_0x11);
