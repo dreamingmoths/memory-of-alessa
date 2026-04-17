8f77657390ef061eeb15217ccaeed5abf54962f8
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-04-17 11:10:50 +0200

    update dma1serv.h

--- silent-hill-2/src/Multi_thr/dma/dma1serv.h
+++ silent-hill-2/src/Multi_thr/dma/dma1serv.h
@@ -1,4 +1,13 @@
 #ifndef DMA1SERV_H
 #define DMA1SERV_H
 
+#include "common.h"
+
+int d1sInit(int th_prio /* r11 */, void* stack /* r10 */, int stackSize /* r9 */, void* queue /* r16 */, int queueSize /* r2 */);
+int d1sSync(int mode /* r3 */, int cid /* r2 */);
+int d1sGetCurCmdId(void);
+int d1sCmpCID(int cid0 /* r2 */, int cid1 /* r2 */);
+int d1sPutCmd0(int (* f0)() /* r2 */);
+int d1sPutCmd1(int (* f1)() /* r3 */, u_long a1 /* r2 */);
+
 #endif // DMA1SERV_H
