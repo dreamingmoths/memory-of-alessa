298c9053aa39859fabe95f2ff0f81e7154c39882
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-04-18 10:18:40 +0200

    update dma1serv.c

--- silent-hill-2/src/Multi_thr/dma/dma1serv.c
+++ silent-hill-2/src/Multi_thr/dma/dma1serv.c
@@ -3,10 +3,9 @@
 
 extern /* static */ void* d1sCmdWorkP;
 
-#line 25
 int d1sInit(int th_prio /* r11 */, void* stack /* r10 */, int stackSize /* r9 */, void* queue /* r16 */, int queueSize /* r2 */) {
     int ret = 0; // r2    
-    if (d1sCmdWorkP == 0) {
+    if (d1sCmdWorkP == NULL) {
         ret = CmdServInit(queue, queueSize, stack, stackSize, th_prio);
         if (ret != 0) {
             d1sCmdWorkP = queue;
@@ -15,43 +14,38 @@ int d1sInit(int th_prio /* r11 */, void* stack /* r10 */, int stackSize /* r9 */
     return ret;
 }
 
-#line 52
 int d1sSync(int mode /* r3 */, int cid /* r2 */) {
     int ret = 0;
-    if (d1sCmdWorkP != 0) 
+    if (d1sCmdWorkP != NULL) 
         ret = CmdQueueSync(d1sCmdWorkP, mode, cid);
     
     return ret;    
 }
 
-#line 67
 int d1sGetCurCmdId(void) {
     int ret = -1; // r2
-    if (d1sCmdWorkP != 0) 
+    if (d1sCmdWorkP != NULL) 
         ret = CmdQueueGetCurrentCommandId(d1sCmdWorkP);  
     
     return ret;    
 }
 
-#line 82
 int d1sCmpCID(int cid0 /* r2 */, int cid1 /* r2 */)  {
     return CmdQueueCmpCmdId(cid0, cid1); // not 100% sure
 }
 
-#line 113
-int d1sPutCmd0(int (* f0)() /* r2 */) {
+int d1sPutCmd0(int (* f0)(void) /* r2 */) {
     int ret = -1; // r2
-    if (d1sCmdWorkP != 0)
+    if (d1sCmdWorkP != NULL)
         ret = CmdQueuePut0(d1sCmdWorkP, f0);
 
     
     return ret;
 }
 
-#line 123
-int d1sPutCmd1(int (* f1)() /* r3 */, u_long a1 /* r2 */) {
+int d1sPutCmd1(int (* f1)(void) /* r3 */, u_long a1 /* r2 */) {
     int ret = -1; // r2
-    if (d1sCmdWorkP != 0) 
+    if (d1sCmdWorkP != NULL) 
         ret = CmdQueuePut1(d1sCmdWorkP, f1, a1);
 
     
