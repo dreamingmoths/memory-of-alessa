1f8a61d241771d11a21793dc8bcbb730fd4c242b
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-04-18 10:16:37 +0200

    update cmd_serv.h

--- silent-hill-2/src/Multi_thr/util/cmd_serv.h
+++ silent-hill-2/src/Multi_thr/util/cmd_serv.h
@@ -7,7 +7,7 @@ int CmdServInit(u_long* work /* r22 */, int workSize /* r2 */, void* stack /* r2
 int CmdQueueSync(u_long* work /* r2 */, int mode /* r20 */, int id /* r19 */);
 int CmdQueueGetCurrentCommandId(u_long* work /* r2 */);
 int CmdQueueCmpCmdId(int cid0 /* r2 */, int cid1 /* r2 */);
-int CmdQueuePut0(u_long* work /* r2 */, int (* func)() /* r17 */);
-int CmdQueuePut1(u_long* work /* r2 */, int (* func)() /* r20 */, u_long a0 /* r19 */); 
+int CmdQueuePut0(u_long* work /* r2 */, int (* func)(void) /* r17 */);
+int CmdQueuePut1(u_long* work /* r2 */, int (* func)(void) /* r20 */, u_long a0 /* r19 */); 
 
 #endif // CMD_SERV_H
