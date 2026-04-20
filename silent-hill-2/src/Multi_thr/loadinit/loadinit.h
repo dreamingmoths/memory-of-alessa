cb77f377119303a7d25bde3bd91e60dd49e35a56
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-04-20 10:48:36 +0200

    update loadinit.h

--- silent-hill-2/src/Multi_thr/loadinit/loadinit.h
+++ silent-hill-2/src/Multi_thr/loadinit/loadinit.h
@@ -1,4 +1,12 @@
 #ifndef LOADINIT_H
 #define LOADINIT_H
 
+#include "common.h"
+#include "Multi_thr/util/cmd_serv.h"
+
+int lisInit(int th_prio /* r11 */, void* stack /* r10 */, int stackSize /* r9 */, void* queue /* r16 */, int queueSize /* r2 */);
+int lisSync(int mode /* r3 */, int cid /* r2 */);
+int lisGetStat(CmdServStat* stat /* r2 */);
+int lisPutCmd0(int (* f0)(void) /* r2 */);
+
 #endif // LOADINIT_H
