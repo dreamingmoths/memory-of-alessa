a77f7b6e42f04c1eeee019dfc76c2482b23be7f9
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-04-20 10:10:08 +0200

    sh2: add 2 others dic funcs

--- silent-hill-2/src/Font/dic.c
+++ silent-hill-2/src/Font/dic.c
@@ -5,12 +5,86 @@ static u_short dicCode82(u_char c);
 static u_short dicCode83(u_char c);
 static u_short dicCode84(u_char c);
 static u_short dicCode87(u_char c);
+static u_short dicCodeAsc(u_char c);
 
 INCLUDE_ASM("asm/nonmatchings/Font/dic", dicSetStr);
 
-INCLUDE_ASM("asm/nonmatchings/Font/dic", dicTransCode);
+u_short dicTransCode(u_short code) {
+    u_char ch = code >> 8;
+    u_char cl = code & 0xFF;
+    
+    switch (ch) {
+        case 0x00: return dicCodeAsc(cl);
+        case 0x81: return dicCode81(cl);
+        case 0x82: return dicCode82(cl);
+        case 0x83: return dicCode83(cl);
+        case 0x84: return dicCode84(cl);
+        case 0x87: return dicCode87(cl);
+        default:
+            if (cl < 0x40 || cl == 0x7F || cl > 0xFC) {
+                return DICTIONARY_INVALID;
+            }
+            if (code >= 0x889F && code < 0x9873) {
+                if (cl > 0x7F) cl--;
+                return ((ch - 0x88) * 0xBC + cl + 0x2A0);
+            }
+            if (code >= 0x989F && code < 0x9FFD) {
+                if (cl > 0x7F) cl--;
+                return ((ch - 0x98) * 0xBC + cl + 0xE35);
+            }
+            if (code >= 0xE040 && code < 0xEAA5) {
+                if (cl > 0x7F) cl--;
+                return ((ch - 0xE0) * 0xBC + cl + 0x1415);
+            }
+            if (code >= 0xED40 && code < 0xEEED) {
+                if (cl > 0x7F) cl--;
+                return ((ch - 0xED) * 0xBC + cl + 0x1BED);
+            }
+            if (code >= 0xFA40 && code < 0xFC4C) {
+                if (cl > 0x7F) cl--;
+                return ((ch - 0xFA) * 0xBC + cl + 0x1BD1);
+            }
+            return DICTIONARY_INVALID;
+    }
+}
 
-INCLUDE_ASM("asm/nonmatchings/Font/dic", dicCodeAsc);
+static u_short dicCodeAsc(u_char c) {
+    u_short num1[15] = { // @308
+        0x0008, 0x0027, 0x0052, 0x004E, 0x0051, 0x0053, 0x0025, 0x0028, 
+        0x0029, 0x0054, 0x003A, 0x0002, 0x003B, 0x0003, 0x001D 
+    };
+    u_short num2[7] = {  // @309
+        0x0005, 0x0006, 0x0041, 0x003F, 0x0042, 0x0007, 0x0055 
+    };
+    u_short num3[6] = { // @310
+        0x002C, 0x004D, 0x002D, 0x000E, 0x0010, 0x0024 
+    };
+    u_short num4[4] = { // @311
+        0x002E, 0x0021, 0x002F, 0x001F 
+    };    
+    if (c == 0x20) { 
+        return 0;
+    }    
+    if (c < 0x30) { 
+        return num1[c - 0x21] + 0xE0;
+    }    
+    if (c < 0x3A) { 
+        return c + 0x142;
+    }
+    if (c < 0x41) {
+        return num2[c - 0x3A] + 0xE0;
+    }
+    if (c < 0x5B) {
+        return c + 0x13B;
+    }
+    if (c < 0x61) {
+        return num3[c - 0x5B] + 0xE0;
+    }
+    if (c < 0x7B) {
+        return c + 0x135;
+    }
+    return num4[c - 0x7B] + 0xE0;
+}
 
 static u_short dicCode81(u_char c) {
     if (!IS_VALID_SJIS_81(c)) {
