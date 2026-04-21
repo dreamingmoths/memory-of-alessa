889291c301c8216fe67479795357db8b4a6ea2aa
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-04-21 10:43:37 +0200

    update sh_cdvd.h

--- silent-hill-2/src/Multi_thr/filesys/sh_cdvd.h
+++ silent-hill-2/src/Multi_thr/filesys/sh_cdvd.h
@@ -1,4 +1,11 @@
 #ifndef SH_CDVD_H
 #define SH_CDVD_H
 
+#include "common.h"
+
+int shCdInitR(int initmode /* r17 */, int mmode /* r16 */);
+int shSifInit(void);
+int shSifLoadModuleR(char* module /* r20 */, int args /* r19 */, char* argp /* r18 */);
+int shSifRebootIopR(char* imgfile /* r18 */);
+
 #endif // SH_CDVD_H
