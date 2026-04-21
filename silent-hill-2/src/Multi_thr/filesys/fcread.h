a7397982fe06460326440d85930b3cea8709fff5
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-04-21 10:43:07 +0200

    update fcread.h

--- silent-hill-2/src/Multi_thr/filesys/fcread.h
+++ silent-hill-2/src/Multi_thr/filesys/fcread.h
@@ -1,4 +1,9 @@
 #ifndef FCREAD_H
 #define FCREAD_H
 
+#include "common.h"
+#include "FilesList/fileslist_bg.h" // correct?
+
+int FcRead(union fsFileIndex* id /* r2 */, void* databuf /* r17 */);
+
 #endif // FCREAD_H
