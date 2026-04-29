2a5463afb8b1f9e3fba0d4b79d18e82d655de126
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-04-29 16:34:42 +0200

    update shtskman.h

--- silent-hill-2/src/taskman/shtskman.h
+++ silent-hill-2/src/taskman/shtskman.h
@@ -7,23 +7,23 @@
 typedef struct _shTskEXECUTE {
     // Members
     void (* adrs)(void *); // offset 0x0, size 0x4
-    void * next; // offset 0x4, size 0x4
-    void * prev; // offset 0x8, size 0x4
+    void* next; // offset 0x4, size 0x4
+    void* prev; // offset 0x8, size 0x4
     int mode; // offset 0xC, size 0x4
     int atr; // offset 0x10, size 0x4
-} _shTskEXECUTE;
+} shTskEXECUTE;
 
 // total size: 0x200
 typedef struct _shTskTASK {
     // Members
-    struct _shTskEXECUTE exe; // offset 0x0, size 0x14
+    shTskEXECUTE exe; // offset 0x0, size 0x14
     u_char freemem[492]; // offset 0x14, size 0x1EC
-} _shTskTASK;
+} shTskTASK;
 
-struct _shTskTASK * shTSKSetTask(void (* func)(void *) /* r6 */, u_char num /* r2 */); 
-void shTSKDelTask(struct _shTskTASK* task);
-struct _shTskTASK* shTSKSearchTaskWithAtr(int atr, struct _shTskTASK* start, u_char num);
+shTskTASK* shTSKSetTask(void (* func)(void *) /* r6 */, u_char num /* r2 */); 
+void shTSKDelTask(shTskTASK* task);
+shTskTASK* shTSKSearchTaskWithAtr(int atr, shTskTASK* start, u_char num);
 
-struct _shTskTASK * shTskTaskListTop[8];
+shTskTASK* shTskTaskListTop[8];
 
 #endif // SHTSKMAN_H
