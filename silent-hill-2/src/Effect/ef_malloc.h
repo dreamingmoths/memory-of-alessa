f374f6460046b9c1194408c0bbfe915714d3a327
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-04-29 16:20:33 +0200

    update ef_malloc.h

--- silent-hill-2/src/Effect/ef_malloc.h
+++ silent-hill-2/src/Effect/ef_malloc.h
@@ -8,6 +8,6 @@ void* EfctInitHeap(void* buf /* r16 */, u_int size /* r2 */);
 void* EfctMalloc(u_int n /* r17 */);
 void EfctFree(void* obj);
 
-extern utilHeapCtrl * EfctCtrl;
+extern utilHeapCtrl* EfctCtrl;
 
 #endif // EF_MALLOC_H
