b53938f4eb0f2d0869e95c513bc2997c43646fa3
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-04-29 16:29:13 +0200

    update ef_stage.h

--- silent-hill-2/src/Effect/ef_stage.h
+++ silent-hill-2/src/Effect/ef_stage.h
@@ -1,4 +1,9 @@
 #ifndef EF_STAGE_H
 #define EF_STAGE_H
 
+#include "common.h"
+#include "Effect/ef_common.h"
+
+int InitEffectObjectGunFire(EFCTObject* pObj /* r18 */, int nIndex /* r2 */, float* Pos /* r2 */, float* vec /* r17 */, int wep_kind /* r19 */, u_char light_status /* r16 */);
+
 #endif // EF_STAGE_H
