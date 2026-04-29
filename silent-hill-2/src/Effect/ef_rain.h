8f24434eaaf3e829b96396f358deec05cad597c0
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-04-29 16:22:49 +0200

    update ef_rain.h

--- silent-hill-2/src/Effect/ef_rain.h
+++ silent-hill-2/src/Effect/ef_rain.h
@@ -11,26 +11,26 @@ typedef struct _EF_RAINDROP_DATA {
     int pad[2]; // offset 0x4, size 0x8
     float p[15][4]__attribute__((aligned(64))); // offset 0x40, size 0xF0
     float v[4][4]__attribute__((aligned(16))); // offset 0x130, size 0x40
-} _EF_RAINDROP_DATA;
+} EF_RAINDROP_DATA;
 
 // total size: 0x30
 typedef struct _EF_RAIN_LINE {
     // Members
     float v[2][4]__attribute__((aligned(16))); // offset 0x0, size 0x20
     u_char rgba[4]; // offset 0x20, size 0x4
-} _EF_RAIN_LINE;
+} EF_RAIN_LINE;
 
 // total size: 0x240
 typedef struct _EF_EF_RAINDROP_TASK {
     // Members
-    _shTskEXECUTE exe; // offset 0x0, size 0x14
-    _EF_RAINDROP_DATA data; // offset 0x40, size 0x180
+    shTskEXECUTE exe; // offset 0x0, size 0x14
+    EF_RAINDROP_DATA data; // offset 0x40, size 0x180
     u_char freemem[108]; // offset 0x1C0, size 0x6C
-} _EF_EF_RAINDROP_TASK;
+} EF_EF_RAINDROP_TASK;
 
 int EFCTSetRainDrop(int lev /* r17 */);
 void EFCTDelRainDrop(void);
-void EFCTRainDropMain(_shTskTASK * ptr /* r18 */);
-void efRainDropDrawLINE(_EF_RAIN_LINE* p);
+void EFCTRainDropMain(shTskTASK* ptr /* r18 */);
+void efRainDropDrawLINE(EF_RAIN_LINE* p);
 
 #endif // EF_RAIN_H
