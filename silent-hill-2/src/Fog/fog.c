4a12a7828f74335e0433e6e7b0eea2b0bd289dba
moth dreams <mothdreams666@gmail.com>
2026-04-30 21:40:55 -0700

    sh2: move `Fog/fog` to c

---
+++ silent-hill-2/src/Fog/fog.c
@@ -0,0 +1,89 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/Fog/fog", fogInit);
+
+INCLUDE_ASM("asm/nonmatchings/Fog/fog", fog_set_defpacket);
+
+INCLUDE_ASM("asm/nonmatchings/Fog/fog", fogSetEnvironment);
+
+INCLUDE_ASM("asm/nonmatchings/Fog/fog", fogInitScreen);
+
+INCLUDE_ASM("asm/nonmatchings/Fog/fog", fogInitWind);
+
+INCLUDE_ASM("asm/nonmatchings/Fog/fog", fogChangeWind);
+
+INCLUDE_ASM("asm/nonmatchings/Fog/fog", fogInitParticle);
+
+INCLUDE_ASM("asm/nonmatchings/Fog/fog", fog_init_part_sub);
+
+INCLUDE_ASM("asm/nonmatchings/Fog/fog", fog_part_newpos);
+
+INCLUDE_ASM("asm/nonmatchings/Fog/fog", fogResetWall);
+
+INCLUDE_ASM("asm/nonmatchings/Fog/fog", fogSetWall);
+
+INCLUDE_ASM("asm/nonmatchings/Fog/fog", fogResetObj);
+
+INCLUDE_ASM("asm/nonmatchings/Fog/fog", fogSetObj);
+
+INCLUDE_ASM("asm/nonmatchings/Fog/fog", fogMoveObj);
+
+INCLUDE_ASM("asm/nonmatchings/Fog/fog", fogSetObj2);
+
+INCLUDE_ASM("asm/nonmatchings/Fog/fog", fogEraseObj);
+
+INCLUDE_ASM("asm/nonmatchings/Fog/fog", fogSetObjSize);
+
+INCLUDE_ASM("asm/nonmatchings/Fog/fog", fogGetObj);
+
+INCLUDE_ASM("asm/nonmatchings/Fog/fog", fogMoveParticle);
+
+INCLUDE_ASM("asm/nonmatchings/Fog/fog", fog_load_objdata);
+
+INCLUDE_ASM("asm/nonmatchings/Fog/fog", fog_set_stay);
+
+INCLUDE_ASM("asm/nonmatchings/Fog/fog", fog_part_wall);
+
+INCLUDE_ASM("asm/nonmatchings/Fog/fog", fog_part_grid);
+
+INCLUDE_ASM("asm/nonmatchings/Fog/fog", fog_part_obj);
+
+INCLUDE_ASM("asm/nonmatchings/Fog/fog", fog_part_obj2);
+
+INCLUDE_ASM("asm/nonmatchings/Fog/fog", fog_part_alp);
+
+INCLUDE_ASM("asm/nonmatchings/Fog/fog", fog_part_clamp);
+
+INCLUDE_ASM("asm/nonmatchings/Fog/fog", fogMakePacket);
+
+INCLUDE_ASM("asm/nonmatchings/Fog/fog", fog_view_screen_fog);
+
+INCLUDE_ASM("asm/nonmatchings/Fog/fog", fogSetColor);
+
+INCLUDE_ASM("asm/nonmatchings/Fog/fog", fogSetPartNum);
+
+INCLUDE_ASM("asm/nonmatchings/Fog/fog", fogSetProjection);
+
+INCLUDE_ASM("asm/nonmatchings/Fog/fog", fogSetFloorY);
+
+INCLUDE_ASM("asm/nonmatchings/Fog/fog", fogSetWorldScreenM);
+
+INCLUDE_ASM("asm/nonmatchings/Fog/fog", fogSetWorldViewM);
+
+INCLUDE_ASM("asm/nonmatchings/Fog/fog", fogSetWorldPosV);
+
+INCLUDE_ASM("asm/nonmatchings/Fog/fog", fogSetStayPos);
+
+INCLUDE_ASM("asm/nonmatchings/Fog/fog", fogResetStayPos);
+
+INCLUDE_ASM("asm/nonmatchings/Fog/fog", fogSetStayPoint);
+
+INCLUDE_ASM("asm/nonmatchings/Fog/fog", fogResetStayPoint);
+
+INCLUDE_ASM("asm/nonmatchings/Fog/fog", fogSetCameraPosV);
+
+INCLUDE_ASM("asm/nonmatchings/Fog/fog", fogSetLocalPosV);
+
+INCLUDE_ASM("asm/nonmatchings/Fog/fog", fogSetSpeedLevel);
+
+INCLUDE_ASM("asm/nonmatchings/Fog/fog", fogTex0Adr);
