e9fd5ceafac26334628197b4f7ad38f48f1bcdf7
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-05-03 10:48:37 +0200

    update sifdev.h

--- include/sifdev.h
+++ include/sifdev.h
@@ -4,5 +4,7 @@
 int sceFsReset(void);
 void sceSifLoadFileReset(void);
 int sceSifSyncIop(void);
+int sceSifLoadModule(const char* filename, int args, const char* argp);
+int sceSifRebootIop(const char* img);
 
 #endif // SIFDEV_H
