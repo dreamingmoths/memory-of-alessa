a9e941f8aef9a13ee9584da7dbabfb1582c6d7f7
moth dreams <mothdreams666@gmail.com>
2026-05-02 23:16:43 -0700

    sh2+3: move `ps2_scf` to c

---
+++ silent-hill-2/src/Multi_thr/scf/ps2_scf.c
@@ -0,0 +1,19 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/Multi_thr/scf/ps2_scf", ps2ScfInit);
+
+INCLUDE_ASM("asm/nonmatchings/Multi_thr/scf/ps2_scf", ps2ScfGetLanguage);
+
+INCLUDE_ASM("asm/nonmatchings/Multi_thr/scf/ps2_scf", ps2ScfGetAspect);
+
+INCLUDE_ASM("asm/nonmatchings/Multi_thr/scf/ps2_scf", ps2ScfGetSpdif);
+
+INCLUDE_ASM("asm/nonmatchings/Multi_thr/scf/ps2_scf", ps2ScfGetTimeZone);
+
+INCLUDE_ASM("asm/nonmatchings/Multi_thr/scf/ps2_scf", ps2ScfGetDateNotation);
+
+INCLUDE_ASM("asm/nonmatchings/Multi_thr/scf/ps2_scf", ps2ScfGetSummerTime);
+
+INCLUDE_ASM("asm/nonmatchings/Multi_thr/scf/ps2_scf", ps2ScfGetTimeNotation);
+
+INCLUDE_ASM("asm/nonmatchings/Multi_thr/scf/ps2_scf", ps2ScfGetLocalTimefromRTC);
---
+++ silent-hill-3/src/Multi_thr/scf/ps2_scf.c
@@ -0,0 +1,21 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/Multi_thr/scf/ps2_scf", func_0020EAD0);
+
+INCLUDE_ASM("asm/nonmatchings/Multi_thr/scf/ps2_scf", func_0020EB20);
+
+INCLUDE_ASM("asm/nonmatchings/Multi_thr/scf/ps2_scf", func_0020EB70);
+
+INCLUDE_ASM("asm/nonmatchings/Multi_thr/scf/ps2_scf", func_0020EBC0);
+
+INCLUDE_ASM("asm/nonmatchings/Multi_thr/scf/ps2_scf", func_0020EC10);
+
+INCLUDE_ASM("asm/nonmatchings/Multi_thr/scf/ps2_scf", func_0020EC50);
+
+INCLUDE_ASM("asm/nonmatchings/Multi_thr/scf/ps2_scf", func_0020ECA0);
+
+INCLUDE_ASM("asm/nonmatchings/Multi_thr/scf/ps2_scf", func_0020ECF0);
+
+INCLUDE_ASM("asm/nonmatchings/Multi_thr/scf/ps2_scf", func_0020ED40);
+
+INCLUDE_ASM("asm/nonmatchings/Multi_thr/scf/ps2_scf", func_0020ED90);
