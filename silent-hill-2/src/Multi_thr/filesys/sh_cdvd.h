940e6b4792b9cfe410ddcf486a4dadfe4027dd23
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-05-04 12:10:24 +0200

    update sh_cdvd.h

--- silent-hill-2/src/Multi_thr/filesys/sh_cdvd.h
+++ silent-hill-2/src/Multi_thr/filesys/sh_cdvd.h
@@ -29,6 +29,16 @@ typedef struct Old_Rtc {
     u_char year; // offset 0x7, size 0x1
 } Old_Rtc;
 
+// total size: 0x14
+typedef struct shHdWorkT {
+    // Members
+    volatile int hd_sid; // offset 0x0, size 0x4
+    int last_fd; // offset 0x4, size 0x4
+    char* last_filename; // offset 0x8, size 0x4
+    int last_offset; // offset 0xC, size 0x4
+    int read_size; // offset 0x10, size 0x4
+} shHdWorkT;
+
 int shCdInit(int initmode /* r16 */, int mmode /* r17 */);
 int shCdMmode(int mmode /* r16 */);
 int shCdInitR(int initmode /* r17 */, int mmode /* r16 */);
@@ -47,10 +57,26 @@ int shCdReadClock(Old_Rtc* rtc /* r17 */);
 int shCdSearchFile(sceCdlFILE* file /* r2 */, char* fullpath /* r2 */);
 int ___shCdSearchFile(sceCdlFILE* fp /* r17 */, char* name /* r16 */);
 int shCdTrayReq(int mode /* r17 */, u_int* traycnt /* r16 */);
+int shCdReadW(int lsn /* r21 */, int sectors /* r20 */, void* buf /* r19 */, sceCdRMode* mode /* r18 */);
+int shCdSeekW(int lsn /* r18 */);
+int shCdDiskReady(int mode /* r16 */);
+int shCdGetDiskType(void);
+int shCdGetError(void);
+int shCdStatus(void);
+int shCdSync(int mode /* r16 */);
+void shCdSdStart(void);
+void shCdSdEnd(void);
+int shHdInit(void);
+int shHdOpen(char* name /* r17 */, int mode /* r16 */);
+int shHdRead(int fd /* r18 */, void* buffer /* r17 */, int size /* r16 */);
+int shHdClose(int fd /* r16 */);
+int shHdLseek(int fd /* r18 */, int offset /* r17 */, int where /* r16 */);
+int ___shHdGetFileSize(char* name /* r16 */);
 
 extern shCdWorkT shCdWork; // size: 0x10, address: 0x33B3D0
 extern int exec_cnt; // size: 0x4, address: 0x1159248
 extern int cmd_cnt; // size: 0x4, address: 0x1159240
 extern Old_Rtc old_rtc; // size: 0x8, address: 0x1159238
+extern shHdWorkT shHdWork;
 
 #endif // SH_CDVD_H
