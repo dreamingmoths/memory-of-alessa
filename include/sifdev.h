820893d6a75972d25105acbb2df25a5f0a732b09
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-05-04 12:09:10 +0200

    update sifdev.h

--- include/sifdev.h
+++ include/sifdev.h
@@ -1,6 +1,11 @@
 #ifndef SIFDEV_H
 #define SIFDEV_H
 
+extern int sceOpen(const char* filename, int flag, ...);
+extern int sceClose(int fd);
+extern int sceRead(int fd, void* buf, int nbyte); 
+extern int sceLseek(int fd, int offset, int where); // I am putting these as extern cause this is how they are put in the sdk
+
 int sceFsReset(void);
 void sceSifLoadFileReset(void);
 int sceSifSyncIop(void);
