7a76ac09f50656176f5156965e346159b2484dab
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-05-07 10:16:11 +0200

    add fileserv.c

---
+++ silent-hill-2/src/Multi_thr/filesys/fileserv.c
@@ -0,0 +1,73 @@
+#include "Multi_thr/filesys/fileserv.h"
+#include "Multi_thr/intc/syncv.h"
+#include "Multi_thr/filesys/filecmd.h"
+
+static void checkReadAlign(void* buffer /* r2 */);
+
+static u_long128* fsCmdWork;
+
+int fsInit(int th_prio /* r11 */, void* stack /* r10 */, int stackSize /* r9 */, void* queue /* r16 */, int queueSize /* r2 */) {
+    int ret; // r2
+
+    ret = 0;
+    if (!fsCmdWork) {
+        ret = CmdServInit(queue, queueSize, stack, stackSize, th_prio);
+        if (ret) {
+            fsCmdWork = queue;
+        }
+    }
+    return ret;
+}
+
+int fsSync(int mode /* r17 */, int fid /* r16 */) {
+    int ret; // r2 
+    
+    if ((mode == 0) && (shSyncVEnd(1) < 0xA)) {
+        shSyncVEnd(0);
+    }
+    ret = CmdQueueSync(fsCmdWork, mode, fid);
+    return ret;
+}
+
+int fsGetStat(CmdServStat* stat /* r2 */) {
+    return CmdServGetStat(fsCmdWork, stat);
+}
+
+int fsGetTrayStat(void) {
+    return fsCmdGetTrayStat();
+}
+
+INCLUDE_ASM("asm/nonmatchings/Multi_thr/filesys/fileserv", fcSifInit);
+
+INCLUDE_ASM("asm/nonmatchings/Multi_thr/filesys/fileserv", fcCdInitW);
+
+INCLUDE_ASM("asm/nonmatchings/Multi_thr/filesys/fileserv", fcIopLoadMod);
+
+INCLUDE_ASM("asm/nonmatchings/Multi_thr/filesys/fileserv", fcDiskSelectC);
+
+INCLUDE_ASM("asm/nonmatchings/Multi_thr/filesys/fileserv", fcDiskSelectCH);
+
+INCLUDE_ASM("asm/nonmatchings/Multi_thr/filesys/fileserv", fcDiskSelectHC);
+
+INCLUDE_ASM("asm/nonmatchings/Multi_thr/filesys/fileserv", fcHdInit);
+
+INCLUDE_ASM("asm/nonmatchings/Multi_thr/filesys/fileserv", fcDiskSelect);
+
+INCLUDE_ASM("asm/nonmatchings/Multi_thr/filesys/fileserv", fcExecDevSelect);
+
+INCLUDE_ASM("asm/nonmatchings/Multi_thr/filesys/fileserv", fcSetParamForCheckDisk);
+
+INCLUDE_ASM("asm/nonmatchings/Multi_thr/filesys/fileserv", fcCdCheckDisk);
+
+static void checkReadAlign(void* buffer /* r2 */) {
+    if ((int)buffer & 0x3F) {
+        printf("fileserv.c:493> buffer alignment error! 0x%08x\n", buffer);
+        while(1);
+    }
+}
+
+INCLUDE_ASM("asm/nonmatchings/Multi_thr/filesys/fileserv", fcRead);
+
+INCLUDE_ASM("asm/nonmatchings/Multi_thr/filesys/fileserv", fcReadPart);
+
+INCLUDE_ASM("asm/nonmatchings/Multi_thr/filesys/fileserv", fcFixFile);
