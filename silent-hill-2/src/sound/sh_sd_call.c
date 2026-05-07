fa46515fa361a4acec8bddec1fc7cbd72e7fd036
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-05-07 05:15:36 +0200

    sh2: match sound/sh_sd_call (#135)
    * match sh_sd_call
    
    * update sh_sd_call.h
    
    * update symbol

--- silent-hill-2/src/sound/sh_sd_call.c
+++ silent-hill-2/src/sound/sh_sd_call.c
@@ -1,37 +1,180 @@
 #include "sound/sh_sd_call.h"
+#include "eekernel.h"
+#include "Multi_thr/filesys/sh_cdvd.h"
+#include "DBG/dbflag.h"
 
+static int shSdWaitExcl(void);
+static int shSdSignalExcl(void);
+static int sd_stat_now_cd_using(void);
 static int shSdCallCheck(int i0 /* r16 */);
+static int shSdCallCheck(int i0);
 
-INCLUDE_ASM("asm/nonmatchings/sound/sh_sd_call", shSdWaitExcl);
+extern /* static */ int excl_sid; // size: 0x4, address: 0x37B8D8
+extern /* static */ int cd_sd_now_access; // size: 0x4, address: 0x122B308
 
-INCLUDE_ASM("asm/nonmatchings/sound/sh_sd_call", shSdSignalExcl);
+static int shSdWaitExcl(void) {
+    return WaitSema(excl_sid);
+}
 
-INCLUDE_ASM("asm/nonmatchings/sound/sh_sd_call", shSdInit);
+static int shSdSignalExcl(void) {
+    return SignalSema(excl_sid);
+}
 
-INCLUDE_ASM("asm/nonmatchings/sound/sh_sd_call", sd_stat_now_cd_using);
+int shSdInit(void) {
+    if (excl_sid == -1) {
+        excl_sid = CreateSema2(0, 1, 0);
+    }
+    shSdSignalExcl();
+    return 1;
+}
 
-INCLUDE_ASM("asm/nonmatchings/sound/sh_sd_call", shSdVSync);
+static int sd_stat_now_cd_using(void) {
+    int stat; // r2
+    int ret; // r4
+    
+    stat = sd_stat();
+    ret = (stat & 0xF) != 0;
+    ret |= ((1 << ((stat >> 4) & 0xF)) & ~0x21) != 0;
+    return ret;
+}
 
-INCLUDE_ASM("asm/nonmatchings/sound/sh_sd_call", shSdCallCheck);
+void shSdVSync(void) {
+    if (!dbFlag(1)) {
+        shSdWaitExcl();
+        if ((cd_sd_now_access == 0) && (sd_stat_now_cd_using() != 0)) {
+            cd_sd_now_access = 1;
+            shCdSdStart();
+        }
+        sd_vsync();
+        if ((cd_sd_now_access != 0) && (sd_stat_now_cd_using() == 0)) {
+            shCdSdEnd();
+            cd_sd_now_access = 0;
+        }
+        sd_stat();
+        shSdSignalExcl();
+    }
+}
 
-INCLUDE_ASM("asm/nonmatchings/sound/sh_sd_call", shSdCall);
+static int shSdCallCheck(int i0) {
+    if (dbFlag(1)) {
+        return 0;
+    }
+    switch (i0) {
+        case 0x5DC:
+            if (!dbFlag(2)) {
+                return 0;
+            }
+            break;        
+        default:
+            if ((i0 < 0x1388 || 0x13D6 < i0) && (i0 < 0xEA60 || 0xEAB6 < i0)) {
+                if (i0 >= 0x1770 && i0 < 0x17B0) {
+                    if (!dbFlag(2)) {
+                        return 0;
+                    }
+                } 
+                else if (i0 >= 0xF618) {
+                    if (!dbFlag(2)) {
+                        return 0;
+                    }
+                }
+            }
+    }    
+    return 1;
+}
 
-INCLUDE_ASM("asm/nonmatchings/sound/sh_sd_call", shSdSeStop);
+int shSdCall(int i0 /* r19 */, int i1 /* r18 */, int i2 /* r17 */, int i3 /* r16 */) {
+    int ret; // r16
+    if (!shSdCallCheck(i0)) return 0;
+    shSdWaitExcl();
+    ret = sd_call(i0, i1, i2, i3);
+    shSdSignalExcl();
+    return ret;
+}
 
-INCLUDE_ASM("asm/nonmatchings/sound/sh_sd_call", shSdStat);
+int shSdSeStop(int i0 /* r16 */) {
+    int ret; // r16
+    if (!shSdCallCheck(i0)) return 0;    
+    shSdWaitExcl();
+    ret = sd_se_stop(i0);
+    shSdSignalExcl();
+    return ret;
+}
 
-INCLUDE_ASM("asm/nonmatchings/sound/sh_sd_call", shSd3dCall);
 
-INCLUDE_ASM("asm/nonmatchings/sound/sh_sd_call", shSd3dMove);
+int shSdStat(void) {
+    int ret; // r16
+    if (!shSdCallCheck(0)) return 0;    
+    shSdWaitExcl();
+    ret = sd_stat();
+    shSdSignalExcl();
+    return ret;
+}
 
-INCLUDE_ASM("asm/nonmatchings/sound/sh_sd_call", shSd3dStop);
+int shSd3dCall(int i0 /* r18 */, int i1 /* r17 */, int i2 /* r16 */, float f0 /* r21 */, float f1 /* r20 */) {
+    int ret; // r16
+    if (!shSdCallCheck(0)) return 0;
+    shSdWaitExcl();
+    ret = sd_3d_call(i0, i1, i2, f0, f1);
+    shSdSignalExcl();
+    return ret;
+}
 
-INCLUDE_ASM("asm/nonmatchings/sound/sh_sd_call", shSd3dAllStop);
+int shSd3dMove(int i0 /* r18 */, int i1 /* r17 */, int i2 /* r16 */, float f0 /* r21 */, float f1 /* r20 */) {
+    int ret; // r16
+    if (!shSdCallCheck(0)) return 0;
+    shSdWaitExcl();
+    ret = sd_3d_move(i0, i1, i2, f0, f1);
+    shSdSignalExcl();
+    return ret;
+}
 
-INCLUDE_ASM("asm/nonmatchings/sound/sh_sd_call", shSdSeChange);
+void shSd3dStop(int i0 /* r16 */) {
+    if (shSdCallCheck(0)) {
+        shSdWaitExcl();
+        sd_3d_stop(i0);
+        shSdSignalExcl();
+    }
+}
 
-INCLUDE_ASM("asm/nonmatchings/sound/sh_sd_call", shSdTrack);
+void shSd3dAllStop(void) {
+    if (shSdCallCheck(0)) {
+        shSdWaitExcl();
+        sd_3d_allstop();
+        shSdSignalExcl();
+    }
+}
 
-INCLUDE_ASM("asm/nonmatchings/sound/sh_sd_call", shSdRadio);
+int shSdSeChange(int i0 /* r19 */, int i1 /* r18 */, int i2 /* r17 */, int i3 /* r16 */) {
+    int ret; // r16
+    if (shSdCallCheck(0) == 0) return 0;    
+    shSdWaitExcl();
+    ret = sd_se_change(i0, i1, i2, i3);
+    shSdSignalExcl();
+    return ret;
+}
 
-INCLUDE_ASM("asm/nonmatchings/sound/sh_sd_call", shSd3dAdrs);
+int shSdTrack(int i0 /* r17 */, int i1 /* r16 */) {
+    int ret; // r16
+    if (!shSdCallCheck(0)) return 0;
+    shSdWaitExcl();
+    ret = sd_track(i0, i1);
+    shSdSignalExcl();
+    return ret;
+}
+
+int shSdRadio(int i0 /* r19 */, int i1 /* r18 */, int i2 /* r17 */, int i3 /* r16 */) {
+    int ret; // r16
+    if (!shSdCallCheck(0)) return 0;
+    shSdWaitExcl();
+    ret = sd_radio(i0, i1, i2, i3);
+    shSdSignalExcl();
+    return ret;
+}
+
+void* shSd3dAdrs(void) {
+    void* ret; // r16
+    shSdWaitExcl();
+    ret = sd_3d_adrs();
+    shSdSignalExcl();
+    return ret;
+}
--- silent-hill-2/src/sound/sh_sd_call.h
+++ silent-hill-2/src/sound/sh_sd_call.h
@@ -3,12 +3,59 @@
 
 #include "common.h"
 
+// @todo: find a home for these
+
+// M:\select\sound\sd0712\ee\sd_call.c
+extern int sd_stat(void);
+
+// M:\select\sound\sd0712\ee\sd_call.c
+extern void sd_vsync(void);
+
+// M:\select\sound\sd0712\ee\sd_call.c
+extern int sd_call(int call /* r19 */, int direct /* r18 */, int dist /* r17 */, int pitch /* r16 */);
+
+// M:\select\sound\sd0712\ee\sd_call.c
+extern int sd_se_stop(int call /* r2 */);
+
+// M:\select\sound\sd0712\ee\sd_call.c
+extern int sd_stat(void);
+
+// M:\select\sound\sd0712\ee\sd_call.c
+extern int sd_3d_call(int call /* r2 */, int arg1 /* r2 */, int arg2 /* r2 */, float arg3 /* r29+0x10 */, float arg4 /* r29+0x10 */);
+
+// M:\select\sound\sd0712\ee\sd_call.c
+extern void* sd_3d_adrs(void);
+
+// M:\select\sound\sd0712\ee\sd_call.c
+extern void sd_3d_allstop(void);
+
+// M:\select\sound\sd0712\ee\sd_call.c
+extern int sd_3d_move(int no /* r2 */, int arg1 /* r2 */, int arg2 /* r2 */, float arg3 /* r29 */, float arg4); // arg4 was not present in the dwarf
+
+// M:\select\sound\sd0712\ee\sd_call.c
+extern void sd_3d_stop(int no /* r2 */);
+
+// M:\select\sound\sd0712\ee\sd_call.c
+extern int sd_radio(int ch /* r2 */, int vol /* r2 */, int pan /* r2 */, int pitch /* r2 */);
+
+// M:\select\sound\sd0712\ee\sd_call.c
+extern int sd_se_change(int call /* r2 */, int direct /* r2 */, int dist /* r2 */, int pitch /* r2 */);
+
+// M:\select\sound\sd0712\ee\sd_call.c
+extern int sd_track(int track /* r2 */, int vol /* r2 */);
+
 int shSdInit(void);
-int shSdStat(void);
-int shSdCall(int i0 /* r19 */, int i1 /* r18 */, int i2 /* r17 */, int i3 /* r16 */);
-void shSd3dAllStop(void);
-void shSd3dStop(int i0 /* r16 */);
-int shSdSeStop(int i0 /* r16 */);
 void shSdVSync(void);
+int shSdCall(int i0 /* r19 */, int i1 /* r18 */, int i2 /* r17 */, int i3 /* r16 */);
+int shSdSeStop(int i0 /* r16 */);
+int shSdStat(void);
+int shSd3dCall(int i0 /* r18 */, int i1 /* r17 */, int i2 /* r16 */, float f0 /* r21 */, float f1 /* r20 */);
+int shSd3dMove(int i0 /* r18 */, int i1 /* r17 */, int i2 /* r16 */, float f0 /* r21 */, float f1 /* r20 */);
+void shSd3dStop(int i0 /* r16 */);
+void shSd3dAllStop(void);
+int shSdSeChange(int i0 /* r19 */, int i1 /* r18 */, int i2 /* r17 */, int i3 /* r16 */);
+int shSdTrack(int i0 /* r17 */, int i1 /* r16 */);
+int shSdRadio(int i0 /* r19 */, int i1 /* r18 */, int i2 /* r17 */, int i3 /* r16 */);
+void* shSd3dAdrs(void);
 
 #endif // SH_SD_CALL_H
