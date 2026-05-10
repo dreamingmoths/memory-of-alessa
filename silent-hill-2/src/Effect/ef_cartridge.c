3ccaa8a4d13d0a5aaa2376f18173111342180a1a
moth dreams <mothdreams666@gmail.com>
2026-05-10 00:35:18 -0700

    sh2: move `ef_cartridge` to c

---
+++ silent-hill-2/src/Effect/ef_cartridge.c
@@ -0,0 +1,15 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/Effect/ef_cartridge", EFCTSetDischargeCartridge);
+
+INCLUDE_ASM("asm/nonmatchings/Effect/ef_cartridge", EFCTSetDischargeCartridgeEddie);
+
+INCLUDE_ASM("asm/nonmatchings/Effect/ef_cartridge", EFCTCartridgeDischarge);
+
+INCLUDE_ASM("asm/nonmatchings/Effect/ef_cartridge", efCartridgeDischargeDisp);
+
+INCLUDE_ASM("asm/nonmatchings/Effect/ef_cartridge", efCartridgeDischargeDel);
+
+INCLUDE_ASM("asm/nonmatchings/Effect/ef_cartridge", efCartridgeDrawCart);
+
+INCLUDE_ASM("asm/nonmatchings/Effect/ef_cartridge", efCartridgeDischargeDrawPolyF4);
