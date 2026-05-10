2830a1f46423dbb968b9f70eddd8019ff50e7089
moth dreams <mothdreams666@gmail.com>
2026-05-10 00:05:51 -0700

    sh3: split `m3_play_event`

---
+++ silent-hill-3/src/Chacter/m3_play_event.c
@@ -0,0 +1,27 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_play_event", func_00190050);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_play_event", func_00190070);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_play_event", func_00190220);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_play_event", func_00190240);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_play_event", func_00190280);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_play_event", func_00190290);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_play_event", func_00190410);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_play_event", PlayerEventMove);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_play_event", func_00190690);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_play_event", func_001906C0);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_play_event", func_001906F0);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_play_event", func_00190860);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_play_event", PlayerGetHeatherWeapon);
