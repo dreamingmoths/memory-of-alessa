f17b54a30544f99d565d3a775fd6cdfd53a0d0b4
moth dreams <mothdreams666@gmail.com>
2026-05-10 00:40:38 -0700

    sh3: move `hh_class_cartridge` to c

---
+++ silent-hill-3/src/Effect2/hh_class_cartridge.c
@@ -0,0 +1,16 @@
+#include "common.h"
+
+/*
+names + filepath may be wrong -- looks like the cartridge effects may have been
+moved to Effect2?
+*/
+
+INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_cartridge", HH_Class_pMain_0x18);
+
+INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_cartridge", EFCTSetDischargeCartridge);
+
+INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_cartridge", efCartridgeDischargeDisp);
+
+INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_cartridge", func_001EF5A0);
+
+INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_cartridge", func_001EFF30);
