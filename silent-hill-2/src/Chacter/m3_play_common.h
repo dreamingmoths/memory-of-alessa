871720280e3c03d4dac61741b6412b5e6b7b81dd
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-05-12 11:20:10 +0200

    update m3_play_common.h

--- silent-hill-2/src/Chacter/m3_play_common.h
+++ silent-hill-2/src/Chacter/m3_play_common.h
@@ -1,4 +1,9 @@
 #ifndef M3_PLAY_COMMON_H
 #define M3_PLAY_COMMON_H
 
+#include "common.h"
+#include "Chacter/character.h"
+
+void PlayerSpeedDownToStand(SubCharacter* p /* r16 */);
+
 #endif // M3_PLAY_COMMON_H
