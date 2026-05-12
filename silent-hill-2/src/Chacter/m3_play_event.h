928c7835673fa7a93bed24ccdcfb4d5054cb8086
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-05-12 11:16:19 +0200

    update m3_play_event.h

--- silent-hill-2/src/Chacter/m3_play_event.h
+++ silent-hill-2/src/Chacter/m3_play_event.h
@@ -2,7 +2,12 @@
 #define M3_PLAY_EVENT_H
 
 #include "common.h"
+#include "Chacter/character.h"
 
+int PlayerNowDemoEventMode(void);
+int PlayerEventButtonCheck(int button /* r2 */);
+int PlayerEventDeadAnimeFinish(void);
+int PlayerEventAnimeSuccessFrame(void);
 int PlayerGetJamesWeapon(void);
 
 #endif // M3_PLAY_EVENT_H
