34c4bbfc6ba6a2f3c0d3fc5e2a51264f3a41bea0
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-05-12 11:19:04 +0200

    update m3_sc.h

--- silent-hill-2/src/Chacter/m3_sc.h
+++ silent-hill-2/src/Chacter/m3_sc.h
@@ -11,8 +11,8 @@ struct shCharacterAll sh2chara; // size: 0x6810, address: 0x3C84D0
 void shCharacterSetPlayer(SubCharacter* scp);
 
 short shCharacterAnimeFrameGet_(SubCharacter * scp /* r2 */, u_int type /* r2 */);
-AnimeInfo* shCharacterAnimeGetInfo_(struct SubCharacter* scp /* r2 */, int ctrl_type /* r2 */);
+AnimeInfo* shCharacterAnimeGetInfo_(SubCharacter* scp /* r2 */, int ctrl_type /* r2 */);
 
-void SCNowPlayableEventSwitch(struct SubCharacter * scp /* r2 */, int flag /* r2 */);
+void SCNowPlayableEventSwitch(SubCharacter * scp /* r2 */, int flag /* r2 */);
 
 #endif
