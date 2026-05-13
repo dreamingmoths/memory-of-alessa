cf2774a8f74a30e6c174d7b9d546e4ad56e9277b
moth dreams <mothdreams666@gmail.com>
2026-05-12 22:00:35 -0700

    update m3_sc

--- silent-hill-2/src/Chacter/m3_sc.h
+++ silent-hill-2/src/Chacter/m3_sc.h
@@ -6,7 +6,7 @@
 #include "shared/Chacter_Draw/clani.h"
 
 // E:\work\sh2(CVS全取得)\src\Chacter\m3_sc.c
-struct shCharacterAll sh2chara; // size: 0x6810, address: 0x3C84D0
+extern struct shCharacterAll sh2chara; // size: 0x6810, address: 0x3C84D0
 
 void shCharacterSetPlayer(SubCharacter* scp);
 
