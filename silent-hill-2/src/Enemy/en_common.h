d295fe1d2f400bb7b12f66e37b98f3a7f01ec9fe
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-05-15 12:19:00 +0200

    update en_common.h

--- silent-hill-2/src/Enemy/en_common.h
+++ silent-hill-2/src/Enemy/en_common.h
@@ -60,6 +60,8 @@ void enSoundCall3D(int num /* r2 */, float vol /* r29+0x10 */, float* pos /* r2 
 void enSoundStop(int num /* r2 */);
 void enSoundSetQueue(struct SubCharacter* scp /* r2 */, int num /* r2 */, float vol /* r29 */, float time /* r29 */);
 
+void enEventDriven(int event /* r2 */, int id /* r2 */);
+
 extern void shQzero(void*, int);
 
 #endif
