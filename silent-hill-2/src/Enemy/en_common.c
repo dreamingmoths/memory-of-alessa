9bd62d6aad3c784b644de42426a7c37f3291fec8
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-05-15 11:23:30 +0200

    match 15 Enemy/en_common funcs

--- silent-hill-2/src/Enemy/en_common.c
+++ silent-hill-2/src/Enemy/en_common.c
@@ -2,6 +2,15 @@
 #include "shared/Fog/fog.h"
 #include "Fog/fog.h"
 #include "SH2_common/playing_info.h"
+#include "sound/sh_sound.h"
+#include "sound/sh_sd_call.h"
+#include "Event/event.h"
+#include "Event/item.h"
+#include "Event/stg_name.h"
+#include "GFW/sh2gfw_LightSet.h"
+#include "Chacter_Draw/sh2gfw_model_light.h"
+#include "Chacter/sh_character_status.h"
+#include "SH2_common/sh2dt.h"
 
 void enInitEnemy(void) {
     shQzero(&enLocalWork, sizeof(EnLOCAL_WORK));
@@ -34,13 +43,78 @@ void enDeleteEnemy(struct EnLOCAL_DATA* dp /* r2 */) {
     }
 }
 
-INCLUDE_ASM("asm/nonmatchings/Enemy/en_common", enDummyCtrl);
+void enDummyCtrl(struct EnLOCAL_DATA* dp /* r2 */) {
+    void* tmp; // r2 // nice function
+}
 
-INCLUDE_ASM("asm/nonmatchings/Enemy/en_common", enTransID);
+int enTransID(int id /* r2 */) {
+    switch (id) {
+        case 0x200:
+            return 1;
+        case 0x201:
+            return 2;
+        case 0x202:
+            return 3;
+        case 0x20C:
+            return 13;
+        case 0x20D:
+            return 14;
+        case 0x208:
+            return 4;
+        case 0x209:
+            return 5;
+        case 0x207:
+        case 0x20B:
+            return 6;
+        case 0x203:
+            return 7;
+        case 0x204:
+            return 8;
+        case 0x205:
+            return 9;
+        case 0x20A:
+            return 10;
+        case 0x206:
+            return 11;
+        case 0x421:
+            return 12;
+        default:
+            return 0;
+    }
+}
 
-INCLUDE_ASM("asm/nonmatchings/Enemy/en_common", enGetWorldCondition);
+int enGetWorldCondition(void) {
+    if (sh2gfw_Get_NightOrDay()) {
+        if (item.light_switch) {
+            return BgIsOut(stage->glb_crd) ? 2 : 3;
+        }
+        return 4;
+    }
+    return 1;
+}
 
-INCLUDE_ASM("asm/nonmatchings/Enemy/en_common", enGetPlace);
+int enGetPlace(void) {
+    u_int ret = 0;
+    int place = stage->glb_crd; // r2
+    
+    switch (place) {
+        case 9:
+            ret = 1;break;
+        case 10:
+            ret = 2;break;
+        case 11:
+            ret = 3;
+        break;
+        case 12:
+            ret = 4;
+        break;
+        case 13:
+            ret =5;
+        break;
+    }
+
+    return ret;
+}
 
 INCLUDE_ASM("asm/nonmatchings/Enemy/en_common", enGetStage);
 
@@ -48,27 +122,88 @@ int enGetMode(void) {
     return playing.battle_level;
 }
 
-INCLUDE_ASM("asm/nonmatchings/Enemy/en_common", enCheckDarkOrBright);
+int enCheckDarkOrBright(struct SubCharacter* scp /* r2 */) {
+    return sh2gfw_Check_CharaDarkOrBright(scp);
+}
 
-INCLUDE_ASM("asm/nonmatchings/Enemy/en_common", enCheckDarkOrBrightPlayer);
-
+int enCheckDarkOrBrightPlayer(void) {
+    return enCheckDarkOrBright(sh2jms.player);
+}
+ 
 INCLUDE_ASM("asm/nonmatchings/Enemy/en_common", enCheckWater);
 
-INCLUDE_ASM("asm/nonmatchings/Enemy/en_common", enSetBattleTarget);
+void enSetBattleTarget(struct EnLOCAL_DATA* dp /* r16 */, u_int type /* r2 */) {
+    dp->scp->battle.target = shBattleGetTargetHuman(dp->scp, type);
+}
 
-INCLUDE_ASM("asm/nonmatchings/Enemy/en_common", enSetHP);
+void enSetHP(struct EnLOCAL_DATA* dp /* r17 */, float hp /* r21 */, float endurance /* r20 */) {
+    struct shBattleInfo* bi = &dp->scp->battle; // r16
+    
+    if ((dp->kind == 1) || (dp->kind == 2) || (dp->kind == 6)) {
+        switch (enGetPlace()) {
+            case 2:
+                hp *= 1.5f;
+                break;
+            case 3:
+                hp *= 1.5f;
+                endurance *= 1.2f;
+                break;
+            case 4:
+            case 5:
+                hp *= 2.0f;
+                endurance *= 1.5f;
+                break;
+        }
+    }
+    bi->hp_max = hp;
+    bi->hp = hp;
+    bi->hp_rate = 100.0f;
+    dp->endurance = endurance;
+    dp->endurance_max = endurance;
+    bi->damage = 0.0f;
+}
 
-INCLUDE_ASM("asm/nonmatchings/Enemy/en_common", enReduceHP);
+float enReduceHP(struct EnLOCAL_DATA* dp /* r2 */) {
+    struct shBattleInfo* bi = &dp->scp->battle; // r16
+    
+    bi->hp -= bi->damage;
+    if (bi->hp < 0.0f) {
+        bi->hp = 0.0f;
+    }
+    dp->endurance -= bi->damage;
+    if (dp->endurance < 0.0f) {
+        dp->endurance = 0.0f;
+    }
+    bi->damage = 0.0f;
+    bi->hp_rate = (100.0f * (bi->hp / bi->hp_max));
+    return dp->endurance;
+}
 
-INCLUDE_ASM("asm/nonmatchings/Enemy/en_common", enAddHP);
+INCLUDE_ASM("asm/nonmatchings/Enemy/en_common", enAddHP); // https://decomp.me/scratch/eoWuF 93%
 
-INCLUDE_ASM("asm/nonmatchings/Enemy/en_common", enAddEnduranceDT);
+float enAddEnduranceDT(struct EnLOCAL_DATA* dp /* r17 */, float n /* r20 */) {
+    struct shBattleInfo* bi = &dp->scp->battle; // r16
+    float en; // r29+0x40
+
+    en = dp->endurance + (n * shGetDT());
+    if (en > dp->endurance_max) {
+        en = dp->endurance_max;
+    }
+    if (en > bi->hp) {
+        en = bi->hp;
+    }
+    return dp->endurance = en;
+}
 
 INCLUDE_ASM("asm/nonmatchings/Enemy/en_common", enCheckDamage);
 
 INCLUDE_ASM("asm/nonmatchings/Enemy/en_common", enCheckSpray);
 
-INCLUDE_ASM("asm/nonmatchings/Enemy/en_common", enResetDamage);
+void enResetDamage(struct EnLOCAL_DATA* dp /* r2 */) {
+    dp->scp->battle.id = 0;
+    dp->scp->battle.damage = 0.0f;
+    dp->scp->battle.shock = 0.0f;
+}
 
 INCLUDE_ASM("asm/nonmatchings/Enemy/en_common", enCheckDeath);
 
@@ -278,13 +413,29 @@ INCLUDE_ASM("asm/nonmatchings/Enemy/en_common", enSetFadeOut);
 
 INCLUDE_ASM("asm/nonmatchings/Enemy/en_common", enResetFilter);
 
-INCLUDE_ASM("asm/nonmatchings/Enemy/en_common", enSoundCall);
+void enSoundCall(int num /* r2 */, float vol /* r29+0x10 */, float* pos /* r2 */) {
+    SeCallPos(num, vol, pos, 0);
+}
 
-INCLUDE_ASM("asm/nonmatchings/Enemy/en_common", enSoundCall3D);
+void enSoundCall3D(int num /* r2 */, float vol /* r29+0x10 */, float* pos /* r2 */) {
+    SeCallPos(num, vol, pos, 1);
+}
 
-INCLUDE_ASM("asm/nonmatchings/Enemy/en_common", enSoundStop);
+void enSoundStop(int num /* r2 */) {
+    shSdSeStop(num);
+}
 
-INCLUDE_ASM("asm/nonmatchings/Enemy/en_common", enSoundSetQueue);
+void enSoundSetQueue(struct SubCharacter* scp /* r2 */, int num /* r2 */, float vol /* r29 */, float time /* r29 */) {
+    struct EnSOUND_QUEUE* que; // r2
+    
+    if (enLocalWork.SoundQueueNum < 8) {
+        que = &enLocalWork.SoundQueue[enLocalWork.SoundQueueNum++];      
+        que->scp = scp;
+        que->num = num;
+        que->vol = vol;
+        que->time = time;
+    }
+}
 
 INCLUDE_ASM("asm/nonmatchings/Enemy/en_common", enSetCommunication);
 
