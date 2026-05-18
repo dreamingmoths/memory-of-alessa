3607f6897cd06b8e6de45e9dfb9d29499d3426c5
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-05-18 10:22:14 +0200

    update hh_class_object_execute.h

--- silent-hill-2/src/Effect2/hh_class_object_execute.h
+++ silent-hill-2/src/Effect2/hh_class_object_execute.h
@@ -9,7 +9,8 @@ void HH_Class_Object_Execute(void);
 
 void HH_Class_Object_Packet_Kick(void);
 
-void HH_Effect_Object_Blood_Pool_Impact_Post(float* Location);
+// void HH_Effect_Object_Blood_Pool_Impact_Post(float* Location);
+void HH_Effect_Object_Blood_Pool_Impact_Post(float* Location /* r16 */, int idk); // second arg not present in the DWARF
 
 void HH_Effect_Object_Blood_Splash_Impact_Post(float* Location, float* Direction, u_int Scp_Address, u_int Impact_Kind);
 
