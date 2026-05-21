8282fdd715ee0c7ccdfbbaa5476dbb0e29fd66b4
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-05-21 09:49:39 +0200

    update en_red.h

--- silent-hill-2/src/Enemy/en_red.h
+++ silent-hill-2/src/Enemy/en_red.h
@@ -4,4 +4,9 @@
 #include "sh2_common.h"
 #include "Enemy/en_common.h"
 
+#define SET_DP_STATE_LV(_dp, _slv, _sslv) do { \
+    _dp->slv = _slv; \
+    _dp->sslv = _sslv; \
+} while (0)
+
 #endif // EN_RED_H
