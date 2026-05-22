ab92df40cfcb56dfb11d05cab62a42e45e149009
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-05-22 15:58:22 +0200

    update model3_equip_n.h

--- silent-hill-2/src/Chacter_Draw/model3_equip_n.h
+++ silent-hill-2/src/Chacter_Draw/model3_equip_n.h
@@ -1,4 +1,8 @@
 #ifndef MODEL3_EQUIP_N_H
 #define MODEL3_EQUIP_N_H
 
+#include "sh2_common.h"
+
+int sh2gfw_Set_JMSequip(void* scp /* r2 */, int lhand /* r2 */, int rhand /* r2 */, int spotlight /* r2 */);
+
 #endif // MODEL3_EQUIP_N_H
