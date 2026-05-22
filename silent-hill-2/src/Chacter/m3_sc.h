26127f8bf7c52bbadcc12b1f5b28790bc4e88684
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-05-22 15:59:00 +0200

    update m3_sc.h

--- silent-hill-2/src/Chacter/m3_sc.h
+++ silent-hill-2/src/Chacter/m3_sc.h
@@ -14,7 +14,15 @@ short shCharacterAnimeFrameGet_(SubCharacter* scp /* r2 */, u_int type /* r2 */)
 AnimeInfo* shCharacterAnimeGetInfo(SubCharacter* scp /* r2 */); 
 AnimeInfo* shCharacterAnimeGetInfo_(SubCharacter* scp /* r2 */, int ctrl_type /* r2 */);
 void shCharacterAnimeFrameSet(SubCharacter* scp /* r2 */, u_short frame /* r2 */);
-void shCharacterAnimeRestart(SubCharacter* scp /* r2 */); 
+void shCharacterAnimeRestart(SubCharacter* scp /* r2 */);
+
+short shCharacterGetModelID(SubCharacter* scp /* r2 */);
+void SCAnimeTypeSwitch(SubCharacter* scp /* r2 */, int flag /* r2 */);
+SubCharacter* shCharacterGetSubCharacter(u_short kind /* r2 */, short id /* r2 */);
+void shCharacterSetFunction(SubCharacter* scp /* r2 */, void (* func)(SubCharacter *) /* r2 */); 
+void* shCharacterGetAnimeAdrForPlay(SubCharacter * scp /* r2 */);
+void shCharacterAnimeSet(SubCharacter* scp /* r19 */, int ctrl_type /* r2 */, int inter_type /* r18 */, AnimeInfo* anim_info /* r17 */, int anime /* r2 */);
+void* shCharacterGetAnimeAdrForDrama(SubCharacter* scp /* r2 */);
 
 void SCNowPlayableEventSwitch(SubCharacter* scp /* r2 */, int flag /* r2 */);
 
