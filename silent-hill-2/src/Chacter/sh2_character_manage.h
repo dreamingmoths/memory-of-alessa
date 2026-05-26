fabe76b54fd8d232e5060e919772f8fe91dcb4e3
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-05-26 11:25:45 +0200

    update sh2_character_manage.h

--- silent-hill-2/src/Chacter/sh2_character_manage.h
+++ silent-hill-2/src/Chacter/sh2_character_manage.h
@@ -7,5 +7,6 @@
 int shCharacter_Manage_Delete(struct SubCharacter* scp /* r2 */, short kind /* r2 */, short id /* r2 */);
 struct SubCharacter* shCharacter_Manage_GetCharacterList(void);
 int shCharacter_Manage_SetDataAdresss(struct SubCharacter* scp /* r16 */);
+void shCharacter_Manage_SetJamesAnimeAdresss(struct SubCharacter* scp /* r2 */, u_int address /* r2 */);
 
 #endif // SH2_CHARACTER_MANAGE_H
