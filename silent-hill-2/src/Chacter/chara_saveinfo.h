4ee6f50b85391c69cd78a812d83123bcc6c0ed53
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-05-31 10:58:45 +0200

    update chara_saveinfo.h

--- silent-hill-2/src/Chacter/chara_saveinfo.h
+++ silent-hill-2/src/Chacter/chara_saveinfo.h
@@ -1,4 +1,13 @@
 #ifndef CHARA_SAVEINFO_H
 #define CHARA_SAVEINFO_H
 
+#include "common.h"
+#include "Chacter/character.h"
+
+extern struct shCharacterAll sh2chara;
+extern struct shPlayerWork sh2jms;
+
+void shCharacterSetSaveData(Character_Info* chara);
+void shCharacterExtGameData(Character_Info* chara);
+
 #endif // CHARA_SAVEINFO_H
