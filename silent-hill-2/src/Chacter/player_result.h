a95c0fdd59ce4232e57139259ac64c0a273be06d
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-06-01 00:19:33 +0200

    match player_result

--- silent-hill-2/src/Chacter/player_result.h
+++ silent-hill-2/src/Chacter/player_result.h
@@ -2,11 +2,29 @@
 #define PLAYER_RESULT_H
 
 #include "common.h"
+#include "Chacter/character.h"
 
+void GameTimerCountUp(void);
+void GameMoveDistanceCountUp(struct SubCharacter* scp, int status);
+void GameKillEnemyCountUp(u_short atk);
+void GameBoatTimerCountUp(void);
+void GameBoatMaxSpeedCheck(float spd);
 void GameItemGetCountUp(void);
+void GameJamesDamagedCountUp(float damage);
+void GameMariaDamagedCountUp(u_short atk, float damage);
+u_int GameCalcRankSaveCount(void);
+u_int GameCalcRankEndingKind(void);
+u_int GameCalcRankBattleLevel(void);
+u_int GameCalcRankRiddleLevel(void);
 u_int GameCalcRankItemGet(void);
 u_int GameCalcRankHiddenItemGet(void);
-void GameItemGetCountUp(void);
-void GameKillEnemyCountUp(u_short atk /* r2 */);
+u_int GameCalcRankKillByShot(void);
+u_int GameCalcRankKillByFight(void);
+u_int GameCalcRankClearTime(void);
+u_int GameCalcRankBoatClearTime(void);
+u_int GameCalcRankJamesDamage(void);
+u_int GameCalcRankTotal(void);
+void GameSavePreviousTotalRank(void);
+void GameSaveSprayPower(void);
 
 #endif // PLAYER_RESULT_H
