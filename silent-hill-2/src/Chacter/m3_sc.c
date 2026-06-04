a7b02d5621961374612179eb872df52513da9fa2
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-06-04 10:57:05 +0200

    match 9 Chacter/m3_sc funcs

--- silent-hill-2/src/Chacter/m3_sc.c
+++ silent-hill-2/src/Chacter/m3_sc.c
@@ -1,14 +1,24 @@
 #include "Chacter/m3_sc.h"
 #include "Chacter/m3_play.h"
+#include "Chacter/skelton.h"
+#include "Heap/sh2_ch_malloc.h"
 
+static SubCharacter* shCharacterGetFreeList(void);
 static void AddFreeList(SubCharacter* scp);
 static void shCharacterSortList(SubCharacter* scp /* r2 */);
-
+static void shCharacterTopOfList(SubCharacter* scp);
 static void shCharacterCutList(SubCharacter* scp);
 
 static void shCharacterSetHandler(SubCharacter* scp /* r16 */);
 
-INCLUDE_ASM("asm/nonmatchings/Chacter/m3_sc", shCharacterGetFreeList);
+static SubCharacter* shCharacterGetFreeList(void) {
+    SubCharacter* scp; // r2
+    scp = (SubCharacter*)sh2chara.free;
+    if (scp != NULL) {
+        sh2chara.free = (SubCharacterDisp*)scp->next;
+    }
+    return scp;
+}
 
 static void AddFreeList(SubCharacter* scp) {
     scp->next = &sh2chara.free->sc;
@@ -57,7 +67,15 @@ static void shCharacterSortList(SubCharacter* scp /* r2 */) {
     scp->next = NULL;
 }
 
-INCLUDE_ASM("asm/nonmatchings/Chacter/m3_sc", shCharacterTopOfList);
+static void shCharacterTopOfList(SubCharacter* scp) {
+    if (sh2chara.head != NULL) {
+        sh2chara.head->pre = scp;
+    }
+    scp->next = sh2chara.head;
+    scp->pre = NULL;
+    sh2chara.head = scp;
+}
+
 
 static void shCharacterCutList(SubCharacter* scp) {
     SubCharacter* pre = scp->pre;
@@ -91,7 +109,11 @@ INCLUDE_ASM("asm/nonmatchings/Chacter/m3_sc", SCAddRot);
 
 INCLUDE_ASM("asm/nonmatchings/Chacter/m3_sc", shCharacterSetClusterAnimeWork);
 
-INCLUDE_ASM("asm/nonmatchings/Chacter/m3_sc", shCharacterClusterAnimeSet);
+void shCharacterClusterAnimeSet(SubCharacter* scp, int anime) {
+    SubCharacterDisp* scp_d = (SubCharacterDisp *) scp;
+
+    ClusterAnimeSet(scp_d->cluster_anime, (void *)anime);
+}
 
 INCLUDE_ASM("asm/nonmatchings/Chacter/m3_sc", SCSetModel);
 
@@ -124,145 +146,305 @@ void shCharacterSetPlayer(SubCharacter* scp) {
 
 }
 
-INCLUDE_ASM("asm/nonmatchings/Chacter/m3_sc", shCharacterGetModelID);
+short shCharacterGetModelID(SubCharacter* scp) {
+    return scp->kind;
+}
 
-INCLUDE_ASM("asm/nonmatchings/Chacter/m3_sc", shCharacterGetSubCharacter);
+SubCharacter* shCharacterGetSubCharacter(u_short kind, short id) {
+    SubCharacter* pre = sh2chara.head; // r2
+    SubCharacter* next = pre->next; // r6
+    
+    if (pre->kind == kind) {
+        if (id == -1 || id == pre->id) 
+            return pre;        
+    }
+    while (next != NULL) {
+        if (next->kind == kind) {
+            if (id == -1 || id == next->id)
+                return next;
+        }
+        next = next->next;
+    }
+    return NULL;
+}
 
 INCLUDE_ASM("asm/nonmatchings/Chacter/m3_sc", shCharacterGetSkeltonNum);
 
-INCLUDE_ASM("asm/nonmatchings/Chacter/m3_sc", shCharacterAnimeOneFrameSize);
+int shCharacterAnimeOneFrameSize(u_short id) {
+    u_short result; // r2     
+    switch (id) {
+        case 0x100:
+        case 0x101:
+            result = 0x210;
+            break;
+        case 0x105:
+            result = 0x1D0;
+            break;
+        case 0x10B:
+            result = 0x46;
+            break;
+        case 0x106:
+            result = 0x3A0;
+            break;
+        case 0x108:
+            result = 0x32C;
+            break;
+        case 0x107:
+            result = 0x3B8;
+            break;
+        case 0x104:
+            result = 0x468;
+            break;
+        case 0x109:
+            result = 0x404;
+            break;
+        case 0x10A:
+            result = 0x3B8;
+            break;
+        case 0x10D:
+            result = 0x190;
+            break;
+        case 0x200:
+            result = 0x184;
+            break;
+        case 0x201:
+            result = 0xE4;
+            break;
+        case 0x202:
+            result = 0x11A;
+            break;
+        case 0x208:
+        case 0x20E:
+        case 0x210:
+        case 0x20F:
+            result = 0x240;
+            break;
+        case 0x207:
+        case 0x20B:
+            result = 0x184;
+            break;
+        case 0x203:
+            result = 0xCE;
+            break;
+        case 0x204:
+            result = 0x17E;
+            break;
+        case 0x205:
+            result = 0x1C4;
+            break;
+        case 0x206:
+            result = 0x23A;
+            break;
+        case 0x209:
+            result = 0x210;
+            break;
+        case 0x20A:
+            result = 0xC8;
+            break;
+        case 0x400:
+            result = 0x98;
+            break;
+        case 0x401:
+            result = 0x10E;
+            break;
+        case 0x801:
+            result = 0x22;
+            break;
+        case 0x802:
+            result = 0x22;
+            break;
+        case 0x803:
+            result = 0x22;
+            break;
+        case 0x805:
+            result = 0x22;
+            break;
+        case 0x806:
+            result = 0x22;
+            break;
+        case 0x804:
+            result = 0x22;
+            break;
+        case 0x807:
+            result = 0x22;
+            break;
+        case 0x808:
+            result = 0x22;
+            break;
+        case 0x40B:
+            result = 0x28;
+            break;
+        case 0x421:
+            result = 0x6A;
+            break;
+        case 0x408:
+            result = 0x5E;
+            break;
+        case 0x42E:
+            result = 0x16;
+            break;
+        default:
+            result = 0;
+            break;
+    }
+    return result;
+}
 
-INCLUDE_ASM("asm/nonmatchings/Chacter/m3_sc", shCharacterInitSubCharacter);
+void shCharacterInitSubCharacter(void) {
+    int i; // r5
+    SubCharacterDisp* scp_d; // r4
+
+    
+    memset(&sh2chara, 0, sizeof(shCharacterAll));
+    
+    scp_d = &sh2chara.work[0];
+    sh2chara.free = sh2chara.work;
+
+
+
+    
+    for (i = 0; i < 31; i++, scp_d++) {
+        scp_d->sc.next = (SubCharacter *)(scp_d + 1);
+    }
+    scp_d->sc.next = NULL;
+
+
+
+    
+    for (i = 0; i < 32; i++) {
+        sh2chara.work[i].sc.index = i;
+    }
+}
 
 static void shCharacterSetHandler(SubCharacter* scp /* r16 */) {
     switch (scp->kind) {
-    case 0x100:
-    case 0x101:
-        shCharacterSetPlayer(scp);
-        return;
-    case 0x120:
-    case 0x121:
-        shCharacterSetHumanRPJMSLow(scp);
-        return;
-    case 0x102:
-    case 0x103:
-        shCharacterSetHumanDJMSLow(scp);
-        return;
-    case 0x122:
-    case 0x123:
-        shCharacterSetHumanRDJMSLow(scp);
-        return;
-    case 0x104:
-        shCharacterSetHumanLAULow(scp);
-        return;
-    case 0x105:
-        shCharacterSetHumanMARLow(scp);
-        return;
-    case 0x106:
-        shCharacterSetHumanDMARLow(scp);
-        return;
-    case 0x107:
-        shCharacterSetHumanAGLLow(scp);
-        return;
-    case 0x127:
-        shCharacterSetHumanRAGLLow(scp);
-        return;
-    case 0x108:
-        shCharacterSetHumanEDILow(scp);
-        return;
-    case 0x109:
-        shCharacterSetHumanMRYLow(scp);
-        return;
-    case 0x10A:
-        shCharacterSetHumanMXXLow(scp);
-        return;
-    case 0x10B:
-        shCharacterSetHumanBOTLow(scp);
-        return;
-    case 0x10D:
-        shCharacterSetHumanINULow(scp);
-        return;
-    case 0x200:
-        shCharacterSetEnemySCULow(scp);
-        return;
-    case 0x201:
-        shCharacterSetEnemyMKNLow(scp);
-        return;
-    case 0x202:
-        shCharacterSetEnemyTYULow(scp);
-        return;
-    case 0x203:
-        shCharacterSetEnemyIKELow(scp);
-        return;
-    case 0x204:
-        shCharacterSetEnemyPAPLow(scp);
-        return;
-    case 0x205:
-        shCharacterSetEnemyEDBLow(scp);
-        return;
-    case 0x206:
-        shCharacterSetEnemyBOSLow(scp);
-        return;
-    case 0x207:
-    case 0x20B:
-        shCharacterSetEnemyNSELow(scp);
-        return;
-    case 0x208:
-        shCharacterSetEnemyREDLow(scp);
-        return;
-    case 0x209:
-        shCharacterSetEnemyONILow(scp);
-        return;
-    case 0x20A:
-        shCharacterSetEnemyARMLow(scp);
-        return;
-    case 0x20C:
-    case 0x20D:
-        shCharacterSetEnemyTY23Low(scp);
-        return;
-    default:
-        switch (scp->kind >> 0x8) {
-        case 8:
-            if (scp->kind  & 0x20) {
-                shCharacterSetWeaponRLow(scp);
-                return;
-            }
-            shCharacterSetWeaponLow(scp);
-            JamesWeaponSet(scp->kind & 0xF);
+        case 0x100:
+        case 0x101:
+            shCharacterSetPlayer(scp);
             return;
-        case 4:
-            switch (scp->kind) {
-            case 0x443:
-            case 0x444:
-                shCharacterSetRObjectLow(scp);
+        case 0x120:
+        case 0x121:
+            shCharacterSetHumanRPJMSLow(scp);
+            return;
+        case 0x102:
+        case 0x103:
+            shCharacterSetHumanDJMSLow(scp);
+            return;
+        case 0x122:
+        case 0x123:
+            shCharacterSetHumanRDJMSLow(scp);
+            return;
+        case 0x104:
+            shCharacterSetHumanLAULow(scp);
+            return;
+        case 0x105:
+            shCharacterSetHumanMARLow(scp);
+            return;
+        case 0x106:
+            shCharacterSetHumanDMARLow(scp);
+            return;
+        case 0x107:
+            shCharacterSetHumanAGLLow(scp);
+            return;
+        case 0x127:
+            shCharacterSetHumanRAGLLow(scp);
+            return;
+        case 0x108:
+            shCharacterSetHumanEDILow(scp);
+            return;
+        case 0x109:
+            shCharacterSetHumanMRYLow(scp);
+            return;
+        case 0x10A:
+            shCharacterSetHumanMXXLow(scp);
+            return;
+        case 0x10B:
+            shCharacterSetHumanBOTLow(scp);
+            return;
+        case 0x10D:
+            shCharacterSetHumanINULow(scp);
+            return;
+        case 0x200:
+            shCharacterSetEnemySCULow(scp);
+            return;
+        case 0x201:
+            shCharacterSetEnemyMKNLow(scp);
+            return;
+        case 0x202:
+            shCharacterSetEnemyTYULow(scp);
+            return;
+        case 0x203:
+            shCharacterSetEnemyIKELow(scp);
+            return;
+        case 0x204:
+            shCharacterSetEnemyPAPLow(scp);
+            return;
+        case 0x205:
+            shCharacterSetEnemyEDBLow(scp);
+            return;
+        case 0x206:
+            shCharacterSetEnemyBOSLow(scp);
+            return;
+        case 0x207:
+        case 0x20B:
+            shCharacterSetEnemyNSELow(scp);
+            return;
+        case 0x208:
+            shCharacterSetEnemyREDLow(scp);
+            return;
+        case 0x209:
+            shCharacterSetEnemyONILow(scp);
+            return;
+        case 0x20A:
+            shCharacterSetEnemyARMLow(scp);
+            return;
+        case 0x20C:
+        case 0x20D:
+            shCharacterSetEnemyTY23Low(scp);
+            return;
+        default:
+            switch (scp->kind >> 0x8) {
+            case 8:
+                if (scp->kind  & 0x20) {
+                    shCharacterSetWeaponRLow(scp);
+                    return;
+                }
+                shCharacterSetWeaponLow(scp);
+                JamesWeaponSet(scp->kind & 0xF);
                 return;
-            case 0x421:
-                shCharacterSetObjectNIKLow(scp);
+            case 4:
+                switch (scp->kind) {
+                case 0x443:
+                case 0x444:
+                    shCharacterSetRObjectLow(scp);
+                    return;
+                case 0x421:
+                    shCharacterSetObjectNIKLow(scp);
+                    return;
+                default:
+                    shCharacterSetObjectLow(scp);
+                    return;
+                }
+                break;
+            case 3:
+            case 5:
+                shCharacterSetStayObjectLow(scp);
                 return;
+            case 7:
+                shCharacterSetWorldScreenItemLow(scp);
+                return;
+            case 6:
+                shCharacterSetItemScreenItemLow(scp);
             default:
-                shCharacterSetObjectLow(scp);
                 return;
             }
             break;
-        case 3:
-        case 5:
-            shCharacterSetStayObjectLow(scp);
-            return;
-        case 7:
-            shCharacterSetWorldScreenItemLow(scp);
-            return;
-        case 6:
-            shCharacterSetItemScreenItemLow(scp);
-        default:
-            return;
-        }
-        break;
     }
 }
 
 INCLUDE_ASM("asm/nonmatchings/Chacter/m3_sc", shCharacterCreate);
 
-void shCharacterDelete(SubCharacter* scp) {
+void shCharacterDelete(SubCharacter* scp) { // but this is not matched lol
     SubCharacterDisp* scp_d = (SubCharacterDisp*) scp;
     if (scp_d == NULL) {
         return;
@@ -392,8 +574,29 @@ INCLUDE_ASM("asm/nonmatchings/Chacter/m3_sc", shCharacterMariaModelToDrama);
 
 INCLUDE_ASM("asm/nonmatchings/Chacter/m3_sc", shCharacterMariaModelToPlayable);
 
-INCLUDE_ASM("asm/nonmatchings/Chacter/m3_sc", shCharacterSetPosAfterDemo);
+void shCharacterSetPosAfterDemo(SubCharacter* scp, float* pos, float roty) {
+    scp->pos.x = scp->b_pos.x = pos[0];
+    scp->pos.y =  scp->b_pos.y = pos[1];
+    scp->pos.z = scp->b_pos.z = pos[2];
+    scp->rot.y = scp->b_rot.y = roty;
+    scp->rot.x = scp->b_rot.x = 0.0f;
+    scp->rot.z = scp->b_rot.z = 0.0f;
+
+    
+    scp->spd = scp->spd_org = 0.0f;
+}
 
 INCLUDE_ASM("asm/nonmatchings/Chacter/m3_sc", shCharacterGetPartsMatrixForShadow);
 
-INCLUDE_ASM("asm/nonmatchings/Chacter/m3_sc", shCharacterGetGroundInfoForShadow);
+void shCharacterGetGroundInfoForShadow(float* pos, float* normal, float* height, u_short kind, u_short id) {
+    SubCharacter* p; // r2
+
+    
+    p = shCharacterGetSubCharacter(kind, id);
+    if (p != NULL) {
+        
+        volatile_vec_copy(pos, &p->pos);
+        volatile_vec_copy(normal, &p->grnd_normal);
+        *height = p->grnd_height;
+    }
+}
