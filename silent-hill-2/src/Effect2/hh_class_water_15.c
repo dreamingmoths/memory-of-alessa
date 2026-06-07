c434a8a21fcbacc02c7a984529578f8fa605ff82
moth dreams <mothdreams666@gmail.com>
2026-06-07 15:17:51 -0700

    sh2: begin `Effect2/hh_class_water_15.c` 💧

---
+++ silent-hill-2/src/Effect2/hh_class_water_15.c
@@ -0,0 +1,27 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_water_15", Grid_Work_Initialize);
+
+INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_water_15", Object_Initialize);
+
+INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_water_15", CurrentPosition_AreaIndex_Calculator);
+
+INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_water_15", Specular_Calculator);
+
+INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_water_15", SpecularRGBA_Calculator);
+
+INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_water_15", Object_Draw);
+
+INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_water_15", HH_Class_Prefix_Water_15);
+
+INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_water_15", HH_Class_Suffix_Water_15);
+
+INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_water_15", HH_Class_Water_15);
+
+INCLUDE_RODATA("asm/nonmatchings/Effect2/hh_class_water_15", @257_0x0039A200);
+
+INCLUDE_RODATA("asm/nonmatchings/Effect2/hh_class_water_15", @258_0x0039A208);
+
+INCLUDE_RODATA("asm/nonmatchings/Effect2/hh_class_water_15", @259_0x0039A210);
+
+INCLUDE_RODATA("asm/nonmatchings/Effect2/hh_class_water_15", @260_0x0039A218);
