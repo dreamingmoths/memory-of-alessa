37a9f90ef5aaef65d32a1cd045595a6df8d8126f
moth dreams <mothdreams666@gmail.com>
2026-06-07 18:04:18 -0700

    sh2: begin `Effect2/hh_class_water_06.c`

---
+++ silent-hill-2/src/Effect2/hh_class_water_06.c
@@ -0,0 +1,19 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_water_06", Grid_Work_Initialize);
+
+INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_water_06", Object_Initialize);
+
+INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_water_06", CurrentPosition_AreaIndex_Calculator);
+
+INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_water_06", Specular_Calculator);
+
+INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_water_06", SpecularRGBA_Calculator);
+
+INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_water_06", Object_Draw);
+
+INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_water_06", HH_Class_Prefix_Water_06);
+
+INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_water_06", HH_Class_Suffix_Water_06);
+
+INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_water_06", HH_Class_Water_06);
