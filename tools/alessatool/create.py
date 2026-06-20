f1a85c89df9861130a6bfe7013b0660a69fb84a0
moth dreams <mothdreams666@gmail.com>
2026-06-20 15:28:11 -0700

    alessatool: generate checksum file

--- tools/alessatool/create.py
+++ tools/alessatool/create.py
@@ -2,21 +2,33 @@
 from re import sub
 from json import load
 from pathlib import Path
-from hashlib import sha1
+from hashlib import sha1, sha256
 from dataclasses import dataclass
 
 from utils import ensure_path_and_write, hex_format
 from mw_overlay_header import MwOverlayHeader
 from mw_overlay import parse_overlay_header, pretty_print_overlay_header
 
+'''
+example invocation for sh2:
+
+alessatool create \
+    --input-path rom/SLUS_202.28/overlay \
+    --output-path silent-hill-2/config/SLUS_202.28/overlay \
+    --executable-path rom/SLUS_202.28/SLUS_202.28.rom \
+    --template-yaml-path silent-hill-2/include/template.yaml \
+    --filename-mapping-path silent-hill-2/include/stages_by_overlay.json \
+    --template-symbol-addrs-path silent-hill-2/config/SLUS_202.28/stage_symbol_addrs.txt
+'''
+
 @dataclass
 class CreationArgs:
     input_path: Path
     output_path: Path
+    executable_path: Path
     template_yaml_path: Path
     filename_mapping_path: Path | None
     template_symbol_addrs_path: Path | None
-
 @dataclass
 class CreationInfo:
     name: str
@@ -56,7 +68,7 @@ def _create_from_template(info: CreationInfo) -> str:
     for (key, value) in data.items():
         result = result.replace("${{ %s }}" % key, value)
 
-    return result
+    return (result, data)
 
 def _parse_template_symbol_addrs(info: CreationInfo):
     symbol_addrs_lines = info.symbol_addrs_lines
@@ -78,9 +90,29 @@ def _parse_template_symbol_addrs(info: CreationInfo):
     
     return "\n".join(output_lines)
 
+def _create_overlay_checksum_line(args: CreationArgs, info: CreationInfo, name: str):
+    dir_name = args.output_path.parent.name
+    hex_value = sha256(info.overlay_contents).hexdigest()
+    checksum_line = f"{hex_value}  build/{dir_name}/{name}.bin"
+    return checksum_line
+
+def _create_executable_checksum_line(args: CreationArgs):
+    dir_name = args.executable_path.stem
+    name = args.executable_path.name
+
+    with open(args.executable_path, "rb") as executable_file:
+        executable_contents = executable_file.read()
+
+    hex_value = sha256(executable_contents).hexdigest()
+    checksum_line = f"{hex_value}  build/{dir_name}/{name}"
+
+    return checksum_line
+
 def create_overlay_yamls(args: CreationArgs):
     with open(args.template_yaml_path) as template_yaml_file:
         yaml_contents = template_yaml_file.read()
+
+    checksum_lines = [_create_executable_checksum_line(args)]
 
     for overlay_path in args.input_path.glob("*.bin"):
         name = overlay_path.stem
@@ -113,10 +145,12 @@ def create_overlay_yamls(args: CreationArgs):
                 map_filename=map_filename,
                 symbol_addrs_lines=symbol_addrs_lines
             )
-            result = _create_from_template(creation_info)
+            (result, data) = _create_from_template(creation_info)
         except Exception as exception:
             print(f"failed on {overlay_path}:")
             raise exception
+
+        checksum_lines.append(_create_overlay_checksum_line(args, creation_info, name))
 
         ensure_path_and_write(args.output_path / f"{name}.yaml", result)
         ensure_path_and_write(
@@ -126,3 +160,6 @@ def create_overlay_yamls(args: CreationArgs):
                 _parse_template_symbol_addrs(creation_info)
             ])
         )
+
+    ensure_path_and_write(args.output_path.parent / "checksum.sha", "\n".join(checksum_lines) + "\n")
+
