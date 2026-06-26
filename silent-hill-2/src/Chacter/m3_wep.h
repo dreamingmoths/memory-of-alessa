ed14d41372116fb2d43433fa6df467269daa135f
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-06-26 18:03:37 +0200

    update m3_wep.h

--- silent-hill-2/src/Chacter/m3_wep.h
+++ silent-hill-2/src/Chacter/m3_wep.h
@@ -2,10 +2,12 @@
 #define M3_WEP_H
 
 #include "common.h"
+#include "Chacter/character.h"
 
+void shUpdateWeaponMatrixAfterAnime(SubCharacter* this, short kind /* r16 */); // I gave the name to the first arg
+
+void shGetJamesWeaponStartPos(float* pos, float* vec);
 void shGetJamesWeaponEndPos(float* pos /* r2 */, float* vec /* r2 */);
-
-void shUpdateWeaponMatrixAfterAnime(SubCharacter* scp, short kind /* r16 */); // I gave the name to the first arg 
-
+void shCharacterSetWeaponLow(SubCharacter* scp);
 
 #endif // M3_WEP_H
