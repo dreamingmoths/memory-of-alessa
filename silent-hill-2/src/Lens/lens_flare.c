eaa8f934d1254e4282a272b4ab498c7c93f4a182
moth dreams <mothdreams666@gmail.com>
2026-06-27 13:07:52 -0700

    sh2: begin `sh2/Lens/Lens_flare` 💥

---
+++ silent-hill-2/src/Lens/lens_flare.c
@@ -0,0 +1,21 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/Lens/lens_flare", shLensFlareGetScreenInfo);
+
+INCLUDE_ASM("asm/nonmatchings/Lens/lens_flare", shLensFlareCameraIsSmooth);
+
+INCLUDE_ASM("asm/nonmatchings/Lens/lens_flare", shLensFlareLightCenterIsVisible);
+
+INCLUDE_ASM("asm/nonmatchings/Lens/lens_flare", shLensFlareSetLightSeed);
+
+INCLUDE_ASM("asm/nonmatchings/Lens/lens_flare", _sceVu0RotTransPers);
+
+INCLUDE_ASM("asm/nonmatchings/Lens/lens_flare", sh2gde_getWorldScreenMatrix);
+
+INCLUDE_ASM("asm/nonmatchings/Lens/lens_flare", shLensFlareOresenHokan);
+
+INCLUDE_ASM("asm/nonmatchings/Lens/lens_flare", shLensFlareMakeEffectTargetRate);
+
+INCLUDE_ASM("asm/nonmatchings/Lens/lens_flare", shLensFlareMakeScreenPos);
+
+INCLUDE_ASM("asm/nonmatchings/Lens/lens_flare", shLensFlareMakeScreenAngle);
