1c3801575c44d8a02c16de1863fef0f9ab98c37b
moth dreams <mothdreams666@gmail.com>
2026-06-28 15:29:52 -0700

    sh2: begin `Lens/lens_th_draw` + `kari_lf_draw` 🎨💥✨

---
+++ silent-hill-2/src/Lens/kari_lf_draw.c
@@ -0,0 +1,21 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/Lens/kari_lf_draw", kari_Thr_LFD2TextureSend);
+
+INCLUDE_ASM("asm/nonmatchings/Lens/kari_lf_draw", kari_Thr_LFD1D2SyncKick);
+
+INCLUDE_ASM("asm/nonmatchings/Lens/kari_lf_draw", shLensFlarePolyFT4AddPacketGif);
+
+INCLUDE_ASM("asm/nonmatchings/Lens/kari_lf_draw", shLensFlareDrawCommon);
+
+INCLUDE_ASM("asm/nonmatchings/Lens/kari_lf_draw", shLensFlareGetScreenInfo);
+
+INCLUDE_ASM("asm/nonmatchings/Lens/kari_lf_draw", kari_shLensFlareDraw);
+
+INCLUDE_ASM("asm/nonmatchings/Lens/kari_lf_draw", shLensFlareSetAlphaEnvironment);
+
+INCLUDE_ASM("asm/nonmatchings/Lens/kari_lf_draw", kari_shLensFlareEffect_Draw);
+
+INCLUDE_ASM("asm/nonmatchings/Lens/kari_lf_draw", shLensFlareSpriteAddPacketGif);
+
+INCLUDE_ASM("asm/nonmatchings/Lens/kari_lf_draw", Kari_LensFlare_DrawExec);
---
+++ silent-hill-2/src/Lens/lens_th_draw.c
@@ -0,0 +1,5 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/Lens/lens_th_draw", sh2gfw_GsSetDefStoreImage);
+
+INCLUDE_ASM("asm/nonmatchings/Lens/lens_th_draw", sh2gfw_GsExecStoreImage);
