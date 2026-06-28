de8f5cc5b4c12fb8b7698fc22d1aba215e293bf0
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-06-28 12:25:07 +0200

    match 3 `stage/stg_toilet.c` funcs

--- silent-hill-2/src/Event/stage/stg_toilet.c
+++ silent-hill-2/src/Event/stage/stg_toilet.c
@@ -1,13 +1,27 @@
-#include "common.h"
+#include "sh2_common.h"
+#include "Event/event.h"
+
+#include "data/daily.thu/data_movie.h"
+#include "data/daily.thu/data_demo_first_toilet.h"
+
 
 INCLUDE_ASM("asm/nonmatchings/Event/stage/stg_toilet", stg_toilet_EvProgPrologueInToilet);
 
-INCLUDE_ASM("asm/nonmatchings/Event/stage/stg_toilet", stg_toilet_EvStageInit);
+/* static */ void stg_toilet_EvStageInit(void) {
+    return;
+}
 
-INCLUDE_ASM("asm/nonmatchings/Event/stage/stg_toilet", stg_toilet_EvBgmControl);
+/* static */ int stg_toilet_EvBgmControl(void) {
+    if (GET_GAME_FLAG(GAME_FLAG_36)) {
+        return 1;
+    }
+    return !GET_GAME_FLAG(GAME_FLAG_33) ? 4 : 0;
+}
 
 INCLUDE_ASM("asm/nonmatchings/Event/stage/stg_toilet", stg_toilet_Kari_hisyakai);
 
-INCLUDE_ASM("asm/nonmatchings/Event/stage/stg_toilet", stg_toilet_LinearTrim);
+/* static */ float stg_toilet_LinearTrim(float Yen, float Yst, float Xen, float Xst, float Parm) {
+    return Yst + (((Yen - Yst) * (Parm - Xst)) / (Xen - Xst));
+}
 
 INCLUDE_ASM("asm/nonmatchings/Event/stage/stg_toilet", stg_toilet_Toilet_Dof_Filter);
