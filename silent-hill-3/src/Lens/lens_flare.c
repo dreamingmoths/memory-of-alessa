b6d8e8d69b737192703ccafc9bc72ad58362d56e
moth dreams <mothdreams666@gmail.com>
2026-06-28 00:28:00 -0700

    sh3: begin `sh3/Lens/lens_flare` 💥

---
+++ silent-hill-3/src/Lens/lens_flare.c
@@ -0,0 +1,29 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/Lens/lens_flare", shLensFlareGetScreenInfo);
+
+INCLUDE_ASM("asm/nonmatchings/Lens/lens_flare", func_0023C110);
+
+INCLUDE_ASM("asm/nonmatchings/Lens/lens_flare", shLensFlareLightCenterIsVisible);
+
+INCLUDE_ASM("asm/nonmatchings/Lens/lens_flare", shLensFlareSetLightSeed);
+
+INCLUDE_ASM("asm/nonmatchings/Lens/lens_flare", shLensFlareOresenHokan);
+
+INCLUDE_ASM("asm/nonmatchings/Lens/lens_flare", shLensFlareMakeEffectTargetRate);
+
+INCLUDE_ASM("asm/nonmatchings/Lens/lens_flare", func_0023C5A0);
+
+INCLUDE_ASM("asm/nonmatchings/Lens/lens_flare", shLensFlareMakeScreenAngle);
+
+INCLUDE_ASM("asm/nonmatchings/Lens/lens_flare", func_0023C760);
+
+INCLUDE_ASM("asm/nonmatchings/Lens/lens_flare", shLensFlareMakeScreenInfo);
+
+INCLUDE_ASM("asm/nonmatchings/Lens/lens_flare", func_0023C850);
+
+INCLUDE_ASM("asm/nonmatchings/Lens/lens_flare", func_0023C890);
+
+INCLUDE_ASM("asm/nonmatchings/Lens/lens_flare", func_0023C8B0);
+
+INCLUDE_ASM("asm/nonmatchings/Lens/lens_flare", shLensFlareExec);
