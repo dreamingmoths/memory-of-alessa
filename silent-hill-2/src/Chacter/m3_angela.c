b43b72bc8faadf51f4697fd5fd3d932893dd9305
moth dreams <mothdreams666@gmail.com>
2026-06-29 16:46:46 -0700

    sh2: begin `Chacter/m3_angela` 🔪🥀

---
+++ silent-hill-2/src/Chacter/m3_angela.c
@@ -0,0 +1,15 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_angela", HumanAGLFunction);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_angela", shCharacterSetHumanAGLLow);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_angela", shCharacterHumanAGLAnimeSet);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_angela", shCharacterHumanAGLAnimeSetP);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_angela", HumanRAGLInit);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_angela", HumanRAGLFunction);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_angela", shCharacterSetHumanRAGLLow);
