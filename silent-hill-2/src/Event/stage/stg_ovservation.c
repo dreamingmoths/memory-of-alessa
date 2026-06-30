de7b7ef2f26d4f2e6eede7cd29b143aece9e3b0c
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-06-30 13:06:47 +0200

    match `stg_ovservation_EvProgLastScene`

--- silent-hill-2/src/Event/stage/stg_ovservation.c
+++ silent-hill-2/src/Event/stage/stg_ovservation.c
@@ -18,6 +18,7 @@
 #include "Multi_thr/filesys/fcread.h"
 #include "Multi_thr/filesys/fileserv.h"
 
+#include "data/daily.thu/data_chr_mar.h"
 #include "data/daily.thu/data_demo_mar_isho_i.h"
 #include "data/daily.thu/data_pic_out.h"
 
@@ -100,7 +101,71 @@ INCLUDE_ASM("asm/nonmatchings/Event/stage/stg_ovservation", stg_ovservation_EvPr
     return 0;
 }
 
-INCLUDE_ASM("asm/nonmatchings/Event/stage/stg_ovservation", stg_ovservation_EvProgLastScene);
+/* static */ int stg_ovservation_EvProgLastScene(void) {
+
+    static short stg_ovservation_ovservation_anim_01F02DD0[2] = { 0x03D1, 0x002C }; // @ 0x01F02DD0 @note: not present in dwarf
+
+    static DramaDemo_PlayInfo stg_ovservation_info_01F02DE0 = {
+        /* .demo_no = */      89,
+        /* .adr_dds_top = */  MemShare_gp_data_buf,
+        /* .adr_anim = */     &stg_ovservation_ovservation_anim_01F02DD0,
+        /* .adr_msg_time = */ NULL,
+        /* .msg_start = */    0x0,
+        /* .voice_sd_no = */  0,
+        /* .adr_voice = */    NULL,
+        /* .stream_no = */    0x0,
+        /* .stream_start = */ 0.0f,
+        /* .add_pos_x = */    0.0f,
+        /* .add_pos_z = */    0.0f
+    }; // @ 0x01F02DE0
+
+    static CharaData_DemoList stg_ovservation_chara_data_01F02E10[2] = {
+        {
+            /* .kind = */      LLL_MAR_CHARA_KIND,
+            /* .model = */     data_chr_mar_lll_mar_mdl,
+            /* .animation = */ data_demo_mar_isho_i_lll_mar_anm,
+            /* .shadow = */    data_chr_mar_hhh_mar_kg1,
+            /* .cluster = */   NULL
+        },
+        {
+            /* .kind = */      0,
+            /* .model = */     NULL,
+            /* .animation = */ NULL,
+            /* .shadow = */    NULL,
+            /* .cluster = */   NULL
+        }
+    }; // @ 0x01F02E10
+
+    static u_long128* stg_ovservation_anim_adr_01F02E80 = NULL;
+
+    switch (ev_p_step) {
+        case 0:
+            FcRead(&data_demo_mar_isho_i_end_maria_i_dds, MemShare_gp_data_buf);
+            CharaDataLoadDemo(&stg_ovservation_chara_data_01F02E10, 0);
+            stg_ovservation_anim_adr_01F02E80 = (u_long128*) &MemShare_gp_data_buf[(FcGetFileSize(&data_demo_mar_isho_i_end_maria_i_dds) + 0x7FF) & ~0x7FF];
+            FcRead(&data_demo_mar_isho_i_lll_jms_anm, stg_ovservation_anim_adr_01F02E80);
+            fsSync(0, -1);
+            stg_ovservation_anim_adr_01F02E80 = CharaDataAnimAdressExchange(sh2jms.player, stg_ovservation_anim_adr_01F02E80);
+            CharaAdminPlayableDisplay(0);
+            SCNowDemoEventSwitch(sh2jms.player, 1);
+            EV_PROG_STEP(22);
+            /* fallthrough */
+        case 22:
+            if (DramaDemoMain(&stg_ovservation_info_01F02DE0) != 0) {
+                EV_PROG_STEP(13);
+            }
+            break;
+        case 13:
+            CharaDataDeleteOne(LAU_CHARA_KIND);
+            stg_ovservation_anim_adr_01F02E80 = CharaDataAnimAdressExchange(sh2jms.player, stg_ovservation_anim_adr_01F02E80);
+            SCNowDemoEventSwitch(sh2jms.player, 0);
+            shCharacterPlayerModelToPlayable();
+            CharaAdminPlayableDisplay(1);
+            vcReturnPreAutoCamWork(1);
+            return 1;
+    }
+    return 0;
+}
 
 /* static */ void stg_ovservation_EvAllTimeFunc(void) {
     int disp_ctrl_list[3]; // r29+0x10
