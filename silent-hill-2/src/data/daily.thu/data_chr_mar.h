b75d4213667ea4309e13be4c42c20f07ed16f481
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-06-30 13:05:50 +0200

    add `data_chr_mar.h`

---
+++ silent-hill-2/src/data/daily.thu/data_chr_mar.h
@@ -0,0 +1,37 @@
+#ifndef DATA_CHR_MAR_H
+#define DATA_CHR_MAR_H
+
+#include "data/fs_structs.h"
+
+extern char z_data_chr_mar_mgf__name[17]; // size: 0x11, address: 0x0
+extern fsFile z_data_chr_mar_mgf__info[1]; // size: 0x10, address: 0x3AB1B0
+extern fsFileIndex data_chr_mar_mgf[1]; // size: 0x8, address: 0x0
+extern /* static */ char z_data_chr_mar_hhh_mar_kg1__name[25]; // size: 0x19, address: 0x3BF280
+extern fsFile z_data_chr_mar_hhh_mar_kg1__info[1]; // size: 0x10, address: 0x3AC6D0
+extern fsFileIndex data_chr_mar_hhh_mar_kg1[1]; // size: 0x8, address: 0x3A0FB0
+extern /* static */ char z_data_chr_mar_hhh_mar_mdl__name[25]; // size: 0x19, address: 0x3BF2A0
+extern fsFile z_data_chr_mar_hhh_mar_mdl__info[1]; // size: 0x10, address: 0x3AC6E0
+extern fsFileIndex data_chr_mar_hhh_mar_mdl[1]; // size: 0x8, address: 0x3A0FB8
+extern /* static */ char z_data_chr_mar_hhh_mar_notex_mdl__name[31]; // size: 0x1F, address: 0x3BF2C0
+extern fsFile z_data_chr_mar_hhh_mar_notex_mdl__info[1]; // size: 0x10, address: 0x3AC6F0
+extern fsFileIndex data_chr_mar_hhh_mar_notex_mdl[1]; // size: 0x8, address: 0x3A0FC0
+extern /* static */ char z_data_chr_mar_lll_mar_anm__name[25]; // size: 0x19, address: 0x3BF2E0
+extern fsFile z_data_chr_mar_lll_mar_anm__info[1]; // size: 0x10, address: 0x3AC700
+extern fsFileIndex data_chr_mar_lll_mar_anm[1]; // size: 0x8, address: 0x3A0FC8
+extern /* static */ char z_data_chr_mar_lll_mar_mdl__name[25]; // size: 0x19, address: 0x3BF300
+extern fsFile z_data_chr_mar_lll_mar_mdl__info[1]; // size: 0x10, address: 0x3AC710
+extern fsFileIndex data_chr_mar_lll_mar_mdl[1]; // size: 0x8, address: 0x3A0FD0
+extern /* static */ char z_data_chr_mar_lll_mar_back_anm__name[30]; // size: 0x1E, address: 0x0
+extern fsFile z_data_chr_mar_lll_mar_back_anm__info[1]; // size: 0x10, address: 0x0
+extern fsFileIndex data_chr_mar_lll_mar_back_anm[1]; // size: 0x8, address: 0x0
+extern /* static */ char z_data_chr_mar_p_hhh_mar_anm__name[27]; // size: 0x1B, address: 0x0
+extern fsFile z_data_chr_mar_p_hhh_mar_anm__info[1]; // size: 0x10, address: 0x0
+extern fsFileIndex data_chr_mar_p_hhh_mar_anm[1]; // size: 0x8, address: 0x0
+extern /* static */ char z_data_chr_mar_rhhh_mar_mdl__name[26]; // size: 0x1A, address: 0x0
+extern fsFile z_data_chr_mar_rhhh_mar_mdl__info[1]; // size: 0x10, address: 0x0
+extern fsFileIndex data_chr_mar_rhhh_mar_mdl[1]; // size: 0x8, address: 0x0
+extern /* static */ char z_data_chr_mar_rlll_mar_mdl__name[26]; // size: 0x1A, address: 0x0
+extern fsFile z_data_chr_mar_rlll_mar_mdl__info[1]; // size: 0x10, address: 0x0
+extern fsFileIndex data_chr_mar_rlll_mar_mdl[1]; // size: 0x8, address: 0x0
+
+#endif DATA_CHR_MAR_H
