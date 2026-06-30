d61346d963748a0d492b7d25c5bfed805fc58253
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-06-30 10:21:38 +0200

    add `data_demo_mar_isho_i.h`

---
+++ silent-hill-2/src/data/daily.thu/data_demo_mar_isho_i.h
@@ -0,0 +1,19 @@
+#ifndef DATA_DEMO_MAR_ISHO_I_H
+#define DATA_DEMO_MAR_ISHO_I_H
+
+#include "data/fs_structs.h"
+
+extern char z_data_demo_mar_isho_i_mgf__name[25]; // size: 0x19, address: 0x0
+extern fsFile z_data_demo_mar_isho_i_mgf__info[1]; // size: 0x10, address: 0x3ACE50
+extern fsFileIndex data_demo_mar_isho_i_mgf[1]; // size: 0x8, address: 0x0
+extern /* static */ char z_data_demo_mar_isho_i_end_maria_i_dds__name[37]; // size: 0x25, address: 0x3C6370
+extern fsFile z_data_demo_mar_isho_i_end_maria_i_dds__info[1]; // size: 0x10, address: 0x3B0170
+extern fsFileIndex data_demo_mar_isho_i_end_maria_i_dds[1]; // size: 0x8, address: 0x3A2990
+extern /* static */ char z_data_demo_mar_isho_i_lll_jms_anm__name[33]; // size: 0x21, address: 0x3C6398
+extern fsFile z_data_demo_mar_isho_i_lll_jms_anm__info[1]; // size: 0x10, address: 0x3B0180
+extern fsFileIndex data_demo_mar_isho_i_lll_jms_anm[1]; // size: 0x8, address: 0x3A2998
+extern /* static */ char z_data_demo_mar_isho_i_lll_mar_anm__name[33]; // size: 0x21, address: 0x3C63C0
+extern fsFile z_data_demo_mar_isho_i_lll_mar_anm__info[1]; // size: 0x10, address: 0x3B0190
+extern fsFileIndex data_demo_mar_isho_i_lll_mar_anm[1]; // size: 0x8, address: 0x3A29A0
+
+#endif
