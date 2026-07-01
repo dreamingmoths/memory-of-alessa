ebe4d14ec94513c9a6653dad4478349eaf153742
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-07-01 09:40:21 +0200

    add `data_demo_tenbou.h`

---
+++ silent-hill-2/src/data/daily.thu/data_demo_tenbou.h
@@ -0,0 +1,19 @@
+#ifndef DATA_DEMO_TENBOU_H
+#define DATA_DEMO_TENBOU_H
+
+#include "data/fs_structs.h"
+
+extern char z_data_demo_tenbou_mgf__name[21]; // size: 0x15, address: 0x0
+extern fsFile z_data_demo_tenbou_mgf__info[1]; // size: 0x10, address: 0x3ACF80
+extern fsFileIndex data_demo_tenbou_mgf[1]; // size: 0x8, address: 0x0
+extern /* static */ char z_data_demo_tenbou_hhh_jms_anm__name[29]; // size: 0x1D, address: 0x3C52F0
+extern fsFile z_data_demo_tenbou_hhh_jms_anm__info[1]; // size: 0x10, address: 0x3AF9C0
+extern fsFileIndex data_demo_tenbou_hhh_jms_anm[1]; // size: 0x8, address: 0x3A25B8
+extern /* static */ char z_data_demo_tenbou_hhh_jms_cls__name[29]; // size: 0x1D, address: 0x3C5310
+extern fsFile z_data_demo_tenbou_hhh_jms_cls__info[1]; // size: 0x10, address: 0x3AF9D0
+extern fsFileIndex data_demo_tenbou_hhh_jms_cls[1]; // size: 0x8, address: 0x3A25C0
+extern /* static */ char z_data_demo_tenbou_tenbou_dds__name[28]; // size: 0x1C, address: 0x3C5330
+extern fsFile z_data_demo_tenbou_tenbou_dds__info[1]; // size: 0x10, address: 0x3AF9E0
+extern fsFileIndex data_demo_tenbou_tenbou_dds[1]; // size: 0x8, address: 0x3A25C8
+
+#endif DATA_DEMO_TENBOU_H
