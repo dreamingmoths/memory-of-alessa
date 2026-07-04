e3ef58d0d405d809145ca2f66c45defda6e2bd2a
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-07-04 16:53:21 +0200

    update `Enemy/en_insect.h`

--- silent-hill-2/src/Enemy/en_insect.h
+++ silent-hill-2/src/Enemy/en_insect.h
@@ -1,4 +1,10 @@
 #ifndef EN_INSECT_H
 #define EN_INSECT_H
 
+#include "sh2_common.h"
+#include "Enemy/en_common.h"
+
+void enSetInsect(float* pos /* r19 */, int num /* r18 */);
+void enKillAllInsect(void);
+
 #endif // EN_INSECT_H
