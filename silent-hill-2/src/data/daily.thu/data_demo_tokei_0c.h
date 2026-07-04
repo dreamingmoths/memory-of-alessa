f0b8db52df1a8237c0062b994d663578473df896
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-07-04 23:28:48 +0200

    add `data_demo_tokei_0c.h`

---
+++ silent-hill-2/src/data/daily.thu/data_demo_tokei_0c.h
@@ -0,0 +1,22 @@
+#ifndef DATA_DEMO_TOKEI_0C_H
+#define DATA_DEMO_TOKEI_0C_H
+
+#include "data/fs_structs.h"
+
+extern char z_data_demo_tokei_0c_mgf__name[23]; // size: 0x17, address: 0x0
+extern fsFile z_data_demo_tokei_0c_mgf__info[1]; // size: 0x10, address: 0x3ACFF0
+extern fsFileIndex data_demo_tokei_0c_mgf[1]; // size: 0x8, address: 0x0
+extern /* static  */ char z_data_demo_tokei_0c_b_clo_anm__name[29]; // size: 0x1D, address: 0x3C3FD0
+extern fsFile z_data_demo_tokei_0c_b_clo_anm__info[1]; // size: 0x10, address: 0x3AF0A0
+extern fsFileIndex data_demo_tokei_0c_b_clo_anm[1]; // size: 0x8, address: 0x3A2128
+extern /* static  */ char z_data_demo_tokei_0c_clock_fail_dds__name[34]; // size: 0x22, address: 0x3C3FF0
+extern fsFile z_data_demo_tokei_0c_clock_fail_dds__info[1]; // size: 0x10, address: 0x3AF0B0
+extern fsFileIndex data_demo_tokei_0c_clock_fail_dds[1]; // size: 0x8, address: 0x3A2130
+extern /* static  */ char z_data_demo_tokei_0c_hhh_jms_anm__name[31]; // size: 0x1F, address: 0x3C4018
+extern fsFile z_data_demo_tokei_0c_hhh_jms_anm__info[1]; // size: 0x10, address: 0x3AF0C0
+extern fsFileIndex data_demo_tokei_0c_hhh_jms_anm[1]; // size: 0x8, address: 0x3A2138
+extern /* static  */ char z_data_demo_tokei_0c_hhh_jms_cls__name[31]; // size: 0x1F, address: 0x3C4038
+extern fsFile z_data_demo_tokei_0c_hhh_jms_cls__info[1]; // size: 0x10, address: 0x3AF0D0
+extern fsFileIndex data_demo_tokei_0c_hhh_jms_cls[1]; // size: 0x8, address: 0x3A2140
+
+#endif DATA_DEMO_TOKEI_0C_H
