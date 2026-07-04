62fe1cd904f0e3a8dd577af3d697f03b26be074b
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-07-04 18:48:35 +0200

    add `data_demo_ana.h`

---
+++ silent-hill-2/src/data/daily.thu/data_demo_ana.h
@@ -0,0 +1,25 @@
+#ifndef DATA_DEMO_ANA_H
+#define DATA_DEMO_ANA_H
+
+#include "data/fs_structs.h"
+
+extern char z_data_demo_ana_mgf__name[18]; // size: 0x12, address: 0x0
+extern fsFile z_data_demo_ana_mgf__info[1]; // size: 0x10, address: 0x3ACA60
+extern fsFileIndex data_demo_ana_mgf[1]; // size: 0x8, address: 0x0
+extern /* static */ char z_data_demo_ana_ana_dds__name[22]; // size: 0x16, address: 0x3BF818
+extern fsFile z_data_demo_ana_ana_dds__info[1]; // size: 0x10, address: 0x3AD020
+extern fsFileIndex data_demo_ana_ana_dds[1]; // size: 0x8, address: 0x3A1150
+extern /* static */ char z_data_demo_ana_hhh_jms_anm__name[26]; // size: 0x1A, address: 0x3BF830
+extern fsFile z_data_demo_ana_hhh_jms_anm__info[1]; // size: 0x10, address: 0x3AD030
+extern fsFileIndex data_demo_ana_hhh_jms_anm[1]; // size: 0x8, address: 0x3A1158
+extern /* static */ char z_data_demo_ana_hhh_jms_cls__name[26]; // size: 0x1A, address: 0x3BF850
+extern fsFile z_data_demo_ana_hhh_jms_cls__info[1]; // size: 0x10, address: 0x3AD040
+extern fsFileIndex data_demo_ana_hhh_jms_cls[1]; // size: 0x8, address: 0x3A1160
+extern /* static */ char z_data_demo_ana_i_j_light_anm__name[28]; // size: 0x1C, address: 0x3BF870
+extern fsFile z_data_demo_ana_i_j_light_anm__info[1]; // size: 0x10, address: 0x3AD050
+extern fsFileIndex data_demo_ana_i_j_light_anm[1]; // size: 0x8, address: 0x3A1168
+extern /* static */ char z_data_demo_ana_i_key_clock_anm__name[30]; // size: 0x1E, address: 0x3BF890
+extern fsFile z_data_demo_ana_i_key_clock_anm__info[1]; // size: 0x10, address: 0x3AD060
+extern fsFileIndex data_demo_ana_i_key_clock_anm[1]; // size: 0x8, address: 0x3A1170
+
+#endif DATA_DEMO_ANA_H
