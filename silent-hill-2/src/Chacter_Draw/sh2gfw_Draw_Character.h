895f905cbaacf3a7a1687418ea9bf02643567dd5
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-07-04 11:34:06 +0200

    update `sh2gfw_Draw_Character.h`

--- silent-hill-2/src/Chacter_Draw/sh2gfw_Draw_Character.h
+++ silent-hill-2/src/Chacter_Draw/sh2gfw_Draw_Character.h
@@ -1,4 +1,9 @@
 #ifndef SH2GFW_DRAW_CHARACTER_H
 #define SH2GFW_DRAW_CHARACTER_H
 
+#include "sh2_common.h"
+
+void sh2gfw_SetNoise_CharaTexture(int charaid /* r2 */);
+void sh2gfw_RemoveNoise_CharaTexture(int charaid /* r2 */);
+
 #endif // SH2GFW_DRAW_CHARACTER_H
