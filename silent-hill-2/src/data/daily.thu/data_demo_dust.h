9a8cf0c3f7020e879770389cc22d14cf52f2eb94
moth dreams <mothdreams666@gmail.com>
2026-07-04 15:37:34 -0700

    sh2: add `data_demo_dust.h`

---
+++ silent-hill-2/src/data/daily.thu/data_demo_dust.h
@@ -0,0 +1,23 @@
+#ifndef DATA_DEMO_DUST_H
+#define DATA_DEMO_DUST_H
+
+#include "data/fs_structs.h"
+
+extern char z_data_demo_dust_mgf__name[19]; // size: 0x13, address: 0x0
+extern fsFile z_data_demo_dust_mgf__info[1]; // size: 0x10, address: 0x3ACB10
+extern fsFileIndex data_demo_dust_mgf[1]; // size: 0x8, address: 0x0
+extern /* static */ char z_data_demo_dust_dust_dds__name[24]; // size: 0x18, address: 0x3C4A60
+extern fsFile z_data_demo_dust_dust_dds__info[1]; // size: 0x10, address: 0x3AF5E0
+extern fsFileIndex data_demo_dust_dust_dds[1]; // size: 0x8, address: 0x3A23C8
+extern /* static */ char z_data_demo_dust_hhl_jms_anm__name[27]; // size: 0x1B, address: 0x3C4A78
+extern fsFile z_data_demo_dust_hhl_jms_anm__info[1]; // size: 0x10, address: 0x3AF5F0
+extern fsFileIndex data_demo_dust_hhl_jms_anm[1]; // size: 0x8, address: 0x3A23D0
+extern /* static */ char z_data_demo_dust_hhl_jms_cls__name[27]; // size: 0x1B, address: 0x3C4A98
+extern fsFile z_data_demo_dust_hhl_jms_cls__info[1]; // size: 0x10, address: 0x3AF600
+extern fsFileIndex data_demo_dust_hhl_jms_cls[1]; // size: 0x8, address: 0x3A23D8
+extern /* static */ char z_data_demo_dust_i_juice_anm__name[27]; // size: 0x1B, address: 0x3C4AB8
+extern fsFile z_data_demo_dust_i_juice_anm__info[1]; // size: 0x10, address: 0x3AF610
+extern fsFileIndex data_demo_dust_i_juice_anm[1]; // size: 0x8, address: 0x3A23E0
+
+#endif // DATA_DEMO_DUST_H
+
