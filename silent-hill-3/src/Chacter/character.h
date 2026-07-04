252abb21dcdcda5e84e7a96a29473bb2f0524fcb
moth dreams <mothdreams666@gmail.com>
2026-07-04 15:26:42 -0700

    amazingly this matches

--- silent-hill-3/src/Chacter/character.h
+++ silent-hill-3/src/Chacter/character.h
@@ -88,29 +88,25 @@ typedef struct _IXY {
     int y; // offset 0x4, size 0x4
 } IXY;
 
+/**
+ * @sh3 parent & next were swapped since the sh2 proto.
+ * also, 0x10 bytes were removed, though it's unclear which ones
+ */
 typedef struct shSkelton {
-    // total size: 0xF0
-    struct shSkelton* next;   // offset 0x0, size 0x4
-    struct shSkelton* parent; // offset 0x4, size 0x4
-    sceVu0FMATRIX src_m;      // offset 0x10, size 0x40
-    sceVu0FVECTOR src_t;      // offset 0x50, size 0x10
-    sceVu0FMATRIX des_m;      // offset 0x60, size 0x40
-    sceVu0FVECTOR des_t;      // offset 0xA0, size 0x10
-    sceVu0FVECTOR axis;       // offset 0xB0, size 0x10
-    float theta;              // offset 0xC0, size 0x4
-    float xx;                 // offset 0xC4, size 0x4
-    float yy;                 // offset 0xC8, size 0x4
-    float zz;                 // offset 0xCC, size 0x4
-    float xy;                 // offset 0xD0, size 0x4
-    float yz;                 // offset 0xD4, size 0x4
-    float zx;                 // offset 0xD8, size 0x4
-    u_short c_count;          // offset 0xDC, size 0x2
-    u_short c_speed;          // offset 0xDE, size 0x2
-    s_char change;            // offset 0xE0, size 0x1
-    char reserved;            // offset 0xE1, size 0x1
-    char is_key;              // offset 0xE2, size 0x1
-    char pad;                 // offset 0xE3, size 0x1
-    void* untouchable;        // offset 0xE4, size 0x4
+    // total size: 0xE0
+    struct shSkelton* parent; // offset 0x0, size 0x4
+    struct shSkelton* next;   // offset 0x4, size 0x4
+    Matrix4 src_m;              // offset 0x10, size 0x40
+    Vector4 src_t;              // offset 0x50, size:0x10
+    Matrix4 des_m;              // offset 0x60, size:0x40
+    Vector4 des_t;              // offset 0xA0, size:0x10
+    Vector4 axis;               // offset 0xB0, size:0x10
+    float theta;                // offset 0xC0, size 0x4
+    float xx;                   // offset 0xC4, size 0x4
+    float yy;                   // offset 0xC8, size 0x4
+    float zz;                   // offset 0xCC, size 0x4
+    int unkD0;                  // offset 0xD0, size 0x4
+    void* untouchable;          // offset 0xD4, size 0x4
 } shSkelton;
 
 typedef struct _AnimeInfo {
--- silent-hill-3/src/Chacter/skelton.c
+++ silent-hill-3/src/Chacter/skelton.c
@@ -4,44 +4,17 @@
 
 #define SH3_CHARACTER_NUM_SKELTONS 400
 
-// 0x10 bytes got removed..... not sure which ones
-// parent & next were swapped
-typedef struct sh3_Skelton {
-    // total size: 0xE0
-    struct sh3_Skelton* parent; // offset 0x4, size 0x4
-    struct sh3_Skelton* next;   // offset 0x0, size 0x4
-    Matrix4 src_m;
-    Vector4 src_t;
-    Matrix4 des_m;
-    Vector4 des_t;
-    Vector4 axis;
-    float theta;     // offset 0xC0, size 0x4
-    float xx;        // offset 0xC4, size 0x4
-    float yy;        // offset 0xC8, size 0x4
-    float zz;        // offset 0xCC, size 0x4
-    float xy;        // offset 0xD0, size 0x4
-    float yz;        // offset 0xD4, size 0x4
-    float zx;        // offset 0xD8, size 0x4
-    u_short c_count; // offset 0xDC, size 0x2
-    u_short c_speed; // offset 0xDE, size 0x2
-    // s_char change; // offset 0xE0, size 0x1
-    // char reserved; // offset 0xE1, size 0x1
-    // char is_key; // offset 0xE2, size 0x1
-    // char pad; // offset 0xE3, size 0x1
-    // void* untouchable; // offset 0xE4, size 0x4
-} sh3_Skelton;
-
-typedef struct sh3_SkeltonWork {
+typedef struct shSkeltonWork {
     // total size: 0x15E10
-    sh3_Skelton work[SH3_CHARACTER_NUM_SKELTONS]; // offset 0x0, size 0x15E00
-    sh3_Skelton* free;                            // offset 0x15E00, size 0x4
+    shSkelton work[SH3_CHARACTER_NUM_SKELTONS]; // offset 0x0, size 0x15E00
+    shSkelton* free;                            // offset 0x15E00, size 0x4
     int last;                                     // offset 0x15E04, size 0x4
-} sh3_SkeltonWork;
+} shSkeltonWork;
 
-sh3_SkeltonWork sh3skelton; // address: 0x046E870, size: 0x15E10
+shSkeltonWork sh3skelton; // address: 0x046E870, size: 0x15E10
 
 void shCharacterInitSkeltons(void) {
-    sh3_Skelton* stp;
+    shSkelton* stp;
     int i;
 
     memset(&sh3skelton, 0, sizeof(sh3skelton));
@@ -56,9 +29,9 @@ void shCharacterInitSkeltons(void) {
     stp->next = NULL;
 }
 
-void shCharacterFreeSkeltons(sh3_Skelton* top) {
+void shCharacterFreeSkeltons(shSkelton* top) {
     int i = 0;
-    sh3_Skelton* free;
+    shSkelton* free;
 
     if (top != NULL) {
 
@@ -85,12 +58,12 @@ void sh3CharacterAddSkeletons(int n) {
     sh3skelton.last += n;
 }
 
-sh3_Skelton* shCharacterGetSkeletons(int n, u_char* hrc) {
-    sh3_Skelton* stp;     // r3
-    sh3_Skelton* top_stp; // r16
-    sh3_Skelton* pre_stp; // r17
+shSkelton* shCharacterGetSkeletons(int n, u_char* hrc) {
+    shSkelton* stp;     // r3
+    shSkelton* top_stp; // r16
+    shSkelton* pre_stp; // r17
     int i;                // r4
-    sh3_Skelton* parent;  // r2
+    shSkelton* parent;  // r2
     int j;                // r2
 
     if (sh3skelton.last < n || n == 0) {
