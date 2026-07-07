42afbf1e3c521a2b6cad535ec92432922f4f1649
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-07-07 12:25:37 +0200

    Update sh2gfw_all_sysinit.h

--- silent-hill-2/src/sh2gfw_all_sysinit.h
+++ silent-hill-2/src/sh2gfw_all_sysinit.h
@@ -3,6 +3,10 @@
 
 #include "sh2_common.h"
 
+int Vcallback_test(int ca);
 void init_PS2(void);
+void step_init_ONE(void);
+void step_init_STAGE(void);
+void map_DATA_LOAD(void);
 
 #endif // SH2GFW_ALL_SYSINIT_H
