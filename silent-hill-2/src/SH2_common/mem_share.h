35fa259d97d9d36ae84fa3232f4c461f96073a77
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-07-07 12:24:39 +0200

    Update mem_share.h

--- silent-hill-2/src/SH2_common/mem_share.h
+++ silent-hill-2/src/SH2_common/mem_share.h
@@ -1,8 +1,24 @@
 #ifndef SH2_COMMON_MEM_SHARE_H
 #define SH2_COMMON_MEM_SHARE_H
 
-extern char MemShare_gp_data_buf[13180928]; // size: 0xC92000, address: 0x126FBC0
+#include "sh2_common.h"
 
+void MemShareAllocateTitleMovie(void);
+void MemShareAllocateEventMovie(int slot);
+void MemShareReleaseMovie(void);
+void MemShareRecoverMovie(void);
+void MemShareWaitRealloc(int mode);
+void* MemShareGetBgLoadWorkAddr(void);
+void* MemShareGetBgLoadSectionWorkAddr(int slot);
+void* MemShareGetBgLoadCacheAddr(void);
+void* MemShareGetBgCharaWorkAddr(void);
 void* MemShareGetEffect2WorkAddr(void);
+void* MemShareGetMovieTagWorkAddr(void);
+void* MemShareGetMovieVoWorkAddr(void);
+void* MemShareGetMovieViWorkAddr(void);
+void* MemShareGetMovieMpegWorkAddr(void);
+void* MemShareGetMovieReadWorkAddr(void);
+
+extern char MemShare_gp_data_buf[13180928]; // size: 0xC92000, address: 0x126FBC0
 
 #endif
