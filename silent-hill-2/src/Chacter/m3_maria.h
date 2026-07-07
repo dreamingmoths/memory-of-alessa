a53b884febe7c9151d0ccc73926d18cb61464012
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-07-07 12:26:02 +0200

    Update m3_maria.h

--- silent-hill-2/src/Chacter/m3_maria.h
+++ silent-hill-2/src/Chacter/m3_maria.h
@@ -1,4 +1,12 @@
 #ifndef M3_MARIA_H
 #define M3_MARIA_H
 
+#include "sh2_common.h"
+#include "Chacter/character.h"
+
+void shCharacterSetHumanMARLow(SubCharacter* scp);
+int shCharacterHumanMARAnimeSet(SubCharacter* scp, int anime_id);
+void shCharacterMariaWorkInit(void);
+void shCharacterMariaWorkInitAtGameStart(void);
+
 #endif // M3_MARIA_H
