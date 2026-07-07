b45bc3a39168c083f4906bc1c176d510cb000817
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-07-07 12:23:49 +0200

    Update sh2gfw_drawloop_main.h

--- silent-hill-2/src/sh2gfw_drawloop_main.h
+++ silent-hill-2/src/sh2gfw_drawloop_main.h
@@ -1,4 +1,14 @@
 #ifndef SH2GFW_DRAWLOOP_MAIN_H
 #define SH2GFW_DRAWLOOP_MAIN_H
 
+#include "sh2_common.h"
+
+void all_Frame_Buffer_Clear(void);
+void DrawLopp_Pre(void);
+void kari_drawloop_main_2dSYNC(void);
+void draw_main_3dSYNC(void);
+void DrawLopp_Post(void);
+u_int Get_FrameRate(void);
+void draw_main(void);
+
 #endif // SH2GFW_DRAWLOOP_MAIN_H
