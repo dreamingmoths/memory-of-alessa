6091b6add6883443faf3411552a215e05b618e58
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-07-07 11:03:16 +0200

    Add data_pic_etc.h

---
+++ silent-hill-2/src/data/daily.thu/data_pic_etc.h
@@ -0,0 +1,70 @@
+#ifndef DATA_PIC_ETC_H
+#define DATA_PIC_ETC_H
+
+#include "data/fs_structs.h"
+
+extern char z_data_pic_etc_mgf__name[17]; // size: 0x11, address: 0x0
+extern fsFile z_data_pic_etc_mgf__info[1]; // size: 0x10, address: 0x3AE4B0
+extern fsFileIndex data_pic_etc_mgf[1]; // size: 0x8, address: 0x0
+extern /* static */ char z_data_pic_etc_botan_tex__name[23]; // size: 0x17, address: 0x3C2B18
+extern fsFile z_data_pic_etc_botan_tex__info[1]; // size: 0x10, address: 0x3AE680
+extern fsFileIndex data_pic_etc_botan_tex[1]; // size: 0x8, address: 0x3A1C18
+extern /* static */ char z_data_pic_etc_carsol_tex__name[24]; // size: 0x18, address: 0x3C2B30
+extern fsFile z_data_pic_etc_carsol_tex__info[1]; // size: 0x10, address: 0x3AE690
+extern fsFileIndex data_pic_etc_carsol_tex[1]; // size: 0x8, address: 0x3A1C20
+extern /* static */ char z_data_pic_etc_cesa_tex__name[22]; // size: 0x16, address: 0x3C2B48
+extern fsFile z_data_pic_etc_cesa_tex__info[1]; // size: 0x10, address: 0x3AE6A0
+extern fsFileIndex data_pic_etc_cesa_tex[1]; // size: 0x8, address: 0x3A1C28
+extern /* static */ char z_data_pic_etc_comingsoon_tex__name[28]; // size: 0x1C, address: 0x3C2B60
+extern fsFile z_data_pic_etc_comingsoon_tex__info[1]; // size: 0x10, address: 0x3AE6B0
+extern fsFileIndex data_pic_etc_comingsoon_tex[1]; // size: 0x8, address: 0x3A1C30
+extern /* static */ char z_data_pic_etc_gameover1_tex__name[27]; // size: 0x1B, address: 0x3C2B80
+extern fsFile z_data_pic_etc_gameover1_tex__info[1]; // size: 0x10, address: 0x3AE6C0
+extern fsFileIndex data_pic_etc_gameover1_tex[1]; // size: 0x8, address: 0x3A1C38
+extern /* static */ char z_data_pic_etc_itemmenu_tex__name[26]; // size: 0x1A, address: 0x0
+extern fsFile z_data_pic_etc_itemmenu_tex__info[1]; // size: 0x10, address: 0x0
+extern fsFileIndex data_pic_etc_itemmenu_tex[1]; // size: 0x8, address: 0x0
+extern /* static */ char z_data_pic_etc_itemmenu2_tex__name[27]; // size: 0x1B, address: 0x3C2BA0
+extern fsFile z_data_pic_etc_itemmenu2_tex__info[1]; // size: 0x10, address: 0x3AE6D0
+extern fsFileIndex data_pic_etc_itemmenu2_tex[1]; // size: 0x8, address: 0x3A1C40
+extern /* static */ char z_data_pic_etc_kcet_tex__name[22]; // size: 0x16, address: 0x0
+extern fsFile z_data_pic_etc_kcet_tex__info[1]; // size: 0x10, address: 0x0
+extern fsFileIndex data_pic_etc_kcet_tex[1]; // size: 0x8, address: 0x0
+extern /* static */ char z_data_pic_etc_konami_tex__name[24]; // size: 0x18, address: 0x0
+extern fsFile z_data_pic_etc_konami_tex__info[1]; // size: 0x10, address: 0x0
+extern fsFileIndex data_pic_etc_konami_tex[1]; // size: 0x8, address: 0x0
+extern /* static */ char z_data_pic_etc_konami_r_tex__name[26]; // size: 0x1A, address: 0x3C2BC0
+extern fsFile z_data_pic_etc_konami_r_tex__info[1]; // size: 0x10, address: 0x3AE6E0
+extern fsFileIndex data_pic_etc_konami_r_tex[1]; // size: 0x8, address: 0x3A1C48
+extern /* static */ char z_data_pic_etc_p_laura_letter_tex__name[32]; // size: 0x20, address: 0x3C2BE0
+extern fsFile z_data_pic_etc_p_laura_letter_tex__info[1]; // size: 0x10, address: 0x3AE6F0
+extern fsFileIndex data_pic_etc_p_laura_letter_tex[1]; // size: 0x8, address: 0x3A1C50
+extern /* static */ char z_data_pic_etc_p_letterm_tex__name[27]; // size: 0x1B, address: 0x3C2C00
+extern fsFile z_data_pic_etc_p_letterm_tex__info[1]; // size: 0x10, address: 0x3AE700
+extern fsFileIndex data_pic_etc_p_letterm_tex[1]; // size: 0x8, address: 0x3A1C58
+extern /* static */ char z_data_pic_etc_p_lostmemory_tex__name[30]; // size: 0x1E, address: 0x0
+extern fsFile z_data_pic_etc_p_lostmemory_tex__info[1]; // size: 0x10, address: 0x0
+extern fsFileIndex data_pic_etc_p_lostmemory_tex[1]; // size: 0x8, address: 0x0
+extern /* static */ char z_data_pic_etc_p_memo_tex__name[24]; // size: 0x18, address: 0x3C2C20
+extern fsFile z_data_pic_etc_p_memo_tex__info[1]; // size: 0x10, address: 0x3AE710
+extern fsFileIndex data_pic_etc_p_memo_tex[1]; // size: 0x8, address: 0x3A1C60
+extern /* static */ char z_data_pic_etc_p_redreling_tex__name[29]; // size: 0x1D, address: 0x0
+extern fsFile z_data_pic_etc_p_redreling_tex__info[1]; // size: 0x10, address: 0x0
+extern fsFileIndex data_pic_etc_p_redreling_tex[1]; // size: 0x8, address: 0x0
+extern /* static */ char z_data_pic_etc_savebg1_tex__name[25]; // size: 0x19, address: 0x0
+extern fsFile z_data_pic_etc_savebg1_tex__info[1]; // size: 0x10, address: 0x0
+extern fsFileIndex data_pic_etc_savebg1_tex[1]; // size: 0x8, address: 0x0
+extern /* static */ char z_data_pic_etc_sce_tex__name[21]; // size: 0x15, address: 0x3C2C38
+extern fsFile z_data_pic_etc_sce_tex__info[1]; // size: 0x10, address: 0x3AE720
+extern fsFileIndex data_pic_etc_sce_tex[1]; // size: 0x8, address: 0x3A1C68
+extern /* static */ char z_data_pic_etc_start00_tex__name[25]; // size: 0x19, address: 0x3C2C50
+extern fsFile z_data_pic_etc_start00_tex__info[1]; // size: 0x10, address: 0x3AE730
+extern fsFileIndex data_pic_etc_start00_tex[1]; // size: 0x8, address: 0x3A1C70
+extern /* static */ char z_data_pic_etc_start01_tex__name[25]; // size: 0x19, address: 0x3C2C70
+extern fsFile z_data_pic_etc_start01_tex__info[1]; // size: 0x10, address: 0x3AE740
+extern fsFileIndex data_pic_etc_start01_tex[1]; // size: 0x8, address: 0x3A1C78
+extern /* static */ char z_data_pic_etc_title_tex__name[23]; // size: 0x17, address: 0x0
+extern fsFile z_data_pic_etc_title_tex__info[1]; // size: 0x10, address: 0x0
+extern fsFileIndex data_pic_etc_title_tex[1]; // size: 0x8, address: 0x0
+
+#endif DATA_PIC_ETC_H
