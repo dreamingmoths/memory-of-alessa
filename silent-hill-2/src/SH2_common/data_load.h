f8b75c6bf22e3ef24939e2ef54e002223af4038d
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-07-07 11:29:10 +0200

    Update data_load.h

--- silent-hill-2/src/SH2_common/data_load.h
+++ silent-hill-2/src/SH2_common/data_load.h
@@ -1,8 +1,9 @@
 #ifndef DATA_LOAD_H
 #define DATA_LOAD_H
 
-#include "common.h"
+#include "sh2_common.h"
 
+int DataLoadMessage(int msg /* r2 */);
 void shMemCopy(void* ds /* r18 */, void* sr /* r17 */, int datasize /* r16 */);
 
 #endif // DATA_LOAD_H
