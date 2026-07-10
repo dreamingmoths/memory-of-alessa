656fe3bc93523a8e413f08be41ea6c2372bf2af4
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-07-10 17:31:21 +0200

    Update charaboundtbl.h

--- silent-hill-2/src/Chacter/charaboundtbl.h
+++ silent-hill-2/src/Chacter/charaboundtbl.h
@@ -1,4 +1,17 @@
 #ifndef CHARABOUNDTBL_H
 #define CHARABOUNDTBL_H
 
+#include "sh2_common.h"
+
+// total size: 0x8
+typedef struct Chara_Bound {
+    // Members
+    int chara_id; // offset 0x0, size 0x4
+    float r; // offset 0x4, size 0x4
+} Chara_Bound;
+
+int CharaBoundTblNum(void);
+
+extern Chara_Bound chara_bound_tbl[236]; // size: 0x760, address: 0x373070
+
 #endif // CHARABOUNDTBL_H
