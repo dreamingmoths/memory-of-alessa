47fdc51dd402ff7ca6328f1f4fae7900ade8e079
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-07-10 12:52:50 +0200

    Update m3_laura.h

--- silent-hill-2/src/Chacter/m3_laura.h
+++ silent-hill-2/src/Chacter/m3_laura.h
@@ -1,4 +1,11 @@
 #ifndef M3_LAURA_H
 #define M3_LAURA_H
 
+#include "sh2_common.h"
+#include "Chacter/character.h"
+
+void shCharacterSetHumanLAULow(SubCharacter* scp);
+int shCharacterHumanLAUAnimeSet(SubCharacter* scp, int anime_id);
+int shCharacterHumanLAUAnimeSetP(SubCharacter* scp, int anime_id);
+
 #endif // M3_LAURA_H
