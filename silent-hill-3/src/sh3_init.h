e64a39c188f9c35e0fbbaeb4482828be740ec745
moth dreams <mothdreams666@gmail.com>
2026-07-10 08:47:34 -0700

    add header file

---
+++ silent-hill-3/src/sh3_init.h
@@ -0,0 +1,18 @@
+#ifndef SH3_INIT_H
+#define SH3_INIT_H
+
+void sceDmaReset_(void);
+
+void sh3_initGS(void);
+
+void sh3_initVU(void);
+
+void sh3_initTimers(void);
+
+void func_0012BD80(void);
+
+void func_0012BDC0(void);
+
+void sh3_initFilesys(void);
+
+#endif // SH3_INIT_H
