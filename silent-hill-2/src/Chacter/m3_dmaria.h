182965ba3f56da68d6f3dd79e72cfed43b3b5ac9
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-07-11 10:16:59 +0200

    Update m3_dmaria.h

--- silent-hill-2/src/Chacter/m3_dmaria.h
+++ silent-hill-2/src/Chacter/m3_dmaria.h
@@ -1,4 +1,11 @@
 #ifndef M3_DMARIA_H
 #define M3_DMARIA_H
 
+#include "sh2_common.h"
+#include "Chacter/character.h"
+
+void shCharacterSetHumanDMARLow(SubCharacter* scp);
+int shCharacterHumanDMARAnimeSet(SubCharacter* scp, int anime_id);
+int shCharacterHumanDMARAnimeSetP(SubCharacter* scp, int anime_id);
+
 #endif // M3_DMARIA_H
