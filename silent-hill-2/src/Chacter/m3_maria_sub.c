d85914bcae073258a2bc4eacbcca4fe074d04556
moth dreams <mothdreams666@gmail.com>
2026-07-10 23:54:08 -0700

    sh2: begin `m3_maria_sub.c` 🦋

---
+++ silent-hill-2/src/Chacter/m3_maria_sub.c
@@ -0,0 +1,155 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_maria_sub", mar_flg_on);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_maria_sub", mar_flg_off);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_maria_sub", mar_sub_flg_on);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_maria_sub", mar_anime_flg_on);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_maria_sub", mar_main_st_set);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_maria_sub", mar_sub_st_set);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_maria_sub", mar_sub_flg_set);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_maria_sub", MariaBodyAngleCloseToTarget);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_maria_sub", MariaSpeedDownToStand);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_maria_sub", MariaPushedAnyoneIsOn);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_maria_sub", shMariaSoundOn);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_maria_sub", MariaCheckSoundLower);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_maria_sub", MariaCheckSoundUpper);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_maria_sub", mar_sub_flg_check);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_maria_sub", mar_muteki_set);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_maria_sub", mar_timer_set);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_maria_sub", mar_main_stand);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_maria_sub", mar_main_close_to);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_maria_sub", mar_main_alert);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_maria_sub", mar_main_discover);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_maria_sub", mar_main_recover);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_maria_sub", mar_main_boredom);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_maria_sub", mar_main_damaged);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_maria_sub", mar_sub_stand);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_maria_sub", mar_sub_relax);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_maria_sub", mar_sub_relax_off);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_maria_sub", mar_sub_afraid);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_maria_sub", mar_sub_tired);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_maria_sub", mar_sub_onestep);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_maria_sub", mar_sub_walk);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_maria_sub", mar_sub_run);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_maria_sub", mar_sub_damage);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_maria_sub", MariaCheckJamesLook);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_maria_sub", JamesCheckMariaLook);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_maria_sub", CheckJamesLook1);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_maria_sub", CheckJamesLook2);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_maria_sub", MariaCheckLook1);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_maria_sub", MariaCheckLook2);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_maria_sub", MariaSetStraightRoot);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_maria_sub", MariaDamageMotionNo);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_maria_sub", MariaTouchJamesCheck);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_maria_sub", MariaCheckFootLine);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_maria_sub", MariaSetSearchArea);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_maria_sub", MariaSetColumn_SetTarget);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_maria_sub", MariaSetColumn_CloseToTarget);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_maria_sub", MariaSetHeightDummy);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_maria_sub", MariaSetHeight);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_maria_sub", MariaSetHitColumn);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_maria_sub", MariaCheckNeckAngle);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_maria_sub", MariaUpdateStatusInitial);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_maria_sub", MariaUpdateStatusExecPhase1);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_maria_sub", MariaUpdateStatusExecPhase2);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_maria_sub", MariaUpdateStatusExec);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_maria_sub", GetMariaPartsWorldMatrix);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_maria_sub", MariaCheckDamage);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_maria_sub", MariaCheckSetParameterPhase1);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_maria_sub", MariaCheckSetParameterPhase2);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_maria_sub", MariaCheckControl);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_maria_sub", maria_anim_set_all);
+
+INCLUDE_RODATA("asm/nonmatchings/Chacter/m3_maria_sub", @1467);
+
+INCLUDE_RODATA("asm/nonmatchings/Chacter/m3_maria_sub", @1468);
+
+INCLUDE_RODATA("asm/nonmatchings/Chacter/m3_maria_sub", @1541_0x00397F60);
+
+INCLUDE_RODATA("asm/nonmatchings/Chacter/m3_maria_sub", @1542_0x00397F90);
+
+INCLUDE_RODATA("asm/nonmatchings/Chacter/m3_maria_sub", @1543);
+
+INCLUDE_RODATA("asm/nonmatchings/Chacter/m3_maria_sub", @1544_0x00398000);
+
+INCLUDE_RODATA("asm/nonmatchings/Chacter/m3_maria_sub", @1545_0x00398050);
+
+INCLUDE_RODATA("asm/nonmatchings/Chacter/m3_maria_sub", @1546_0x00398080);
+
+INCLUDE_RODATA("asm/nonmatchings/Chacter/m3_maria_sub", @1547_0x003980A0);
+
+INCLUDE_RODATA("asm/nonmatchings/Chacter/m3_maria_sub", @1548_0x003980D0);
+
+INCLUDE_RODATA("asm/nonmatchings/Chacter/m3_maria_sub", @1817);
+
+INCLUDE_RODATA("asm/nonmatchings/Chacter/m3_maria_sub", @1818);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_maria_sub", MariaCheckAnime);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_maria_sub", MariaUpdatePosition);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_maria_sub", MariaCheckSound);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_maria_sub", MariaInitOnConnect);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_maria_sub", MariaSetHeightConnectWait);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_maria_sub", MariaStatusClear);
