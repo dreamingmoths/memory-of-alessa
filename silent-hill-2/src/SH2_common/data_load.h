ff0644fbdfc1da54104e822e3ae5e156f682c2b8
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-07-12 16:56:27 +0200

    update `data_load.h`

--- silent-hill-2/src/SH2_common/data_load.h
+++ silent-hill-2/src/SH2_common/data_load.h
@@ -6,4 +6,7 @@
 int DataLoadMessage(int msg /* r2 */);
 void shMemCopy(void* ds /* r18 */, void* sr /* r17 */, int datasize /* r16 */);
 
+extern u_short msg_buffer[32768]; // size: 0x10000, address: 0x11B7040
+extern u_short msg_station[2048]; // size: 0x1000, address: 0x11C7040
+
 #endif // DATA_LOAD_H
