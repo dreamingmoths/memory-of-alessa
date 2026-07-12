3454441c425bb0697b83dc4e03dde77f57d37d40
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-07-12 18:45:03 +0200

    match `m3_red.c`

--- silent-hill-2/src/Chacter/m3_red.c
+++ silent-hill-2/src/Chacter/m3_red.c
@@ -4,12 +4,13 @@
 #include "Enemy/en_common.h"
 #include "Enemy/en_red.h"
 #include "libvu0.h"
+#include "vec.h"
 
 #define D_RED_ANIM_START 6300
 
 // @todo: general clean up
 
-// @todo: migrate data
+// @todo: migrate rodata
 
 extern /* static */ AnimeInfo red_anim[12]; // size: 0x90, address: 0x0
 extern /* static */ AnimeInfo d_red_anim[7]; // size: 0x54, address: 0x0
@@ -129,4 +130,61 @@ int shCharacterEnemyREDAnimeSetP(SubCharacter* scp /* r17 */, int anime_id /* r1
     return -1;
 }
 
-INCLUDE_ASM("asm/nonmatchings/Chacter/m3_red", shGetEnemyREDAttackPos);
+
+
+void shGetEnemyREDAttackPos(SubCharacter* scp /* r2 */, float* s_pos /* r18 */, float* s_vec /* r17 */, u_short atk /* r2 */) {
+    sceVu0FVECTOR pos0;
+    sceVu0FVECTOR pos1;
+    sceVu0FVECTOR vec;
+    sceVu0FMATRIX lw_mat;
+    Matrix4 mat;
+    int i;
+    int sk_num;
+    shSkelton* stp;
+    sceVu0FVECTOR wep_range = { 500.0f, 0.0f, 0.0f, 0.0f };
+   
+    lw_mat = scp->mat;
+    stp = scp->sk_top;
+    
+    switch (atk) {
+        case 0x2C:
+        case 0x2D:
+            sk_num = 0x24;
+            break;
+        case 0x2E:
+        case 0x2F:
+        case 0x30:
+            sk_num = 0x19;
+            break;
+    }
+        
+    for (i = 0; i < sk_num; i++) {
+        stp = stp->next;
+    }
+    mat = stp->src_m;
+    pos0[0] = stp->src_m.d[3][0];
+    pos0[1] = stp->src_m.d[3][1];
+    pos0[2] = stp->src_m.d[3][2];
+    pos0[3] = stp->src_m.d[3][3];
+    mat.d[3][0] = 0.0f;
+    mat.d[3][3] = 1.0f;
+    
+    sceVu0ApplyMatrix(&pos0[0], &lw_mat[0], &pos0[0]);
+    volatile_vec_copy(s_pos, pos0);
+   
+    sceVu0ApplyMatrix(&pos1[0], &mat.d[0], &wep_range[0]);
+    
+    pos1[0] += stp->src_m.d[3][0];
+    pos1[1] += stp->src_m.d[3][1];
+    pos1[2] += stp->src_m.d[3][2];
+    pos1[3] = stp->src_m.d[3][3];
+    
+    sceVu0ApplyMatrix(&pos1[0], &lw_mat[0], &pos1[0]);
+    
+    vec[0] = pos1[0] - pos0[0];
+    vec[1] = pos1[1] - pos0[1];
+    vec[2] = pos1[2] - pos0[2];
+    vec[3] = 0.0f;
+    
+    vec_normalize(vec, s_vec);    
+}
