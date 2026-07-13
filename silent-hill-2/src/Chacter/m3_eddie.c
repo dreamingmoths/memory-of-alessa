ea11f2a4e0fe4e7a0fc103c2cb7b2531322ab594
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-07-13 17:55:48 +0200

    Update m3_eddie.c

--- silent-hill-2/src/Chacter/m3_eddie.c
+++ silent-hill-2/src/Chacter/m3_eddie.c
@@ -2,13 +2,6 @@
 #include "Chacter/chara_list.h"
 #include "Chacter/m3_sc.h"
 
-#define EDDIE_DRAMA_ANIME_ID_START 4500
-#define EDDIE_PLAY_ANIME_ID_START  4800
-
-#define EDDIE_PLAY_ANIM_4801       4801
-#define EDDIE_PLAY_ANIM_4802       4802
-#define EDDIE_PLAY_ANIM_4803       4803
-
 // @todo: migrate data
 
 extern /* static */ AnimeInfo eddie_anim[28]; // size: 0x150, address: 0x38A940
