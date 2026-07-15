4deda3dde66fb689f7cf7bb74c5e14d0ed6c4037
moth dreams <mothdreams666@gmail.com>
2026-07-15 00:39:23 -0700

    matching

--- silent-hill-2/src/Enemy/en_bos.c
+++ silent-hill-2/src/Enemy/en_bos.c
@@ -20,6 +20,16 @@
 #define ENEMY_BOS_STATE_DEAD     5
 
 #define ENEMY_BOS_ANIME_5801     5801
+#define ENEMY_BOS_ANIME_5801     5801
+#define ENEMY_BOS_ANIME_5802     5802
+#define ENEMY_BOS_ANIME_5803     5803
+#define ENEMY_BOS_ANIME_5804     5804
+#define ENEMY_BOS_ANIME_5805     5805
+#define ENEMY_BOS_ANIME_5806     5806
+#define ENEMY_BOS_ANIME_5808     5808
+#define ENEMY_BOS_ANIME_5807     5807
+#define ENEMY_BOS_ANIME_5809     5809
+
 #define ENEMY_BOS_SIGNS_SOUND    18862
 #define ENEMY_BOS_MOTH_COUNT     32
 
@@ -44,9 +54,27 @@ static void enBOSAnimeExec(EnLOCAL_DATA* dp);
 static float enBOSGetMoveSpeed(void);
 static void enBOSSoundSigns(EnLOCAL_DATA* dp);
 
-extern /* static */ EnANIME_DATA EnBOSAnime[10]; // size: 0x28, address: 0x2A9A60
-extern /* static */ float bed_pos[4];            // size: 0x10, address: 0x2A9AF0
-extern /* static */ float stair_pos[4];          // size: 0x10, address: 0x2A9B00
+// size: 0x28, address: 0x2A9A60
+extern /* static */ EnANIME_DATA EnBOSAnime[10] = {
+    { ENEMY_BOS_ANIME_5801, false, },
+    { ENEMY_BOS_ANIME_5801, true,  },
+    { ENEMY_BOS_ANIME_5802, false, },
+    { ENEMY_BOS_ANIME_5803, false, },
+    { ENEMY_BOS_ANIME_5804, true,  },
+    { ENEMY_BOS_ANIME_5805, false, },
+    { ENEMY_BOS_ANIME_5806, false, },
+    { ENEMY_BOS_ANIME_5808, false, },
+    { ENEMY_BOS_ANIME_5807, false, },
+    { ENEMY_BOS_ANIME_5809, true   },
+};
+
+// @note inlines needed for match
+static inline float enBOSSize(/* no void */) {
+    return 250.0f;
+}
+static inline float enBOSTall(/* no void */) {
+    return 800.0f;
+}
 
 void enBOSInitData(EnLOCAL_DATA* dp /* r17 */) {
     float vitarity[5] = {
@@ -60,7 +88,7 @@ void enBOSInitData(EnLOCAL_DATA* dp /* r17 */) {
 
     ENEMY_MLV(1);
     enSetHP(dp, vitarity[mode], vitarity[mode]);
-    enSetSize(dp, 250.0f, 800.0f, 100.0f, -150.0f);
+    enSetSize(dp, enBOSSize(), enBOSTall(), 100.0f, -150.0f);
     dp->weight = 2;
     dp->flag |= (1 << 10) | (1 << 1);
     if (mode < 3) {
@@ -83,7 +111,8 @@ void enBOSCtrlMain(EnLOCAL_DATA* dp /* r2 */) {
         enBOSCtrlAutomatic,
         enDummyCtrl,
         enBOSCtrlGoPlayable,
-        enDummyCtrl
+        enDummyCtrl,
+        enBOSCtrlHand
     }; // r29+0x10
     enCtrlBOSFunc[dp->mlv](dp);
 }
@@ -101,7 +130,7 @@ static void enBOSCtrlAutomatic(EnLOCAL_DATA* dp /* r16 */) {
     enSetBattleTarget(dp, 1);
     enCtrlBOSSubFunc[dp->slv](dp);
     enBOSAnimeExec(dp);
-    if (dp->slv < 5) {
+    if (dp->slv < ENEMY_BOS_STATE_DEAD) {
         enBOSMoveExec(dp);
         enBOSSoundSigns(dp);
         fogSetStayPoint(&dp->scp->pos);
@@ -121,15 +150,6 @@ static void enBOSCtrlHand(void) {
     /* empty */
 }
 
-static inline void vec_add_reverse(void* x, void* y, void* out) {
-    asm("\
-        lqc2 vf4, 0(%1)\n\
-        lqc2 vf5, 0(%0)\n\
-        vadd.xyzw vf4, vf4, vf5\n\
-        sqc2 vf4, 0(%2)"
-        : "+r"(x), "+r"(y), "+r"(out));
-}
-
 static void enBOSCtrlApproach(EnLOCAL_DATA* dp /* r16 */) {
     sceVu0FVECTOR pos, vec; // r29+0x20, r29+0x30
     float a;
@@ -145,15 +165,15 @@ static void enBOSCtrlApproach(EnLOCAL_DATA* dp /* r16 */) {
             ENEMY_NEXT_SUB_STEP();
         }
 
-        shSinCosV_Scale(&vec, enGetPlayerAngle(dp), 600.0f);
-        vec_add_reverse(vec, enGetPlayerPos(dp), pos);
+        shSinCosV_Scale(vec, enGetPlayerAngle(dp), 600.0f);
+        vu0_add_vector(pos, enGetPlayerPos(dp), vec);
         if (enCheckPlayerHitEyes(dp, pos) >= 0.0f) {
-            vec_sub_reverse(vec, enGetPlayerPos(dp), pos);
+            vu0_sub_vector(pos, enGetPlayerPos(dp), vec);
         }
 
         a                 = enCalcDirection(pos, &dp->scp->pos);
         a                 = shAngleRegulate(a - dp->path.angle);
-        dp->bos.rot_speed = 0.05f * a;
+        dp->bos.rot_add   = 0.05f * a;
 
         switch (enBOSCanAttackPlayer(dp)) {
             case 1:
@@ -169,19 +189,15 @@ static void enBOSCtrlApproach(EnLOCAL_DATA* dp /* r16 */) {
                 break;
 
             default:
-                if (dp->p_dist >= 1500.0f && enCheckIntoScreen(dp)) {
-                    if (dp->bos.insect_dp) {
-                        if (*(s_char*) dp->bos.insect_dp == 0) {
-                            dp->bos.insect_dp = NULL;
-                        } else {
-                            break;
-                        }
-                    }
-                    ENEMY_STEP(ENEMY_BOS_STATE_GENERATE);
+                if (dp->p_dist < 1500.0f || !enCheckIntoScreen(dp)) break;
+                if (dp->bos.insect_dp) {
+                    if (((EnLOCAL_DATA*)dp->bos.insect_dp)->kind != NULL) break;
+                    dp->bos.insect_dp = NULL;
                 }
-                enBOSCheckNearPlayer(dp);
+                ENEMY_STEP(ENEMY_BOS_STATE_GENERATE);
                 break;
         }
+        enBOSCheckNearPlayer(dp);
     }
 }
 
@@ -210,7 +226,7 @@ static void enBOSCtrlDistance(EnLOCAL_DATA* dp /* r16 */) {
             }
 
             shSinCosV_Scale(&vec, enGetPlayerDirection(dp), 1500.0f);
-            vec_sub_reverse(vec, enGetPlayerPos(dp), pos);
+            vu0_sub_vector(pos, enGetPlayerPos(dp), vec);
             dp->bos.rot_add = 0.05f * shAngleRegulate(enCalcDirection(&pos, &dp->scp->pos.x) - dp->path.angle);
             if ((enReduceTimer(dp) <= 0) || (enBOSCanAttackPlayer(dp) != 0)) {
                 ENEMY_STEP(ENEMY_BOS_STATE_APPROACH);
@@ -235,7 +251,7 @@ static void enBOSCtrlGenerate(EnLOCAL_DATA* dp /* r17 */) {
         return;
 
     switch (dp->sslv) {
-        case 0:
+        case ENEMY_BOS_STATE_APPROACH:
             if ((dp->anim != 8) && (dp->anim != 1)) {
                 enBOSAnimeSet(dp, 1);
             }
@@ -264,7 +280,7 @@ static void enBOSCtrlGenerate(EnLOCAL_DATA* dp /* r17 */) {
             ENEMY_NEXT_SUB_STEP();
             break;
 
-        case 1:
+        case ENEMY_BOS_STATE_DISTANCE:
             if (tp->slv == 1) {
                 enAttackCheck(dp, 62);
                 if (!(dp->flag & 4)) {
@@ -275,14 +291,14 @@ static void enBOSCtrlGenerate(EnLOCAL_DATA* dp /* r17 */) {
                 ENEMY_STEP(ENEMY_BOS_STATE_DISTANCE);
             }
             break;
-        case 2:
+        case ENEMY_BOS_STATE_GENERATE:
             enAttackCheckHug(dp, 63);
             if (enCheckHuggedPlayer() == 0) {
                 tp->slv = 2;
                 ENEMY_NEXT_SUB_STEP();
             }
             break;
-        case 3:
+        case ENEMY_BOS_STATE_ATTACK:
             if (enBOSCanAttackPlayer(dp) == 2) {
                 if (enBOSCheckFloor(dp) == 0) {
                     ENEMY_STEP(ENEMY_BOS_STATE_ATTACK);
@@ -293,15 +309,15 @@ static void enBOSCtrlGenerate(EnLOCAL_DATA* dp /* r17 */) {
             }
             break;
     }
-    shSinCosV_Scale(&vec, enGetPlayerAngle(dp), 600.0f);
-    vec_add_reverse(&vec, enGetPlayerPos(dp), &pos);
+    shSinCosV_Scale(vec, enGetPlayerAngle(dp), 600.0f);
+    vu0_add_vector(pos, enGetPlayerPos(dp), vec);
     if (enCheckPlayerHitEyes(dp, &pos) >= 0.0f) {
-        enGetPlayerPos(dp);
-        vec_sub_reverse(enGetPlayerPos(dp), &vec, &pos);
+        vu0_sub_vector(pos, enGetPlayerPos(dp), vec);
     }
-    a               = enCalcDirection(&pos, &dp->scp->pos) - dp->path.angle;
-    dp->bos.rot_add = 0.05f * shAngleRegulate(a);
-    if (*(s_char*) dp->bos.insect_dp == 0) {
+    a = enCalcDirection(&pos, &dp->scp->pos);
+    a = shAngleRegulate(a - dp->path.angle);
+    dp->bos.rot_add = 0.05f * a;
+    if (tp->kind == 0) {
         ENEMY_STEP(ENEMY_BOS_STATE_APPROACH);
         dp->bos.mode      = 0.0f;
         dp->bos.insect_dp = NULL;
@@ -315,43 +331,43 @@ static void enBOSCtrlAttack(EnLOCAL_DATA* dp /* r16 */) {
     if (enCheckDamage(dp)) {
         if (enCheckHuggedPlayer()) {
             enResetDamage(dp);
-        } else if (enBOSCheckDamage(dp) != 2) {
+        } else {
+            if (enBOSCheckDamage(dp) == 2) return;
             ENEMY_STEP(ENEMY_BOS_STATE_DISTANCE);
             return;
         }
     }
 
     switch (dp->sslv) {
-        case 0:
+        case ENEMY_BOS_STATE_APPROACH:
             enBOSAnimeSet(dp, 2);
             enAttackStart(dp);
             dp->flag |= 1 << 15;
             enFlagSetCritical(dp);
-            dp->bos.mode = 0;
+            dp->bos.mode = 2;
             ENEMY_NEXT_SUB_STEP();
             return;
 
-        case 1:
+        case ENEMY_BOS_STATE_DISTANCE:
             shSinCosV_Scale(&vec, enGetPlayerAngle(dp), 600.0f);
 
-            vec_add(enGetPlayerPos(dp), &vec, &vec);
-            vec_sub_reverse(&vec, dp->vec, &dp->scp->pos);
+            vu0_add_vector(vec, enGetPlayerPos(dp), vec);
+            vu0_sub_vector(dp->vec, vec, &dp->scp->pos);
             dp->vec[3] = 0.0f;
             dp->vec[1] = 0.0f;
             enAttackCheck(dp, 59);
-            if (dp->anim_n == -1) {
-                if (dp->flag & (1 << 2)) {
-                    enBOSAnimeSet(dp, 6);
-                    ENEMY_SUB_STEP(4);
-                    return;
-                }
-                enBOSAnimeSet(dp, 3);
-                enAttackStart(dp);
-                ENEMY_NEXT_SUB_STEP();
+            if (dp->anim_n != -1) return;
+            if (dp->flag & (1 << 2)) {
+                enBOSAnimeSet(dp, 6);
+                ENEMY_SUB_STEP(4);
+                return;
             }
+            enBOSAnimeSet(dp, 3);
+            enAttackStart(dp);
+            ENEMY_NEXT_SUB_STEP();
             return;
 
-        case 2:
+        case ENEMY_BOS_STATE_GENERATE:
             if (dp->anim_n == -1) {
                 enBOSAnimeSet(dp, 4);
                 dp->flag &= 0xFFFF7FFF;
@@ -360,7 +376,7 @@ static void enBOSCtrlAttack(EnLOCAL_DATA* dp /* r16 */) {
             }
             break;
 
-        case 3:
+        case ENEMY_BOS_STATE_ATTACK:
             enAttackCheckHug(dp, 60);
             if (enCheckHuggedPlayer() == 0) {
                 enBOSAnimeSet(dp, 5);
@@ -369,7 +385,7 @@ static void enBOSCtrlAttack(EnLOCAL_DATA* dp /* r16 */) {
             }
             break;
 
-        case 4:
+        case ENEMY_BOS_STATE_ATTACK2:
             if (dp->anim_n == -1) {
                 ENEMY_STEP(ENEMY_BOS_STATE_DISTANCE);
                 dp->flag &= 0xFFFF7FFF;
@@ -387,13 +403,13 @@ static void enBOSCtrlAttack2(EnLOCAL_DATA* dp /* r16 */) {
         }
     } else {
         switch (dp->sslv) {
-            case 0:
+            case ENEMY_BOS_STATE_APPROACH:
                 enBOSAnimeSet(dp, 7);
                 enAttackStart(dp);
                 enFlagSetCritical(dp);
                 ENEMY_NEXT_SUB_STEP();
                 /* fallthrough */
-            case 1:
+            case ENEMY_BOS_STATE_DISTANCE:
                 enAttackCheck(dp, 61);
                 if (dp->anim_n == -1) {
                     ENEMY_STEP(ENEMY_BOS_STATE_APPROACH);
@@ -407,7 +423,7 @@ static void enBOSCtrlAttack2(EnLOCAL_DATA* dp /* r16 */) {
 
 static void enBOSCtrlDead(EnLOCAL_DATA* dp /* r16 */) {
     if (dp->scp->en_first_status == 13) {
-        if (dp->sslv == 1) {
+        if (dp->sslv == ENEMY_BOS_STATE_DISTANCE) {
             if (enReduceTimer(dp) <= 0) {
                 enResetFilter();
                 SET_GAME_FLAG(GAME_FLAG_508);
@@ -422,7 +438,7 @@ static void enBOSCtrlDead(EnLOCAL_DATA* dp /* r16 */) {
             ENEMY_NEXT_SUB_STEP();
         }
     } else {
-        if (dp->sslv == 0) {
+        if (dp->sslv == ENEMY_BOS_STATE_APPROACH) {
             enSetTimer(dp, 60);
             enSetBlur();
             dp->flag |= 1 << 3;
@@ -508,6 +524,22 @@ static int enBOSCheckDamage(EnLOCAL_DATA* dp /* r16 */) {
     return 1;
 }
 
+// size: 0x10, address: 0x2A9AF0
+static float bed_pos[4] = {
+    58900.0f,
+    -16000.0f,
+    58800.0f,
+    800.0f,
+};
+
+// size: 0x10, address: 0x2A9B00
+static float stair_pos[4] = {
+    60900.0f,
+    -16000.0f,
+    55600.0f,
+    900.0f,
+};
+
 static int enBOSCheckFloor(EnLOCAL_DATA* dp /* r2 */) {
     if (vec3_dist_xz(bed_pos, &dp->scp->pos) < bed_pos[3] || vec3_dist_xz(stair_pos, &dp->scp->pos) < stair_pos[3]) {
         return true;
@@ -519,17 +551,18 @@ static void enBOSCheckNearPlayer(EnLOCAL_DATA* dp /* r2 */) {
     enCheckNearPlayer(dp, &dp->bos.near_count, &dp->bos.dist, 200.0f);
 }
 
+
 static void enBOSMoveExec(EnLOCAL_DATA* dp /* r17 */) {
     float add;               // r20
     float d;                 // r21
     float s;                 // r29+0x50
     int mode = dp->bos.mode; // r16
     EnPATH_DATA tpath;       // r29+0x40
-
+    float sway = -250.0f;    // @hack not in dwarf, needed for match
     if (mode < 2) {
         dp->bos.rot_speed  = (0.95f * dp->bos.rot_speed) + dp->bos.rot_add;
         dp->path.markangle = dp->path.angle + dp->bos.rot_speed;
-        enMoveAngle(&dp->path, 0.034906585f);
+        enMoveAngle(&dp->path, TO_RAD(2));
         add = 250.0f * shGetDT();
         d   = dp->bos.move_speed;
         if (dp->anim == 8) {
@@ -552,15 +585,15 @@ static void enBOSMoveExec(EnLOCAL_DATA* dp /* r17 */) {
         shSinCosV_Scale(dp->vec, dp->path.angle, d);
     }
     tpath.markangle = enGetPlayerDirection(dp);
-    tpath.angle     = dp->path.angle;
-    enMoveAngle(&tpath, 0.017453292f);
-    dp->path.angle = tpath.angle;
+    tpath.angle     = dp->scp->rot.y;
+    enMoveAngle(&tpath, TO_RAD(1));
+    dp->scp->rot.y  = tpath.angle;
     if (mode != 0) {
         s = (-17500.0f - (dp->scp->pos).y) * 0.2f;
     } else {
-        s = shSway1f(-250.0f, 0.1f);
+        s = shSway1f(sway, 0.1f);
     }
-    dp->bos.y_speed = 0.98f * s;
+    dp->bos.y_speed = ((dp->bos.y_speed + s) - ((dp->scp->pos).y - (-17500.0f)) * 0.01f) * 0.98f;
     if (((dp->scp->pos.y < -17700.0f) && (dp->bos.y_speed < 0.0f)) || (dp->scp->pos.y >= -17300.0f && dp->bos.y_speed > 0.0f)) {
         dp->bos.y_speed *= 0.5f;
     }
@@ -576,7 +609,7 @@ static void enBOSAnimeSet(EnLOCAL_DATA* dp /* r17 */, int anim /* r16 */) {
         return;
     }
     FJ_ASSERT_ON_LINE(anim >= 0 && anim < sizeof(EnBOSAnime) / sizeof(EnANIME_DATA), 725);
-    enAnimeSet(dp, anim, (s32) EnBOSAnime[anim].Anime);
+    enAnimeSet(dp, anim, EnBOSAnime[anim].Anime);
 }
 
 static void enBOSAnimeExec(EnLOCAL_DATA* dp /* r16 */) {
@@ -589,19 +622,24 @@ static void enBOSAnimeExec(EnLOCAL_DATA* dp /* r16 */) {
             dp->anim_s = 1024;
             return;
         }
-        dp->anim_s = 1024;
+        dp->anim_s = 4096;
     }
 }
 
 static float enBOSGetMoveSpeed(void) {
     float speed_rate[5] = {
-        0.69f,
+        0.7f,
         0.8f,
         1.0f,
         1.5f,
         2.0f,
     }; // r29+0x10
     return 500.0f * speed_rate[enGetMode()];
+}
+
+// @note inline needed for match
+static inline float enBOSSoundVolume(void) {
+    return 0.4f;
 }
 
 static void enBOSSoundSigns(EnLOCAL_DATA* dp /* r16 */) {
@@ -614,9 +652,9 @@ static void enBOSSoundSigns(EnLOCAL_DATA* dp /* r16 */) {
         } while (signs == dp->bos.signs);
         dp->bos.signs = signs;
         if (signs) {
-            enSoundCall(signs + ENEMY_BOS_SIGNS_SOUND, 0.4f, &dp->scp->pos);
+            enSoundCall(signs + ENEMY_BOS_SIGNS_SOUND, enBOSSoundVolume(), &dp->scp->pos);
             return;
         }
-        enSoundCall3D(ENEMY_BOS_SIGNS_SOUND, 0.4f, &dp->scp->pos);
+        enSoundCall3D(ENEMY_BOS_SIGNS_SOUND, enBOSSoundVolume(), &dp->scp->pos);
     }
 }
