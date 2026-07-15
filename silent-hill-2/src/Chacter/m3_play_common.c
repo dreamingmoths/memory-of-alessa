57b269d061e75f51ac35a918a7f2fc8592e10627
moth dreams <mothdreams666@gmail.com>
2026-07-14 22:22:22 -0700

    begin `m3_play_common`

---
+++ silent-hill-2/src/Chacter/m3_play_common.c
@@ -0,0 +1,95 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_play_common", PlayerCheckHuggedPos);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_play_common", PlayerSetDT);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_play_common", PlayerUpdateStatus);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_play_common", PlayerCheckKeyLStickTrg);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_play_common", PlayerCheckHuggingButton);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_play_common", PlayerSpeedDownToStand);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_play_common", PlayerSetAttackWithWalkIsOk);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_play_common", PlayerSetAttackWithRunIsOk);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_play_common", PlayerCheckAimingToEnemy);
+
+INCLUDE_RODATA("asm/nonmatchings/Chacter/m3_play_common", @1221_0x00396920);
+
+INCLUDE_RODATA("asm/nonmatchings/Chacter/m3_play_common", @1222_0x00396950);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_play_common", PlayerCheckAttack);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_play_common", lower_stand);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_play_common", upper_stand);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_play_common", lower_relax);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_play_common", upper_relax);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_play_common", lower_alert);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_play_common", upper_alert);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_play_common", lower_tired);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_play_common", upper_tired);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_play_common", lower_ready);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_play_common", upper_ready);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_play_common", lower_readyoff);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_play_common", upper_readyoff);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_play_common", upper_run1);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_play_common", upper_run2);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_play_common", upper_run3);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_play_common", lower_hold);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_play_common", upper_hold);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_play_common", lower_release);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_play_common", upper_release);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_play_common", lower_attack);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_play_common", upper_attack);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_play_common", lower_kick);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_play_common", upper_kick);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_play_common", lower_fall);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_play_common", upper_fall);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_play_common", lower_damage);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_play_common", upper_damage);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_play_common", lower_to_stand);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_play_common", upper_to_stand);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_play_common", upper_wall_f);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_play_common", lower_event);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/m3_play_common", upper_event);
+
+INCLUDE_RODATA("asm/nonmatchings/Chacter/m3_play_common", @1852_0x00396A50);
+
+INCLUDE_RODATA("asm/nonmatchings/Chacter/m3_play_common", @1853);
+
+INCLUDE_RODATA("asm/nonmatchings/Chacter/m3_play_common", @1887);
