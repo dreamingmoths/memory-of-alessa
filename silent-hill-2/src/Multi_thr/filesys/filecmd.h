99dec4d13c16fb82bedc546e6948db2380cfe3e5
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-07-17 12:37:46 +0200

    update `filecmd.h`

--- silent-hill-2/src/Multi_thr/filesys/filecmd.h
+++ silent-hill-2/src/Multi_thr/filesys/filecmd.h
@@ -1,11 +1,23 @@
 #ifndef FILECMD_H
 #define FILECMD_H
 
-#include "common.h"
-#include "FilesList/fileslist_bg.h" // correct?
+#include "sh2_common.h"
+#include "data/fs_structs.h"
 
+fsFile* fsCmdCheckFixFile(fsFile* fp);
+int fsCmdCheckExistFile(fsFile* fp);
+int fsCmdSetRealFile(fsFile* realfp, fsFile* fp);
+int fsCmdDiskSelectC(void);
+int fsCmdDiskSelectCH(void);
+int fsCmdDiskSelectHC(void);
+int fsCmdExecDevSelect(int mode);
+int fsCmdFixFile(fsFile* fp);
+int fsCmdHdInit(int enable);
+int fsCmdRead(fsFile* fp, void* buf);
+int fsCmdReadPart(fsFile* fp, void* buf, int offset, int size);
+int fsCmdSetParamForCheckDisk(int media_permission, fsFile** fplist, void** buflist, int (* check_func)(fsFile* *, void* *));
+int fsCmdCdCheckDisk(int force_check);
+int fsCmdCdCheckDisk2(int force_check, int media_permission, fsFile** fplist, void** buflist, int (* check_func)(fsFile* *, void* *));
 int fsCmdGetTrayStat(void);
-int fsCmdSetRealFile(union fsFile* realfp /* r2 */, union fsFile* fp /* r2 */);
-union fsFile* fsCmdCheckFixFile(union fsFile* fp /* r16 */);
 
 #endif // FILECMD_H
