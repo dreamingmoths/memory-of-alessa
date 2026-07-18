9e88d1dae1a4057a65e9282a9a86da32ce332dc2
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-07-18 10:10:04 +0200

    update `filepath.h`

--- silent-hill-2/src/Multi_thr/filesys/filepath.h
+++ silent-hill-2/src/Multi_thr/filesys/filepath.h
@@ -1,4 +1,17 @@
 #ifndef FILEPATH_H
 #define FILEPATH_H
 
+#include "sh2_common.h"
+
+int shPathCheckExecRoot(char* filename);
+int shPathMakeIop(char* fullpath, char* filename);
+int shPathMakeCdData(char* fullpath, char* filename);
+int shPathMakeHdData(char* fullpath, char* filename);
+int shPathMakeCdDataBase(char* fullpath, char* filename);
+int shPathMakeHdDataBase(char* fullpath, char* filename);
+int ___shPathMakeCdDataBase(char* fullpath, char* filename);
+int ___shPathMakeCdData(char* fullpath, char* filename);
+int ___shPathMakeHdDataBase(char* fullpath, char* filename);
+int ___shPathMakeHdData(char* fullpath, char* filename);
+
 #endif // FILEPATH_H
