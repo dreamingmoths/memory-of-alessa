fbb48a25a153a591f9a027fe2488fe9b195bf46e
moth dreams <mothdreams666@gmail.com>
2026-07-18 14:29:16 -0700

    sh3: split `sh3gfw_GSCTL_man`

---
+++ silent-hill-3/src/GFW/sh3gfw_GSCTL_man.c
@@ -0,0 +1,13 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/GFW/sh3gfw_GSCTL_man", sh3gfw_init_GSPACKMAN);
+
+INCLUDE_ASM("asm/nonmatchings/GFW/sh3gfw_GSCTL_man", func_001CDAF0);
+
+INCLUDE_ASM("asm/nonmatchings/GFW/sh3gfw_GSCTL_man", sh3gfw_setREF_gsctl);
+
+INCLUDE_ASM("asm/nonmatchings/GFW/sh3gfw_GSCTL_man", sh3gfw_setREF_tagchain);
+
+INCLUDE_ASM("asm/nonmatchings/GFW/sh3gfw_GSCTL_man", sh3gfw_setEND_gsctl);
+
+INCLUDE_ASM("asm/nonmatchings/GFW/sh3gfw_GSCTL_man", sh3gfw_0x001cdbf0);
