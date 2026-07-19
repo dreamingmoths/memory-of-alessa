a9e8843ac11bd2b297698f3d73ee928721f76d4c
moth dreams <mothdreams666@gmail.com>
2026-07-18 19:04:45 -0700

    sh3: update `GSCTL_man`
    ---------
    
    Co-authored-by: Mc-muffin <Mc-muffin@users.noreply.github.com>

--- silent-hill-3/src/GFW/sh3gfw_GSCTL_man.c
+++ silent-hill-3/src/GFW/sh3gfw_GSCTL_man.c
@@ -1,19 +1,16 @@
 #include "common.h"
+#include "sce/libgraph.h"
 #include "GFW/sh3gfw_GSCTL_man.h"
 
-/**
- * @note: this file is unfinished!
- */
-
-#define GSCTL_man Gman
 extern Q_WORDDATA GSENV_CTL_PACK[1024];
 extern G_MAN GSCTL_man;
 
+// @sh3 in the sh2 proto the G-man was passed in as a pointer
 void sh3gfw_init_GSPACKMAN(void) {
-    Gman.main_num = 0;
-    Gman.transnum = 0;
-    Gman.GS_kick  = GSENV_CTL_PACK;
-    Gman.GS_tail  = GSENV_CTL_PACK;
+    GSCTL_man.main_num = 0;
+    GSCTL_man.transnum = 0;
+    GSCTL_man.GS_kick  = GSENV_CTL_PACK;
+    GSCTL_man.GS_tail  = GSENV_CTL_PACK;
     return;
 }
 
@@ -46,6 +43,7 @@ void sh3gfw_setREF_tagchain(Q_WORDDATA** pqwd, Q_WORDDATA* gt) {
     *pqwd        = qwd + 1;
 }
 
+#ifdef NON_MATCHING
 Q_WORDDATA* sh3gfw_setEND_gsctl(void) {
     Q_WORDDATA* qwd = GSCTL_man.GS_tail;
 
@@ -57,26 +55,29 @@ Q_WORDDATA* sh3gfw_setEND_gsctl(void) {
     GSCTL_man.GS_kick = GSCTL_man.GS_tail;
     return qwd;
 }
+#else
+INCLUDE_ASM("asm/nonmatchings/GFW/sh3gfw_GSCTL_man", sh3gfw_setEND_gsctl);
+#endif
 
-#define SCE_VIF1_SET_FLUSH(irq) (((u_int)0x11 << 24) | ((u_int)(irq) << 31))
-
-#define SCE_VIF1_SET_DIRECT(count, irq) ((u_int)(count) | ((u_int)(0x50) << 24) | ((u_int)(irq) << 31))
+static inline void set_giftag(Q_WORDDATA* qwd, u_long arg0, u_long arg1) {
+    qwd->ui32[3] = arg1 >> 0x20;
+    qwd->ui32[2] = arg1;
+    qwd->ui32[1] = arg0 >> 0x20;
+    qwd->ui32[0] = arg0;
+}
 
 /* perhaps: sh3gfw_setCOUNT_TEXFLUSH */
 /* was a reftag in sh2 */
 void sh3gfw_0x001cdbf0(void) {
     Q_WORDDATA* qwd = GSCTL_man.GS_tail;
     qwd->ui32[0]    = DMAcnt | 2;
-    qwd->ui32[1]    = 0 /* (u_int) shGs_AllEnv.GsSync_DummyTEXFLUSH */;
+    qwd->ui32[1]    = 0 /* @sh3: in the sh2 proto, (u_int) shGs_AllEnv.GsSync_DummyTEXFLUSH */;
     qwd->ui32[2]    = SCE_VIF1_SET_FLUSH(0);
     qwd->ui32[3]    = SCE_VIF1_SET_DIRECT(2, 0);
 
-    // @todo: add macros
-    qwd[1].ui32[3]    = 0;
-    qwd[1].ui32[2]    = 0xE;
-    qwd[1].ui32[1]    = DMAcnt;
-    qwd[1].ui32[0]    = 0x8001;
-    qwd[2].ul64[1]    = 0x3F;
+    // thanks ethanol for the macros :)
+    set_giftag(&qwd[1], SCE_GIF_SET_TAG(1, SCE_GS_TRUE, 0, 0, SCE_GIF_PACKED, 1), 0xE);
+    qwd[2].ul64[1]    = SCE_GS_TEXFLUSH;
     qwd[2].ul64[0]    = 0;
     GSCTL_man.GS_tail = qwd + 3;
 }
