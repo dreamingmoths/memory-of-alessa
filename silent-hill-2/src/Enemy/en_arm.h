6697096d86f9f7e979ec17496a67cabcba41660d
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-07-20 17:45:00 +0200

    Update en_arm.h

--- silent-hill-2/src/Enemy/en_arm.h
+++ silent-hill-2/src/Enemy/en_arm.h
@@ -1,4 +1,11 @@
 #ifndef EN_ARM_H
 #define EN_ARM_H
 
+#include "sh2_common.h"
+#include "Enemy/en_common.h"
+
+void enARMInitData(EnLOCAL_DATA* dp);
+void enARMCtrlMain(EnLOCAL_DATA* dp);
+void enARMGetHandPos(float* vec, EnLOCAL_DATA* dp, int d);
+
 #endif // EN_ARM_H
