2f5332a0e330c1ee3d8af0cbf88d64defad48b35
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-07-26 12:49:06 +0200

    update `m3_edb.h`

--- silent-hill-2/src/Chacter/m3_edb.h
+++ silent-hill-2/src/Chacter/m3_edb.h
@@ -1,4 +1,12 @@
 #ifndef M3_EDB_H
 #define M3_EDB_H
 
+#include "sh2_common.h"
+#include "Chacter/character.h"
+
+void shCharacterSetEnemyEDBLow(SubCharacter* scp);
+int shCharacterEnemyEDBAnimeSet(SubCharacter* scp, int anime_id);
+int shCharacterEnemyEDBAnimeSetP(SubCharacter* scp, int anime_id, int comp);
+void shGetEnemyEDBAttackPos(SubCharacter* scp, float* s_pos, float* s_vec, u_short atk);
+
 #endif // M3_EDB_H
