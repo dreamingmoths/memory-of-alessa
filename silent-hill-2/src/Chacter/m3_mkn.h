ec7298986fd5ed92f3b6bd78d04ca8e5d1edfcfd
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-07-26 12:42:28 +0200

    match `m3_mkn.h`

--- silent-hill-2/src/Chacter/m3_mkn.h
+++ silent-hill-2/src/Chacter/m3_mkn.h
@@ -1,4 +1,13 @@
 #ifndef M3_MKN_H
 #define M3_MKN_H
 
+#include "sh2_common.h"
+#include "Chacter/character.h"
+
+void shCharacterSetEnemyMKNLow(SubCharacter* scp);
+int shCharacterEnemyMKNAnimeSet(SubCharacter* scp, int anime_id);
+int shCharacterEnemyMKNAnimeSetP(SubCharacter* scp, int anime_id, int comp);
+void shGetEnemyMKNAttackPos(SubCharacter* scp, float* s_pos, float* s_vec, u_short atk);
+void shEnemyMKN_EffectFoot(SubCharacter* scp, int type);
+
 #endif // M3_MKN_H
