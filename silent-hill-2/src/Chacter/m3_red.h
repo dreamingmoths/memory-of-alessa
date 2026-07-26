0a506da0bad32cdb2e4637c5119fba2dcb716a86
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-07-26 12:45:43 +0200

    update `m3_red.h`

--- silent-hill-2/src/Chacter/m3_red.h
+++ silent-hill-2/src/Chacter/m3_red.h
@@ -8,5 +8,6 @@ void shCharacterSetEnemyREDLow(SubCharacter* scp);
 void shCharacterSetWeaponRED(SubCharacter* scp, int on_off);
 int shCharacterEnemyREDAnimeSet(SubCharacter* scp, int anime_id);
 int shCharacterEnemyREDAnimeSetP(SubCharacter* scp /* r17 */, int anime_id /* r18 */, int comp /* r16 */);
+void shGetEnemyREDAttackPos(SubCharacter* scp, float* s_pos, float* s_vec, u_short atk);
 
 #endif // M3_RED_H
