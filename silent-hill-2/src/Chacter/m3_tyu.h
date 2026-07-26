86c32015f2de0ff28d81971bf4f450119b50dedb
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-07-26 12:52:22 +0200

    update `m3_tyu.h`

--- silent-hill-2/src/Chacter/m3_tyu.h
+++ silent-hill-2/src/Chacter/m3_tyu.h
@@ -1,4 +1,11 @@
 #ifndef M3_TYU_H
 #define M3_TYU_H
 
+#include "sh2_common.h"
+#include "Chacter/character.h"
+
+void shCharacterSetEnemyTYULow(SubCharacter* scp);
+int shCharacterEnemyTYUAnimeSetP(SubCharacter* scp, int anime_id, int comp);
+void shGetEnemyTYUAttackPos(SubCharacter* scp, float* s_pos, float* s_vec, u_short atk);
+
 #endif // M3_TYU_H
