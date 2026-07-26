a59b6a8789613f83f6b141f2b035a74e00ff0c82
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-07-26 12:54:40 +0200

    update `m3_ike.h`

--- silent-hill-2/src/Chacter/m3_ike.h
+++ silent-hill-2/src/Chacter/m3_ike.h
@@ -1,4 +1,12 @@
 #ifndef M3_IKE_H
 #define M3_IKE_H
 
+#include "sh2_common.h"
+#include "Chacter/character.h"
+
+void shCharacterSetEnemyIKELow(SubCharacter* scp);
+int shCharacterEnemyIKEAnimeSet(SubCharacter* scp, int anime_id);
+int shCharacterEnemyIKEAnimeSetP(SubCharacter* scp, int anime_id, int comp);
+void shGetEnemyIKEAttackPos(SubCharacter* scp, float* s_pos, float* s_vec, u_short atk); // 4th arg not present in DWARF
+
 #endif // M3_IKE_H
