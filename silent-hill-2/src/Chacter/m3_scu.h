c3d6a79786144e8a475f05e21c8eab5ea9da03da
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-07-26 12:40:09 +0200

    update `m3_scu.h`

--- silent-hill-2/src/Chacter/m3_scu.h
+++ silent-hill-2/src/Chacter/m3_scu.h
@@ -1,4 +1,13 @@
 #ifndef M3_SCU_H
 #define M3_SCU_H
 
+#include "sh2_common.h"
+#include "Chacter/character.h"
+
+void shCharacterSetEnemySCULow(SubCharacter* scp);
+int shCharacterEnemySCUAnimeSet(SubCharacter* scp, int anime_id);
+int shCharacterEnemySCUAnimeSetP(SubCharacter* scp, int anime_id, int comp);
+void shGetEnemySCUAttackPos(SubCharacter* scp, float* s_pos, float* s_vec, u_short atk);
+void shEnemySCU_EffectFoot(SubCharacter* scp, int type);
+
 #endif // M3_SCU_H
