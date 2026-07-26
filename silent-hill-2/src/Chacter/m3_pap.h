2caa7824c7718952cd243d9b16d2a2a829a90a16
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-07-26 12:51:02 +0200

    update `m3_pap.h`

--- silent-hill-2/src/Chacter/m3_pap.h
+++ silent-hill-2/src/Chacter/m3_pap.h
@@ -1,4 +1,12 @@
 #ifndef M3_PAP_H
 #define M3_PAP_H
 
+#include "sh2_common.h"
+#include "Chacter/character.h"
+
+void shCharacterSetEnemyPAPLow(SubCharacter* scp);
+int shCharacterEnemyPAPAnimeSet(SubCharacter* scp, int anime_id);
+int shCharacterEnemyPAPAnimeSetP(SubCharacter* scp, int anime_id, int comp);
+void shGetEnemyPAPAttackPos(SubCharacter* scp, float* s_pos, float* s_vec, u_short atk);
+
 #endif // M3_PAP_H
