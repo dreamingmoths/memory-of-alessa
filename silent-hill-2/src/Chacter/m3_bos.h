046d10cc1b1dd031575788bd5ebee283a03f892d
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-07-26 12:38:30 +0200

    update `m3_bos.h`

--- silent-hill-2/src/Chacter/m3_bos.h
+++ silent-hill-2/src/Chacter/m3_bos.h
@@ -1,4 +1,12 @@
 #ifndef M3_BOS_H
 #define M3_BOS_H
 
+#include "sh2_common.h"
+#include "Chacter/character.h"
+
+void shCharacterSetEnemyBOSLow(SubCharacter* scp);
+int shCharacterEnemyBOSAnimeSet(SubCharacter* scp, int anime_id);
+int shCharacterEnemyBOSAnimeSetP(SubCharacter* scp, int anime_id, int comp);
+void shGetEnemyBOSAttackPos(SubCharacter* scp, float* s_pos, float* s_vec, u_short atk);
+
 #endif // M3_BOS_H
