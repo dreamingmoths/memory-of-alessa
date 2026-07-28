44b923c9f57e997b8ee67e10098338e32800ce78
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-07-28 18:15:40 +0200

    update `sh2_get_drawenv.h`

--- silent-hill-2/src/GFW/sh2_get_drawenv.h
+++ silent-hill-2/src/GFW/sh2_get_drawenv.h
@@ -1,6 +1,8 @@
 #ifndef SH2_GET_DRAWENV_H
 #define SH2_GET_DRAWENV_H
 
+void kari_sh2gde_getspotParams(float* spotpos /* r18 */, float* spotdir /* r17 */, float* decayparms /* r16 */);
+
 void sh2gde_getWorldScreenMatrix(float (* wsm)[4]);
 
 void sh2gde_getWorldViewMatrix(float (* wsm)[4]);
