cdb95100c29a2811e239a98d50395a6d7466b071
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-07-30 11:59:10 +0200

    update `lib_sh_pad.h`

--- silent-hill-2/src/shPad/lib_sh_pad.h
+++ silent-hill-2/src/shPad/lib_sh_pad.h
@@ -1,4 +1,10 @@
 #ifndef LIB_SH_PAD_H
 #define LIB_SH_PAD_H
 
+#include "sh2_common.h"
+
+// We dont have symbols for this TU
+
+void libShPadRead(int a, int b, void* c); // ???
+
 #endif // LIB_SH_PAD_H
