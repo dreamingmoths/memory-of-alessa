dd1f17479d9388cb9be56a0c89c2bfbb00344c4b
moth dreams <mothdreams666@gmail.com>
2026-07-30 21:06:21 -0700

    sh2: begin `Event/title`

---
+++ silent-hill-2/src/Event/title.c
@@ -0,0 +1,113 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/Event/title", TitleMain);
+
+INCLUDE_ASM("asm/nonmatchings/Event/title", titleSetDataStartPoint);
+
+INCLUDE_ASM("asm/nonmatchings/Event/title", titleInit);
+
+INCLUDE_ASM("asm/nonmatchings/Event/title", titleFadeIn);
+
+INCLUDE_ASM("asm/nonmatchings/Event/title", titleChangeMainMenu);
+
+INCLUDE_ASM("asm/nonmatchings/Event/title", titleCreateMainMenu);
+
+INCLUDE_ASM("asm/nonmatchings/Event/title", titleMainSelect);
+
+INCLUDE_ASM("asm/nonmatchings/Event/title", titleCheckPad);
+
+INCLUDE_ASM("asm/nonmatchings/Event/title", titleGetCursorFromBattleLevel);
+
+INCLUDE_ASM("asm/nonmatchings/Event/title", titleBattleSelect);
+
+INCLUDE_ASM("asm/nonmatchings/Event/title", titleBacktoMainMenuFromLevelMenu);
+
+INCLUDE_ASM("asm/nonmatchings/Event/title", titleGetBattleLevelFromCursor);
+
+INCLUDE_ASM("asm/nonmatchings/Event/title", titleGetCursorFromRiddleLevel);
+
+INCLUDE_ASM("asm/nonmatchings/Event/title", titleRiddleSelect);
+
+INCLUDE_ASM("asm/nonmatchings/Event/title", titleGetRiddleLevelFromCursor);
+
+INCLUDE_ASM("asm/nonmatchings/Event/title", titleFadeOutNewGame);
+
+INCLUDE_ASM("asm/nonmatchings/Event/title", titleFadeOut);
+
+INCLUDE_ASM("asm/nonmatchings/Event/title", titleExit);
+
+INCLUDE_ASM("asm/nonmatchings/Event/title", titleDrawTitle);
+
+INCLUDE_ASM("asm/nonmatchings/Event/title", titleDrawSprite);
+
+INCLUDE_ASM("asm/nonmatchings/Event/title", titleDrawMainMenu);
+
+INCLUDE_ASM("asm/nonmatchings/Event/title", titleDrawSubMenu);
+
+INCLUDE_ASM("asm/nonmatchings/Event/title", titleGetCursorXPosBattle);
+
+INCLUDE_ASM("asm/nonmatchings/Event/title", titleGetMenuEfctPosBattle);
+
+INCLUDE_ASM("asm/nonmatchings/Event/title", titleGetCursorXPosRiddle);
+
+INCLUDE_ASM("asm/nonmatchings/Event/title", titleGetMenuEfctPosRiddle);
+
+INCLUDE_ASM("asm/nonmatchings/Event/title", titleChangeSpriteColor2);
+
+INCLUDE_ASM("asm/nonmatchings/Event/title", titleChangeCursorSpriteColor);
+
+INCLUDE_ASM("asm/nonmatchings/Event/title", titleRenewChangeColorManagement);
+
+INCLUDE_ASM("asm/nonmatchings/Event/title", titleChangeColor);
+
+INCLUDE_ASM("asm/nonmatchings/Event/title", GameendMain);
+
+INCLUDE_ASM("asm/nonmatchings/Event/title", opd_work_init);
+
+INCLUDE_ASM("asm/nonmatchings/Event/title", GameoverMain);
+
+INCLUDE_ASM("asm/nonmatchings/Event/title", draw_opd_wowk_main);
+
+INCLUDE_RODATA("asm/nonmatchings/Event/title", @1086_0x00392310);
+
+INCLUDE_RODATA("asm/nonmatchings/Event/title", @1087_0x00392340);
+
+INCLUDE_RODATA("asm/nonmatchings/Event/title", @1088_0x00392370);
+
+INCLUDE_RODATA("asm/nonmatchings/Event/title", @1089_0x003923A0);
+
+INCLUDE_RODATA("asm/nonmatchings/Event/title", @1090_0x003923D0);
+
+INCLUDE_RODATA("asm/nonmatchings/Event/title", @1091_0x00392400);
+
+INCLUDE_RODATA("asm/nonmatchings/Event/title", @1092_0x00392430);
+
+INCLUDE_RODATA("asm/nonmatchings/Event/title", @1093_0x00392460);
+
+INCLUDE_RODATA("asm/nonmatchings/Event/title", @1094_0x00392490);
+
+INCLUDE_RODATA("asm/nonmatchings/Event/title", @1095_0x003924C0);
+
+INCLUDE_RODATA("asm/nonmatchings/Event/title", @1206);
+
+INCLUDE_RODATA("asm/nonmatchings/Event/title", @1207_0x00392510);
+
+INCLUDE_RODATA("asm/nonmatchings/Event/title", @1287_0x00392520);
+
+INCLUDE_RODATA("asm/nonmatchings/Event/title", @1313);
+
+INCLUDE_RODATA("asm/nonmatchings/Event/title", @1337);
+
+INCLUDE_RODATA("asm/nonmatchings/Event/title", @1357);
+
+INCLUDE_RODATA("asm/nonmatchings/Event/title", @1383);
+
+INCLUDE_RODATA("asm/nonmatchings/Event/title", @1403_0x003925C0);
+
+INCLUDE_RODATA("asm/nonmatchings/Event/title", @1641);
+
+INCLUDE_RODATA("asm/nonmatchings/Event/title", @1661);
+
+INCLUDE_RODATA("asm/nonmatchings/Event/title", @1681);
+
+INCLUDE_RODATA("asm/nonmatchings/Event/title", @1701);
