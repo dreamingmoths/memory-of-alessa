34ddf08c5ab5f01628e638d90c38d8a6237ee9f8
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-08-08 22:05:16 +0200

    update `m3_oni.h`

--- silent-hill-2/src/Chacter/m3_oni.h
+++ silent-hill-2/src/Chacter/m3_oni.h
@@ -4,6 +4,10 @@
 #include "sh2_common.h"
 #include "Chacter/character.h"
 
+#define ONI_PLAY_ANIME_ID_START 5400
+#define ONI_PLAY_ANIM_5401      5401
+#define D_ONI_ANIM_START        6350
+
 void shCharacterSetEnemyONILow(SubCharacter* scp);
 int shCharacterEnemyONIAnimeSet(SubCharacter* scp, int anime_id);
 int shCharacterEnemyONIAnimeSetP(SubCharacter* scp, int anime_id, int comp);
