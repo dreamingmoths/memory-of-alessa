3d38eda3c2429a7e3bac1c9c8a0fcb91d424ef7e
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-08-12 12:46:33 +0200

    update `m3_play_efect.h`

--- silent-hill-2/src/Chacter/m3_play_efect.h
+++ silent-hill-2/src/Chacter/m3_play_efect.h
@@ -1,4 +1,12 @@
 #ifndef M3_PLAY_EFECT_H
 #define M3_PLAY_EFECT_H
 
+#include "sh2_common.h"
+#include "Chacter/character.h"
+
+void PlayerCheckEffect(void);
+void PlayerCheckSound(void);
+void PlayerCheckDualShock(void);
+int PlayerJamesIsInSquare(float* pos0, float* pos1);
+
 #endif // M3_PLAY_EFECT_H
