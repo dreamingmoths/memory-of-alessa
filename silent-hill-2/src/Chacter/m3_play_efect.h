ed3ac93b74adea81a33b090df3266222d1d8ecc9
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-08-13 10:49:27 +0200

    update `m3_play_efect.h`

--- silent-hill-2/src/Chacter/m3_play_efect.h
+++ silent-hill-2/src/Chacter/m3_play_efect.h
@@ -4,6 +4,14 @@
 #include "sh2_common.h"
 #include "Chacter/character.h"
 
+// total size: 0x8
+typedef struct JamesSoundInfo {
+    // Members
+    float vol; // offset 0x0, size 0x4
+    s_char frame; // offset 0x4, size 0x1
+    s_char domain; // offset 0x5, size 0x1
+} JamesSoundInfo;
+
 void PlayerCheckEffect(void);
 void PlayerCheckSound(void);
 void PlayerCheckDualShock(void);
