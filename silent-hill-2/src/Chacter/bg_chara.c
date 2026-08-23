44c4106d665755aa366cd2a8001d13b77a7334a3
moth dreams <mothdreams666@gmail.com>
2026-08-22 23:51:18 -0700

    sh2: begin `Chacter/bg_chara`

---
+++ silent-hill-2/src/Chacter/bg_chara.c
@@ -0,0 +1,57 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/bg_chara", get_block_num);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/bg_chara", BgCharaGetBlockSize);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/bg_chara", BgCharaRelocateItemSet);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/bg_chara", BgCharaRelocateItemSetXZ);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/bg_chara", BgCharaRelocateSet);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/bg_chara", BgCharaIsId);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/bg_chara", BgCharaIsLoad);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/bg_chara", init);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/bg_chara", BgCharaManInit);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/bg_chara", BgCharaMan);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/bg_chara", BgCharaDelSlot);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/bg_chara", BgCharaGetSlotId);
+
+INCLUDE_RODATA("asm/nonmatchings/Chacter/bg_chara", @509_0x0039A000);
+
+INCLUDE_RODATA("asm/nonmatchings/Chacter/bg_chara", @510_0x0039A020);
+
+INCLUDE_RODATA("asm/nonmatchings/Chacter/bg_chara", @511_0x0039A030);
+
+INCLUDE_RODATA("asm/nonmatchings/Chacter/bg_chara", @512);
+
+INCLUDE_RODATA("asm/nonmatchings/Chacter/bg_chara", @513);
+
+INCLUDE_RODATA("asm/nonmatchings/Chacter/bg_chara", @514);
+
+INCLUDE_RODATA("asm/nonmatchings/Chacter/bg_chara", @515);
+
+INCLUDE_RODATA("asm/nonmatchings/Chacter/bg_chara", @516);
+
+INCLUDE_RODATA("asm/nonmatchings/Chacter/bg_chara", @517);
+
+INCLUDE_RODATA("asm/nonmatchings/Chacter/bg_chara", @518);
+
+INCLUDE_RODATA("asm/nonmatchings/Chacter/bg_chara", @519);
+
+INCLUDE_RODATA("asm/nonmatchings/Chacter/bg_chara", @547_0x0039A0D0);
+
+INCLUDE_RODATA("asm/nonmatchings/Chacter/bg_chara", @548);
+
+INCLUDE_RODATA("asm/nonmatchings/Chacter/bg_chara", @622);
+
+INCLUDE_RODATA("asm/nonmatchings/Chacter/bg_chara", @623);
+
+INCLUDE_RODATA("asm/nonmatchings/Chacter/bg_chara", @113_0x0039A150);
