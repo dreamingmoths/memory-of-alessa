99746be73b70591304158da2c6184f46dfdf4816
moth dreams <mothdreams666@gmail.com>
2026-08-23 00:50:51 -0700

    sh2: begin `sh2gfw_Init_ModelDrawData`

---
+++ silent-hill-2/src/GFW/sh2gfw_Init_ModelDrawData.c
@@ -0,0 +1,95 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/GFW/sh2gfw_Init_ModelDrawData", sh2gfw_srInit_ModelDrawWork);
+
+INCLUDE_ASM("asm/nonmatchings/GFW/sh2gfw_Init_ModelDrawData", sh2gfw_Get_pMD);
+
+INCLUDE_ASM("asm/nonmatchings/GFW/sh2gfw_Init_ModelDrawData", sh2gfw_Check_ModelIsOnMemory);
+
+INCLUDE_ASM("asm/nonmatchings/GFW/sh2gfw_Init_ModelDrawData", sh2gfw_LoadInit_CharaModelData);
+
+INCLUDE_ASM("asm/nonmatchings/GFW/sh2gfw_Init_ModelDrawData", sh2gfw_LoadMemorySet_CharaModelData);
+
+INCLUDE_ASM("asm/nonmatchings/GFW/sh2gfw_Init_ModelDrawData", sh2gfw_LOAD_CharaModelData);
+
+INCLUDE_ASM("asm/nonmatchings/GFW/sh2gfw_Init_ModelDrawData", sh2gfw_SyncInit_ChacterModelData);
+
+INCLUDE_ASM("asm/nonmatchings/GFW/sh2gfw_Init_ModelDrawData", sh2gfw_Cancel_LOADCharaModelData);
+
+INCLUDE_ASM("asm/nonmatchings/GFW/sh2gfw_Init_ModelDrawData", sh2gfw_ModelDrawInit_for_BackgroundLoad);
+
+INCLUDE_ASM("asm/nonmatchings/GFW/sh2gfw_Init_ModelDrawData", sh2gfw_CreateSubCharacter);
+
+INCLUDE_ASM("asm/nonmatchings/GFW/sh2gfw_Init_ModelDrawData", Delete_Model);
+
+INCLUDE_ASM("asm/nonmatchings/GFW/sh2gfw_Init_ModelDrawData", sh2gfw_Delete_Model_from_CharaID);
+
+INCLUDE_ASM("asm/nonmatchings/GFW/sh2gfw_Init_ModelDrawData", sh2gfw_kari_clear_LM);
+
+INCLUDE_ASM("asm/nonmatchings/GFW/sh2gfw_Init_ModelDrawData", Check_RevChara);
+
+INCLUDE_ASM("asm/nonmatchings/GFW/sh2gfw_Init_ModelDrawData", sh2gfw_init_CharaModel_TextureData);
+
+INCLUDE_ASM("asm/nonmatchings/GFW/sh2gfw_Init_ModelDrawData", init_CharaTex);
+
+INCLUDE_ASM("asm/nonmatchings/GFW/sh2gfw_Init_ModelDrawData", init_ReverseCharaTex);
+
+INCLUDE_ASM("asm/nonmatchings/GFW/sh2gfw_Init_ModelDrawData", Init_WithoutCharaTex);
+
+INCLUDE_RODATA("asm/nonmatchings/GFW/sh2gfw_Init_ModelDrawData", @1051_0x0038F120);
+
+INCLUDE_RODATA("asm/nonmatchings/GFW/sh2gfw_Init_ModelDrawData", @1052_0x0038F150);
+
+INCLUDE_RODATA("asm/nonmatchings/GFW/sh2gfw_Init_ModelDrawData", @1053_0x0038F160);
+
+INCLUDE_RODATA("asm/nonmatchings/GFW/sh2gfw_Init_ModelDrawData", @1054_0x0038F190);
+
+INCLUDE_RODATA("asm/nonmatchings/GFW/sh2gfw_Init_ModelDrawData", @1055_0x0038F1A0);
+
+INCLUDE_RODATA("asm/nonmatchings/GFW/sh2gfw_Init_ModelDrawData", @1056);
+
+INCLUDE_RODATA("asm/nonmatchings/GFW/sh2gfw_Init_ModelDrawData", @1057);
+
+INCLUDE_RODATA("asm/nonmatchings/GFW/sh2gfw_Init_ModelDrawData", @1058_0x0038F220);
+
+INCLUDE_RODATA("asm/nonmatchings/GFW/sh2gfw_Init_ModelDrawData", @1138);
+
+INCLUDE_RODATA("asm/nonmatchings/GFW/sh2gfw_Init_ModelDrawData", @1139);
+
+INCLUDE_RODATA("asm/nonmatchings/GFW/sh2gfw_Init_ModelDrawData", @1177_0x0038F280);
+
+INCLUDE_RODATA("asm/nonmatchings/GFW/sh2gfw_Init_ModelDrawData", @1178_0x0038F2B0);
+
+INCLUDE_RODATA("asm/nonmatchings/GFW/sh2gfw_Init_ModelDrawData", @1217);
+
+INCLUDE_RODATA("asm/nonmatchings/GFW/sh2gfw_Init_ModelDrawData", @1218_0x0038F2F0);
+
+INCLUDE_RODATA("asm/nonmatchings/GFW/sh2gfw_Init_ModelDrawData", @1219);
+
+INCLUDE_RODATA("asm/nonmatchings/GFW/sh2gfw_Init_ModelDrawData", @1220_0x0038F340);
+
+INCLUDE_RODATA("asm/nonmatchings/GFW/sh2gfw_Init_ModelDrawData", @1306);
+
+INCLUDE_RODATA("asm/nonmatchings/GFW/sh2gfw_Init_ModelDrawData", @1307_0x0038F370);
+
+INCLUDE_RODATA("asm/nonmatchings/GFW/sh2gfw_Init_ModelDrawData", @1363_0x0038F390);
+
+INCLUDE_RODATA("asm/nonmatchings/GFW/sh2gfw_Init_ModelDrawData", @1364);
+
+INCLUDE_RODATA("asm/nonmatchings/GFW/sh2gfw_Init_ModelDrawData", @1498);
+
+INCLUDE_RODATA("asm/nonmatchings/GFW/sh2gfw_Init_ModelDrawData", @1499);
+
+INCLUDE_RODATA("asm/nonmatchings/GFW/sh2gfw_Init_ModelDrawData", @1500_0x0038F450);
+
+INCLUDE_RODATA("asm/nonmatchings/GFW/sh2gfw_Init_ModelDrawData", @1612);
+
+INCLUDE_RODATA("asm/nonmatchings/GFW/sh2gfw_Init_ModelDrawData", @1613_0x0038F490);
+
+INCLUDE_RODATA("asm/nonmatchings/GFW/sh2gfw_Init_ModelDrawData", @1614_0x0038F4C0);
+
+INCLUDE_RODATA("asm/nonmatchings/GFW/sh2gfw_Init_ModelDrawData", @1615_0x0038F4F0);
+
+INCLUDE_RODATA("asm/nonmatchings/GFW/sh2gfw_Init_ModelDrawData", @1616);
+
+INCLUDE_RODATA("asm/nonmatchings/GFW/sh2gfw_Init_ModelDrawData", @1617);
