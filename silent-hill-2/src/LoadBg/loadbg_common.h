c3e5b5fdf987227f9e0a292a7bd11995060d526d
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-08-23 16:32:50 +0200

    update `loadbg_common.h`

--- silent-hill-2/src/LoadBg/loadbg_common.h
+++ silent-hill-2/src/LoadBg/loadbg_common.h
@@ -4,6 +4,31 @@
 #include "types.h"
 #include "data/fs_structs.h"
 
+// total size: 0xBC
+typedef struct _loadBgCommon_Info_T {
+    // Members
+    int glb_crd; // offset 0x0, size 0x4
+    float px; // offset 0x4, size 0x4
+    float pz; // offset 0x8, size 0x4
+    float fcx; // offset 0xC, size 0x4
+    float fcz; // offset 0x10, size 0x4
+    int icx; // offset 0x14, size 0x4
+    int icz; // offset 0x18, size 0x4
+    int minx; // offset 0x1C, size 0x4
+    int minz; // offset 0x20, size 0x4
+    int outdoor; // offset 0x24, size 0x4
+    int RoomID; // offset 0x28, size 0x4
+    int BlockID[4]; // offset 0x2C, size 0x10
+    int AroundID[5][5]; // offset 0x3C, size 0x64
+    int unit; // offset 0xA0, size 0x4
+    int load; // offset 0xA4, size 0x4
+    int need; // offset 0xA8, size 0x4
+    int lock; // offset 0xAC, size 0x4
+    int miss; // offset 0xB0, size 0x4
+    int hide_map; // offset 0xB4, size 0x4
+    int hide_map_request; // offset 0xB8, size 0x4
+} loadBgCommon_Info_T;
+
 int _loadBgCommon_LoadData(void* loadbuf, fsFileIndex* file, int limit);
 
 int BPOSfromFPOS(float f);
@@ -13,5 +38,7 @@ float FPOSfromBPOS(int i);
 int loadBgCommon_SetInfo(int glb_crd, float px, float pz);
 
 int loadBgCommon_HideMapBlockOutdoor(int mapid);
+
+extern loadBgCommon_Info_T _loadBgCommon_Info[1]; // size: 0xBC, address: 0x36D750
 
 #endif // LOADBG_COMMON_H
