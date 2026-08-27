4942bd643975252e657a501b5596657e15a00a45
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-08-27 17:40:23 +0200

    match `loadbg_event`

---
+++ silent-hill-2/src/LoadBg/loadbg_event.c
@@ -0,0 +1,180 @@
+#include "LoadBg/loadbg_event.h"
+
+static void initwork(void);
+static loadBgMem_File* getfilelist(void);
+static loadBgMem_Sect* getfreesect(void);
+static loadBgMem_Sect* getsect(void* adr);
+static loadBgMem_Sect* setsect(void* adr, int size);
+
+static void initwork(void) {
+    memset(d.filelist, 0, sizeof(d.filelist));
+    d.SectList[0] = 0;
+}
+
+static loadBgMem_File* getfilelist(void) {
+    int i;
+
+    for (i = 0; i < 1024; i++) {
+        if (d.filelist[i].file == NULL) {
+            return &d.filelist[i];
+        }    
+    }
+    return NULL;
+}
+
+static loadBgMem_Sect* getfreesect(void) {
+    int i;
+
+    for (i = 0; i < 1024; i++) {
+        if (d.SectListBuf[i].ofsE == 0) {
+            return &d.SectListBuf[i];
+        }
+    }
+    ASSERT_ON_LINE(0, 64);
+
+}
+
+static loadBgMem_Sect* getsect(void* adr) { // not line matched
+    int i; // r3
+    loadBgMem_Sect* p; // r2        
+
+    for (i = 0; i < 1024; i++) {
+        if (d.ctrl.Buffer + i * 0x2000 == adr) {
+            p = getfreesect();
+            p->ofsS = i << 13;
+            return p;
+        }  
+    
+    }
+
+    ASSERT_ON_LINE(0, 81);
+}
+
+static loadBgMem_Sect* setsect(void* adr, int size) {
+    loadBgMem_Sect* p;
+    
+    p = getsect(adr);
+
+    ASSERT_ON_LINE(!p->ofsE, 91);
+    p->ofsE = p->ofsS + (size + 0x1FFF) / 0x2000 * 0x2000;
+    return p;
+}
+
+void addsectlist(loadBgMem_Sect* sect) {
+    int i;
+    loadBgMem_Sect** l = d.SectList;     
+    
+    for (i = 0; ((i < 100) && (*l != NULL)); i++, l++) {
+    }
+    ASSERT_ON_LINE(!(*l && i == 100), 112);
+    *l = sect;
+    
+    *(l + 1) = 0;
+}
+
+void LoadBgEventInit(void* adr, u_int size) {
+    int Units;
+
+    ASSERT_ON_LINE(!((unsigned int)adr&0x3), 149);
+    Units = (size + 0x1FFF) / 0x2000;
+    _loadBgMem_InitLoad(&d.ctrl, 0x2000, Units, d.UnitLArray, d.UnitRArray,adr);
+    
+    
+    
+    
+    _loadBgMem_InitCache(&d.cache, 0x2000, 4, d.UnitCArray, d.c_buf);
+    
+    
+    
+    
+    
+    
+    
+    initwork();
+}
+
+void LoadBgEventFileLoad(fsFileIndex* fileid, void* adr) {
+    loadBgMem_Sect* p; // r5
+    int size; // r16
+    
+    size = FcGetFileSize(fileid);
+    p = setsect(adr, size);
+    ASSERT_ON_LINE(p, 175);
+    p->files = 1;
+    p->filelist = getfilelist();
+    ASSERT_ON_LINE(p->filelist, 178);
+    p->filelist->file = fileid;
+    p->filelist->ofsE = size;
+    addsectlist(p);
+}
+
+void LoadBgEventLoadSync(void) {
+    _loadBgMem_ClearRequest(&d.ctrl);
+    loadBgMem_SetRequest(&d.ctrl, d.SectList);
+    _loadBgMem_SyncLoadUnits(&d.ctrl);
+    
+    loadBgMem_LoadRequest(&d.ctrl, &d.cache, 2, 2, 0, 0, 0, 0);
+
+
+
+
+
+
+
+
+}
+
+int LoadBgEventListCnt(void) {
+    int i; // r2
+    loadBgMem_Sect** p = d.SectList; // r4
+    for (i = 0; (*p != NULL) && (i < 100); i++, p++) {
+    }
+    return i;
+}
+
+int LoadBgEventLoadCnt(void) {
+    int cnt; // r2
+    
+    
+    _loadBgMem_ClearRequest(&d.ctrl);
+    loadBgMem_SetRequest(&d.ctrl, d.SectList);
+    _loadBgMem_SyncLoadUnits(&d.ctrl);
+    
+    cnt = loadBgMem_CheckRequest(&d.ctrl, NULL);
+    return cnt;
+}
+
+int LoadBgEventIsLoad(void) {
+    return !LoadBgEventLoadCnt();
+}
+
+void LoadBgEventDispose(void) {
+    initwork();
+    
+}
+
+// INCLUDE_RODATA("asm/nonmatchings/LoadBg/loadbg_event", @45_0x0039C060);
+
+// INCLUDE_RODATA("asm/nonmatchings/LoadBg/loadbg_event", @46_0x0039C080);
+
+// INCLUDE_RODATA("asm/nonmatchings/LoadBg/loadbg_event", @69_0x0039C090);
+
+// INCLUDE_RODATA("asm/nonmatchings/LoadBg/loadbg_event", @84_0x0039C0B0);
+
+// INCLUDE_RODATA("asm/nonmatchings/LoadBg/loadbg_event", @85_0x0039C0D0);
+
+// INCLUDE_RODATA("asm/nonmatchings/LoadBg/loadbg_event", @112_0x0039C0E0);
+
+// INCLUDE_RODATA("asm/nonmatchings/LoadBg/loadbg_event", @113_0x0039C110);
+
+// INCLUDE_RODATA("asm/nonmatchings/LoadBg/loadbg_event", @153_0x0039C120);
+
+// INCLUDE_RODATA("asm/nonmatchings/LoadBg/loadbg_event", @154_0x0039C150);
+
+// INCLUDE_RODATA("asm/nonmatchings/LoadBg/loadbg_event", @180_0x0039C170);
+
+// INCLUDE_RODATA("asm/nonmatchings/LoadBg/loadbg_event", @181);
+
+// INCLUDE_RODATA("asm/nonmatchings/LoadBg/loadbg_event", @182);
+
+// INCLUDE_RODATA("asm/nonmatchings/LoadBg/loadbg_event", @183);
