7dc9868a8697233839140429dcd1900960f361b3
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-09-19 19:33:35 +0200

    update `hh_class_blood_pool_phenomenon_01.h`

--- silent-hill-2/src/Effect2/hh_class_blood_pool_phenomenon_01.h
+++ silent-hill-2/src/Effect2/hh_class_blood_pool_phenomenon_01.h
@@ -1,4 +1,18 @@
 #ifndef HH_CLASS_BLOOD_POOL_PHENOMENON_01_H
 #define HH_CLASS_BLOOD_POOL_PHENOMENON_01_H
 
+#include "sh2_common.h"
+#include "Effect2/hh_class_manager.h"
+
+// total size: 0x20
+typedef struct HH_Object_Blood_Pool_Phenomenon_01 {
+    // Members
+    struct Object_DataBlock_Header Header; // offset 0x0, size 0x10
+    unsigned int Step; // offset 0x10, size 0x4
+    float Alpha; // offset 0x14, size 0x4
+    float Timer; // offset 0x18, size 0x4
+} HH_Object_Blood_Pool_Phenomenon_01;
+
+u_int HH_Class_Blood_Pool_Phenomenon_01(void* pBlock, ImpactQueue_Element* pElement);
+
 #endif // HH_CLASS_BLOOD_POOL_PHENOMENON_01_H
